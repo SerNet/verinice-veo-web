ad42a12a5056ebe0e5071e5fe7d354880b11212a
Markus Werner <markus@cpmsys.de>
2018-08-10 20:05:47 +0200

    Faker

--- components/TreeNav/TreeView.vue
+++ components/TreeNav/TreeView.vue
@@ -3,12 +3,17 @@
 </template>
 
 <script lang="ts">
-import { Component, Inject, Model, Prop, Vue, Watch } from 'nuxt-property-decorator'
+import {
+  Component,
+  Inject,
+  Model,
+  Prop,
+  Vue,
+  Watch
+} from "nuxt-property-decorator";
 
 @Component
-export default class TreeView extends Vue {
-  
-};
+export default class TreeView extends Vue {}
 </script>
 
 <style lang="stylus" scoped>
