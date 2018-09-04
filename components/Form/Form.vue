ae68057d709ca47c217a842924b9a17dbb7891c6
Philipp Ballhausen <philipp@cpmsys.de>
2018-09-04 16:45:44 +0200

    Links

--- components/Form/Form.vue
+++ components/Form/Form.vue
@@ -1,11 +1,13 @@
 <template>
   <v-layout class="form-panels" style="overflow: auto !important;" column>
-    <v-expansion-panel :value="[true]" class="pa-3" :expand="true">
-      <v-expansion-panel-content class="grey lighten-3">
+    <v-expansion-panel :value="[true]" class="pa-3" :expand="false">
+
+      <!-- Editor -->
+      <v-expansion-panel-content class="grey lighten-3 elevation-1">
         <div slot="header">
           <span style="margin-left: 6px;">Basic</span>
           <v-menu style="float:right;" @click.native.stop>
-            <v-btn slot="activator" icon>
+            <v-btn slot="activator" icon style="margin: 0; height: 24px; width: 24px;">
               <v-icon>more_vert</v-icon>
             </v-btn>
             <v-list>
@@ -29,6 +31,39 @@
           </v-card-text>
         </v-card>
       </v-expansion-panel-content>
+
+      <!-- Links -->
+      <v-expansion-panel-content class="grey lighten-3 elevation-1">
+        <div slot="header">
+          <span style="margin-left: 6px;">Links</span>
+          <v-menu style="float:right;" @click.native.stop>
+            <v-btn slot="activator" icon style="margin: 0; height: 24px; width: 24px;">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-title>Test 1</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+        </div>
+        <v-card>
+          <v-card-text class="white lighten-3">
+
+            <v-data-table :headers="headers" :items="links" v-model="selected" item-key="name" select-all>
+              <template slot="items" slot-scope="link">
+                <td>
+                  <v-checkbox v-model="link.selected" primary hide-details></v-checkbox>
+                </td>
+                <td>{{ link.item.from }}</td>
+                <td>{{ link.item.desc }}</td>
+                <td>{{ link.item.to }}</td>
+              </template>
+            </v-data-table>
+
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
     </v-expansion-panel>
   </v-layout>
 </template>
@@ -47,10 +82,41 @@ export default Vue.extend({
         chapter: "TEST"
       },
       previewNav: { open: false },
-      desc:
-        "Lorem ipsum dolor sit amet, consectetur \n" +
-        "adipiscing elit. Nam gravida venenatis \n" +
-        "accumsan. In mi massa, tempus"
+      selected: [],
+      headers: [
+        {
+          text: "Start-Element",
+          value: "from"
+        },
+        {
+          text: "Beziehung",
+          value: "desc"
+        },
+        {
+          text: "Ziel-Element",
+          value: "to"
+        }
+      ],
+      links: [
+        {
+          value: false,
+          from: "Router",
+          to: "Netzwerk",
+          desc: "befindet sich in"
+        },
+        {
+          value: false,
+          from: "Server",
+          to: "Serverschrank",
+          desc: "in"
+        },
+        {
+          value: false,
+          from: "Schlüssel",
+          to: "Schloss",
+          desc: "passt in"
+        }
+      ]
     };
   },
   props: {
--- components/MainLayout/MainToolbar.vue
+++ components/MainLayout/MainToolbar.vue
@@ -2,6 +2,7 @@
   <v-toolbar app color="primary" dark="" style="overflow:hidden" :clipped-left="clipped" id="mainToolbar">
     <main-toolbar-logo v-if="!showSearch" />
     <v-toolbar-side-icon light class="hidden-sm-and-up" @click.stop="toggleDrawer()" v-if="!showSearch" style="color: #e53935" />
+
     <!-- normale Suche -->
     <v-layout class="hidden-sm-and-down" row justify-space-around style="margin-left: 200px;">
       <v-flex xs12 sm6 align-content-center>
@@ -23,23 +24,6 @@
       <account-btn v-if="username" :username="username" @logout="logout"></account-btn>
       <login-dialog v-else @login="login" :error="authError"></login-dialog>
     </template>
-
-    <v-menu offset-y v-if="!showSearch">
-      <v-btn slot="activator" color="primary" icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
-      <v-list>
-        <v-list-tile>
-          <v-list-tile-title>Test 1</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-title>Test 2</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-title>Test 3</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-menu>
   </v-toolbar>
 </template>
 
