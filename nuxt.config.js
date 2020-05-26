4e3da73b3135aa0623ad652785e9b0d15c3ecc10
Davit Svanidze <davit@cpmsys.de>
2020-05-26 09:57:35 +0200

    CHANGED: import VeoForm as plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,12 +39,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    '~/plugins/logger',
-    '~/plugins/auth',
-    '~/plugins/api',
-    '~/plugins/navigation'
-  ],
+  plugins: ['~/plugins/logger', '~/plugins/auth', '~/plugins/api', '~/plugins/navigation', '~/plugins/veoform'],
 
   /*
    ** Icon options
---
+++ pages/forms.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <VeoForm v-model="form.value" :schema="form.schema" :ui="form.ui" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import BLA, { VeoForm } from '@cpmsys/veo-forms'
+
+export default Vue.extend({
+  name: 'Forms',
+  components: {
+    // VeoForm: Vue.extend(VeoForm)
+  },
+  data() {
+    return {
+      form: {
+        schema: {
+          type: 'object',
+          required: ['name', 'description', 'orderMulti'],
+          properties: {
+            name: {
+              type: 'string',
+              minLength: 1
+            },
+            description: {
+              type: 'string'
+            }
+          }
+        },
+        ui: {
+          type: 'Layout',
+          options: {
+            direction: 'vertical',
+            format: 'group'
+          },
+          elements: [
+            {
+              type: 'Control',
+              scope: '#/properties/name'
+            },
+            {
+              type: 'Control',
+              scope: '#/properties/description',
+              options: {
+                label: 'Description',
+                format: 'multiline',
+                maxRows: 12
+              }
+            }
+          ]
+        },
+        value: {}
+      }
+    }
+  },
+  methods: {},
+  head() {
+    return {
+      title: 'History'
+    }
+  }
+})
+</script>
+
+<style scoped></style>
