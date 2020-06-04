7400e0cd0149156eeb27cfec7ae2605c3a8cf61e
Davit Svanidze <davit@cpmsys.de>
2020-06-04 16:22:19 +0200

    CHANGED: working VeoForm component and cleaned useless comments

--- pages/forms.vue
+++ pages/forms.vue
@@ -1,27 +1,20 @@
 <template>
   <div>
     <VeoForm v-model="form.value" :schema="form.schema" :ui="form.ui" />
-    <!-- <VeoForm v-model="test" :visible="true" name="asadadad" :options="options" :schema="schema" :disabled="false" /> -->
-    <!-- <HelloWorld /> -->
-    <!-- <SfcRollupVuetifySample v-model="test" /> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { VeoForm } from '@cpmsys/veo-forms'
-// import '@cpmsys/veo-forms/lib/css/main.css'
-// import { SfcRollupVuetifySample } from 'sfc-rollup-vuetify'
 
 export default Vue.extend({
   name: 'Forms',
   components: {
     VeoForm
-    // SfcRollupVuetifySample
   },
   data() {
     return {
-      test: '',
       schema: {
         type: 'string'
       },
@@ -74,7 +67,7 @@ export default Vue.extend({
   methods: {},
   head() {
     return {
-      title: 'History'
+      title: 'Forms'
     }
   }
 })
