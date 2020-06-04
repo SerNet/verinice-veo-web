55c131c5a97009ff41bf71d1df66aa05dca2ae50
Davit Svanidze <davit@cpmsys.de>
2020-06-04 12:07:05 +0200

    CHANGED: @cpmsys/veo-forms component import does work

--- pages/forms.vue
+++ pages/forms.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
     <VeoForm v-model="form.value" :schema="form.schema" :ui="form.ui" />
+    <!-- <VeoForm v-model="test" :visible="true" name="asadadad" :options="options" :schema="schema" :disabled="false" /> -->
+    <!-- <HelloWorld /> -->
+    <!-- <SfcRollupVuetifySample v-model="test" /> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { VeoForm } from '@cpmsys/veo-forms'
+// import '@cpmsys/veo-forms/lib/css/main.css'
+// import { SfcRollupVuetifySample } from 'sfc-rollup-vuetify'
 
 export default Vue.extend({
   name: 'Forms',
   components: {
     VeoForm
+    // SfcRollupVuetifySample
   },
   data() {
     return {
+      test: '',
+      schema: {
+        type: 'string'
+      },
+      options: {
+        label: 'Text'
+      },
       form: {
         schema: {
           type: 'object',
@@ -38,7 +51,10 @@ export default Vue.extend({
           elements: [
             {
               type: 'Control',
-              scope: '#/properties/name'
+              scope: '#/properties/name',
+              options: {
+                label: 'Input Text'
+              }
             },
             {
               type: 'Control',
