630da95c87f11699ce255bc97e3cae174ef3c539
Davit Svanidze <davit@cpmsys.de>
2020-06-17 10:51:20 +0200

    FIXED: added languages query to translations api request

--- plugins/api/translation.ts
+++ plugins/api/translation.ts
@@ -1,12 +1,14 @@
 import { Client } from '~/plugins/api'
 
+type Languages = 'de' | 'en' | 'cs' | 'it'
+
 export default function(api: Client) {
   return {
     /**
      * Retrieves a map of UI translation key-value pairs.
      */
-    fetch() {
-      return api.req('/api/translations')
+    fetch(languages: Languages[]) {
+      return api.req(`/api/translations?languages=${encodeURIComponent(languages.toString())}`)
     }
   }
 }
