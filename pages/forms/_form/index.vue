2162530d621e144538f9f16252282fb36fb9001a
Davit Svanidze <davit@cpmsys.de>
2020-06-22 18:03:14 +0200

    CHANGED: forms/_forms/ and forms/_forms/_object work

--- pages/forms/_form/index.vue
+++ pages/forms/_form/index.vue
@@ -2,11 +2,7 @@
   <div>
     <div class="d-flex flex-row">
       <div class="d-flex flex-column flex-grow-1 pa-6">
-        <div class="text-center my-6">
-          <v-btn dark class="ma-1" @click="activeLanguage = 'en'">English</v-btn>
-          <v-btn dark class="ma-1" @click="activeLanguage = 'de'">Deutsch</v-btn>
-        </div>
-        <veo-form v-model="value" :schema="objectSchema" :ui="formSchema" :lang="lang[activeLanguage]" />
+        <veo-form v-model="form.value" :schema="form.objectSchema" :ui="form.formSchema" />
       </div>
     </div>
 
@@ -18,7 +14,7 @@
               <v-expansion-panel-header>Generated Data</v-expansion-panel-header>
               <v-expansion-panel-content>
                 <code>
-                  <pre>{{ JSON.stringify(value, null, 4) }}</pre>
+                  <pre>{{ JSON.stringify(form.value, null, 4) }}</pre>
                 </code>
               </v-expansion-panel-content>
             </v-expansion-panel>
@@ -43,33 +39,80 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import JsonPointer from 'json-ptr'
+// import JsonPointer from 'json-ptr'
 
-const preprocessSchemaForTranslation = (schema: any) => {
-  schema.$schema = 'http://json-schema.org/draft-07/schema#'
-  JsonPointer.list(schema, '#')
-    .filter((obj: any) => /\/(?<!type\/)enum\/\d+$/gi.test(obj.fragmentId))
-    .forEach((obj: any) => {
-      JsonPointer.set(schema, obj.fragmentId, `#lang/${obj.value}`)
-    })
-  return schema
-}
+// const preprocessSchemaForTranslation = (schema: any) => {
+//   schema.$schema = 'http://json-schema.org/draft-07/schema#'
+//   JsonPointer.list(schema, '#')
+//     .filter((obj: any) => /\/(?<!type\/)enum\/\d+$/gi.test(obj.fragmentId))
+//     .forEach((obj: any) => {
+//       JsonPointer.set(schema, obj.fragmentId, `#lang/${obj.value}`)
+//     })
+//   return schema
+// }
 
 export default Vue.extend({
   name: 'Forms',
   async asyncData(context) {
-    const objectSchema = preprocessSchemaForTranslation(await context.app.$api.schema.fetch('process'))
-    // const translation = await context.app.$api.translation.fetch(['de', 'en'])
-    const formSchema = await require(`./${context.route.params.form}.json`)
-    const translation = await require('./../Translations.json')
-    return { objectSchema, formSchema, lang: translation.lang }
+    // const objectSchema = preprocessSchemaForTranslation(await context.app.$api.schema.fetch('process'))
+    // // const translation = await context.app.$api.translation.fetch(['de', 'en'])
+    // const formSchema = await require(`./${context.route.params.form}.json`)
+    // const translation = await require('./../Translations.json')
+    // return { objectSchema, formSchema, lang: translation.lang }
   },
   data() {
     return {
       panel: true,
-      activeLanguage: 'de',
-      value: {
-        owner: { href: '/units/d071f194-4d7c-4ba1-809a-ddc7aa865f8b' }
+      form: {
+        objectSchema: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string'
+            },
+            owner: {
+              type: 'object',
+              properties: {
+                displayName: {
+                  type: 'string',
+                  description: 'A friendly human readable title of the referenced unit.'
+                },
+                href: {
+                  type: 'string',
+                  description: 'The resource URL of the referenced unit.'
+                }
+              },
+              required: ['displayName', 'href'],
+              description: 'A reference to the unit containing this entity.'
+            }
+          }
+        },
+        formSchema: {
+          type: 'Layout',
+          options: {
+            direction: 'vertical',
+            format: 'group'
+          },
+          elements: [
+            {
+              type: 'Label',
+              text: 'Formular für die Objekterstellung',
+              options: {
+                class: 'display-1'
+              }
+            },
+            {
+              type: 'Control',
+              scope: '#/properties/name',
+              options: {
+                label: 'Name'
+              }
+            }
+          ]
+        },
+        value: {
+          owner: { href: '/units/fe255b98-443a-4bd3-8f8e-14af258b21a8' }
+        }
       },
       createdObjectUUID: ''
     }
@@ -81,7 +124,7 @@ export default Vue.extend({
   },
   methods: {
     async create() {
-      const res = await this.$api.process.create(this.value)
+      const res = await this.$api.process.create(this.form.value)
       this.createdObjectUUID = res.resourceId
     }
   },
