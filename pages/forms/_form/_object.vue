64a5ffdea662f26bae35c3c2bcda28deb24ba0e5
Davit Svanidze <davit@cpmsys.de>
2020-06-23 12:20:42 +0200

    CHANGED: asyncData replaced with fetch() in _object

--- pages/forms/_form/_object.vue
+++ pages/forms/_form/_object.vue
@@ -1,39 +1,47 @@
 <template>
   <div>
-    <div class="d-flex flex-row">
-      <div class="d-flex flex-column flex-grow-1 pa-6">
-        <div class="text-center my-6">
-          <v-btn dark class="ma-1" @click="activeLanguage = 'en'">English</v-btn>
-          <v-btn dark class="ma-1" @click="activeLanguage = 'de'">Deutsch</v-btn>
-        </div>
-        <veo-form v-model="value" :schema="objectSchema" :ui="formSchema" :lang="lang[activeLanguage]" />
+    <template v-if="$fetchState.pending">
+      <div class="text-center ma-12">
+        <v-progress-circular indeterminate color="primary" size="50" />
       </div>
-    </div>
+    </template>
 
-    <div class="d-flex flex-row">
-      <div class="d-flex flex-column flex-grow-1 pa-6">
-        <div class="mx-auto" style="width:800px">
-          <v-expansion-panels v-model="panel">
-            <v-expansion-panel>
-              <v-expansion-panel-header>Generated Data</v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <code>
-                  <pre>{{ JSON.stringify(value, null, 4) }}</pre>
-                </code>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
+    <template v-else>
+      <div class="d-flex flex-row">
+        <div class="d-flex flex-column flex-grow-1 pa-6">
+          <div class="text-center my-6">
+            <v-btn dark class="ma-1" @click="activeLanguage = 'en'">English</v-btn>
+            <v-btn dark class="ma-1" @click="activeLanguage = 'de'">Deutsch</v-btn>
+          </div>
+          <veo-form v-if="!$fetchState.pending" v-model="form.value" :schema="form.objectSchema" :ui="form.formSchema" :lang="form.lang[activeLanguage]" />
         </div>
       </div>
-    </div>
 
-    <div class="d-flex flex-row">
-      <div class="d-flex flex-column flex-grow-1 pa-6">
-        <div class="mx-auto" style="width:800px">
-          <v-btn color="primary" block @click="save">Speichern</v-btn>
+      <div class="d-flex flex-row">
+        <div class="d-flex flex-column flex-grow-1 pa-6">
+          <div class="mx-auto" style="width:800px">
+            <v-expansion-panels v-model="panel">
+              <v-expansion-panel>
+                <v-expansion-panel-header>Generated Data</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <code>
+                    <pre>{{ JSON.stringify(form.value, null, 4) }}</pre>
+                  </code>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div class="d-flex flex-row">
+        <div class="d-flex flex-column flex-grow-1 pa-6">
+          <div class="mx-auto" style="width:800px">
+            <v-btn color="primary" block @click="save">Speichern</v-btn>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -53,20 +61,30 @@ const preprocessSchemaForTranslation = (schema: any) => {
 
 export default Vue.extend({
   name: 'Forms',
-  async asyncData({ app, params }) {
+  async fetch() {
     // TODO "process" muss überall ersetzt werden durch das Objekt, welches im formSchema als ziel Objekt vorgegeben wird
-    const objectSchema = preprocessSchemaForTranslation(await app.$api.schema.fetch('process'))
+    const objectSchema = preprocessSchemaForTranslation(await this.$api.schema.fetch('process'))
     // const translation = await context.app.$api.translation.fetch(['de', 'en'])
-    const formSchema = await require(`./${params.form}.json`)
-    const translation = await require('./../Translations.json')
-    const value = await app.$api.process.fetch(params.object)
-    return { objectSchema, formSchema, lang: translation.lang, value }
+    const formSchema = await require(`./${this.$route.params.form}.json`)
+    const { lang } = await require('./../Translations.json')
+    const value = await this.$api.process.fetch(this.$route.params.object)
+    this.form = {
+      objectSchema,
+      formSchema,
+      value,
+      lang
+    }
   },
   data() {
     return {
       panel: true,
       activeLanguage: 'de',
-      value: undefined as any | undefined
+      form: {
+        objectSchema: {},
+        formSchema: {},
+        value: {},
+        lang: {}
+      }
     }
   },
   methods: {
@@ -74,12 +92,12 @@ export default Vue.extend({
       try {
         // TODO: find better solution
         //  Add Keys and IDs manually
-        Object.keys(this.value.customAspects).forEach((key: string) => {
-          this.value.customAspects[key] = { ...this.value.customAspects[key], id: '00000000-0000-0000-0000-000000000000', type: key }
+        Object.keys(this.form.value.customAspects).forEach((key: string) => {
+          this.form.value.customAspects[key] = { ...this.form.value.customAspects[key], id: '00000000-0000-0000-0000-000000000000', type: key }
         })
         // this.value.customAspects = {}
-        console.log(this.value)
-        await this.$api.process.update(this.$route.params.object, this.value)
+        console.log(this.form.value)
+        await this.$api.process.update(this.$route.params.object, this.form.value)
       } catch (e) {
         console.error(e)
       }
