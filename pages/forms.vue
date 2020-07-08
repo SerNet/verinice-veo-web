0047b9f67bf09c79ba5a14d13cf9cb57f43b5ff6
Davit Svanidze <davit@cpmsys.de>
2020-07-08 13:38:23 +0200

    CHANGED: veo.forms navigation and loading progress of /forms/ list items

--- pages/forms.vue
+++ pages/forms.vue
@@ -3,13 +3,17 @@
     <v-row no-gutters>
       <AppSideContainer side="left" :width="350">
         <v-col cols="12 pa-0">
-          <div v-if="processes.length > 0">
-            <div class="display pa-3">veo.forms Navigation</div>
+          <div v-if="$fetchState.pending">
+            <div class="text-center mt-6">
+              <v-progress-circular indeterminate color="primary" />
+            </div>
+          </div>
+          <div v-else>
             <v-list dense>
               <v-list-item-group>
                 <v-list-item v-for="process in processes" :key="process.id" :to="`/forms/07b57947-6259-471e-95cf-5970a40fac3f/${process.id}/`">
                   <v-list-item-content>
-                    <v-list-item-title class="primary--text text-uppercase font-weight-medium" v-text="process.name" />
+                    <v-list-item-title class="primary--text text-uppercase" v-text="process.name" />
                   </v-list-item-content>
                 </v-list-item>
               </v-list-item-group>
@@ -27,7 +31,12 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { IBaseObject } from '../lib/utils'
 import AppSideContainer from '~/components/layout/AppSideContainer.vue'
+
+interface IData {
+  processes: IBaseObject[]
+}
 
 export default Vue.extend({
   components: {
@@ -37,7 +46,7 @@ export default Vue.extend({
   async fetch() {
     this.processes = await this.$api.process.fetchAll()
   },
-  data() {
+  data(): IData {
     return {
       processes: []
     }
