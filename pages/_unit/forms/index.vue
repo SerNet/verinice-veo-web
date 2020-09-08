bef4fa9ddcbdfcc0a162d4d1602048b27a58fb16
Davit Svanidze <davit@cpmsys.de>
2020-09-08 20:16:44 +0200

    CHANGED: updated @cpmsys/veo-forms module and VT Form

--- pages/_unit/forms/index.vue
+++ pages/_unit/forms/index.vue
@@ -2,7 +2,7 @@
 export default {
   middleware({ params, redirect }) {
     // TODO fetchAll forms und redirect zur ersten Form
-    return redirect(`/${params.unit}/forms/dc763de2-84c3-40e7-b4c2-7e378aba8564/`)
+    return redirect(`/${params.unit}/forms/50093a5c-52e1-4b32-aa52-cb2e13758a1f/`)
   }
 }
 </script>
