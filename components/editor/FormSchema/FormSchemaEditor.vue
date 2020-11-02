4207d0bf1a888978a6c679782a1926432f91848e
Davit Svanidze <davit@cpmsys.de>
2020-11-02 18:02:42 +0100

    CHANGED: FormschemaEditor test nested drag

--- components/editor/FormSchema/FormSchemaEditor.vue
+++ components/editor/FormSchema/FormSchemaEditor.vue
@@ -4,19 +4,55 @@
       <v-select :items="items" v-model="layout" label="Layout Style" style="width:200px;"></v-select>
       <VeoForm v-model="objectData" :schema="objectSchema" :is-valid.sync="isValid" :error-messages.sync="errorMessages" />
     </div>
+
+    <!-- <draggable v-model="myArray" group="people" @start="drag = true" @end="drag = false">
+      <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+    </draggable> -->
+    <nested-draggable :tasks="list" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
+// import draggable from 'vuedraggable'
+import NestedDraggable from '@/components/editor/FormSchema/NestedDraggable.vue'
 
 export default Vue.extend({
   name: 'FormSchemaEditor',
+  components: {
+    // draggable
+    NestedDraggable
+  },
   props: {
     objectSchema: Object
   },
   data() {
     return {
+      list: [
+        {
+          name: 'task 1',
+          tasks: [
+            {
+              name: 'task 2',
+              tasks: []
+            }
+          ]
+        },
+        {
+          name: 'task 3',
+          tasks: [
+            {
+              name: 'task 4',
+              tasks: []
+            }
+          ]
+        },
+        {
+          name: 'task 5',
+          tasks: []
+        }
+      ],
+      myArray: [{ name: 'Hallo 1' }, { name: 'Hallo 2' }, { name: 'Hallo 3' }, { name: 'Hallo 4' }],
       layout: 'long',
       items: [
         {
---
+++ components/editor/FormSchema/NestedDraggable.vue
@@ -0,0 +1,31 @@
+<template>
+  <draggable class="dragArea" tag="ul" :list="tasks" :group="{ name: 'g1' }">
+    <li v-for="el in tasks" :key="el.name">
+      <p>{{ el.name }}</p>
+      <nested-draggable :tasks="el.tasks" />
+    </li>
+  </draggable>
+</template>
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import draggable from 'vuedraggable'
+
+export default Vue.extend({
+  name: 'nested-draggable',
+  props: {
+    tasks: {
+      required: true,
+      type: Array
+    } as PropOptions<any[]>
+  },
+  components: {
+    draggable
+  }
+})
+</script>
+<style scoped>
+.dragArea {
+  min-height: 50px;
+  outline: 1px dashed;
+}
+</style>
