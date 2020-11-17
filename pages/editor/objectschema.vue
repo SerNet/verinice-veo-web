878451997f1c3739f985e41c9899cebb4240f057
Jonas Heitmann <jonas@cpmsys.de>
2020-11-17 17:13:22 +0100

    Editor can now get hidden

--- pages/editor/objectschema.vue
+++ pages/editor/objectschema.vue
@@ -1,11 +1,19 @@
 <template>
   <v-col class="pa-0 fill-height overflow-hidden" style="max-height: calc(100vh - 70px);" cols="12">
     <v-row class="fill-height ma-0">
-      <v-col class="pa-0" :style="{ maxHeight }" style="overflow: auto" cols="12" lg="6">
-        <h1 class="ml-4 mt-2">Objektschema Editor</h1>
+      <v-col class="pa-0" :style="{ maxHeight }" style="overflow: auto" cols="12" :lg="(collapsed) ? 12 : 6">
+        <div class="d-flex justify-lg-space-between align-center">
+          <h1 class="ml-4 mt-2">Objektschema Editor</h1>
+          <div class="veo-collapse-editor pa-1">
+            <v-btn icon @click="collapsed = !collapsed">
+              <v-icon v-if="collapsed">mdi-chevron-left</v-icon>
+              <v-icon v-else>mdi-chevron-right</v-icon>
+            </v-btn>
+          </div>
+        </div>
         <ObjectSchemaEditor v-if="schema" v-model="schema" @schema-updated="updateSchema" />
       </v-col>
-      <v-col class="pa-0 fill-height" :style="{ maxHeight }" style="overflow: auto;" cols="12" lg="6">
+      <v-col v-if="!collapsed" class="pa-0 fill-height" :style="{ maxHeight }" style="overflow: auto;" cols="12" lg="6">
         <CodeEditor v-model="code" />
       </v-col>
     </v-row>
@@ -21,6 +29,7 @@ import { generateSchema } from '~/lib/ObjectSchemaHelper'
 export default Vue.extend({
   data() {
     return {
+      collapsed: false as boolean,
       tab: 'form-schema',
       schema: undefined as VEOObjectSchemaRAW | undefined
     }
@@ -52,4 +61,10 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.veo-collapse-editor {
+  background-color: rgb(245, 245, 245);
+  border-bottom-left-radius: 4px;
+  border-top-left-radius: 4px;
+}
+</style>
