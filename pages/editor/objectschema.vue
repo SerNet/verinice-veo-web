568abc0e271e3f452c6021a8fa8dc36259ff6f0a
Jonas Heitmann <jonas@cpmsys.de>
2020-11-17 17:23:40 +0100

    Improved layout while editor is collapsed

--- pages/editor/objectschema.vue
+++ pages/editor/objectschema.vue
@@ -1,17 +1,21 @@
 <template>
   <v-col class="pa-0 fill-height overflow-hidden" style="max-height: calc(100vh - 70px);" cols="12">
     <v-row class="fill-height ma-0">
-      <v-col class="pa-0" :style="{ maxHeight }" style="overflow: auto" cols="12" :lg="(collapsed) ? 12 : 6">
-        <div class="d-flex justify-lg-space-between align-center">
-          <h1 class="ml-4 mt-2">Objektschema Editor</h1>
-          <div class="veo-collapse-editor pa-1">
-            <v-btn icon @click="collapsed = !collapsed">
-              <v-icon v-if="collapsed">mdi-chevron-left</v-icon>
-              <v-icon v-else>mdi-chevron-right</v-icon>
-            </v-btn>
-          </div>
+      <v-col class="pa-0" :style="{ maxHeight }" style="overflow: auto; position: relative;" cols="12" :lg="collapsed ? 12 : 6">
+        <div class="veo-collapse-editor pa-1">
+          <v-btn icon @click="collapsed = !collapsed">
+            <v-icon v-if="collapsed">mdi-chevron-left</v-icon>
+            <v-icon v-else>mdi-chevron-right</v-icon>
+          </v-btn>
         </div>
-        <ObjectSchemaEditor v-if="schema" v-model="schema" @schema-updated="updateSchema" />
+        <v-row no-gutters class="flex-column align-center">
+          <v-col :cols="collapsed ? 8 : 12">
+            <h1 class="ml-4 mt-2">Objektschema Editor</h1>
+          </v-col>
+          <v-col :cols="collapsed ? 8 : 12">
+            <ObjectSchemaEditor v-if="schema" v-model="schema" @schema-updated="updateSchema" />
+          </v-col>
+        </v-row>
       </v-col>
       <v-col v-if="!collapsed" class="pa-0 fill-height" :style="{ maxHeight }" style="overflow: auto;" cols="12" lg="6">
         <CodeEditor v-model="code" />
@@ -66,5 +70,7 @@ export default Vue.extend({
   background-color: rgb(245, 245, 245);
   border-bottom-left-radius: 4px;
   border-top-left-radius: 4px;
+  position: absolute;
+  right: 0;
 }
 </style>
