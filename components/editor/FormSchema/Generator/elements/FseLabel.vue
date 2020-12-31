c81ec9c27f078886027c1771edb976d83c0bae13
Davit Svanidze <davit@cpmsys.de>
2020-12-31 13:44:13 +0100

    CHANGED: FseLabel Dialog adjusted to other FSE dialogs

--- components/editor/FormSchema/Generator/elements/FseLabel.vue
+++ components/editor/FormSchema/Generator/elements/FseLabel.vue
@@ -18,53 +18,73 @@
         </v-btn>
       </v-col>
     </v-row>
-    <VeoDialog v-model="dialog.open" headline="Edit" large persistent>
+    <VeoDialog
+      v-model="dialog.open"
+      :key="formSchemaPointer"
+      :headline="$t('editor.formschema.edit.text.headline')"
+      large
+    >
       <template #default>
-        <v-text-field v-model="dialog.data.text.value" label="Text" />
-        <v-combobox
-          v-model="dialog.data.class.value"
-          label="Class"
-          multiple
-          chips
-          append-icon=""
-        />
-        <v-combobox
-          v-model="dialog.data.style.value"
-          label="Style"
-          multiple
-          chips
-          append-icon=""
-        />
+        <v-form>
+          <v-row no-gutters class="align-center mt-4">
+            <v-col :cols="12" :md="5">
+              <span style="font-size: 1.2rem;"> {{ $t('editor.formschema.edit.input.text.text') }}*: </span>
+            </v-col>
+            <v-col :cols="12" :md="5">
+              <v-text-field
+                v-model="dialog.data.text.value"
+                :label="$t('editor.formschema.edit.input.text')"
+                required
+              />
+            </v-col>
+          </v-row>
+          <v-row no-gutters class="align-center">
+            <v-col :cols="12" :md="5">
+              <span style="font-size: 1.2rem;"> {{ $t('editor.formschema.edit.css.class') }}: </span>
+            </v-col>
+            <v-col :cols="12" :md="5">
+              <v-combobox
+                v-model="dialog.data.class.value"
+                :label="$t('editor.formschema.edit.css.class.text')"
+                multiple
+                chips
+                append-icon=""
+              />
+            </v-col>
+          </v-row>
+          <v-row no-gutters class="align-center">
+            <v-col :cols="12" :md="5">
+              <span style="font-size: 1.2rem;"> {{ $t('editor.formschema.edit.css.style') }}: </span>
+            </v-col>
+            <v-col :cols="12" :md="5">
+              <v-combobox
+                v-model="dialog.data.style.value"
+                :label="$t('editor.formschema.edit.css.style.text')"
+                multiple
+                chips
+                append-icon=""
+              />
+            </v-col>
+          </v-row>
+        </v-form>
+        <small>{{ $t('editor.dialog.requiredfields') }}</small>
       </template>
       <template #dialog-options>
         <v-spacer />
-        <v-btn text color="primary" @click="dialog.open = false">
+        <v-btn text color="primary" outlined @click="dialog.open = false">
           {{ $t('global.button.close') }}
         </v-btn>
-        <v-btn text color="primary" @click="save">
+        <v-btn text color="primary" outlined @click="save">
           {{ $t('global.button.save') }}
         </v-btn>
       </template>
     </VeoDialog>
 
-    <!-- TODO: i18n for dialogs -->
-    <VeoDialog
+    <VEOFSEDeleteDialog
       v-model="deleteDialog.open"
-      :headline="$t('editor.formschema.delete.control.headline')"
-    >
-      <template #default>
-        {{ $t('editor.formschema.delete.control.text', { element: 'Text' }) }}
-      </template>
-      <template #dialog-options>
-        <v-btn text color="primary" @click="deleteDialog.open = false">
-          {{ $t('global.button.no') }}
-        </v-btn>
-        <v-spacer />
-        <v-btn text color="primary" @click="onDelete">
-          {{ $t('global.button.delete') }}
-        </v-btn>
-      </template>
-    </VeoDialog>
+      :name="$t('editor.formschema.elements.text.name')"
+      @delete="onDelete"
+    />
   </v-card>
 </template>
 
@@ -74,9 +94,13 @@ import { Prop } from 'vue/types/options'
 import vjp from 'vue-json-pointer'
 import { JsonPointer } from 'json-ptr'
 import { BaseObject } from '~/components/forms/utils'
+import VEOFSEDeleteDialog from '~/components/dialogs/SchemaEditors/VEOFSEDeleteDialog.vue'
 
 export default Vue.extend({
   name: 'FseLabel',
+  components: {
+    VEOFSEDeleteDialog
+  },
   props: {
     name: String,
     lang: Object as Prop<BaseObject>,
@@ -109,10 +133,7 @@ export default Vue.extend({
     open() {
       this.dialog.open = true
 
-      this.dialog.data.text.value = this.getValue(
-        '#/text',
-        this.dialog.data.text.default
-      )
+      this.dialog.data.text.value = this.getValue('#/text', this.dialog.data.text.default)
       this.dialog.data.class.value = this.stringToArray(
         this.getValue('#/options/class', this.dialog.data.class.default),
         ' '
@@ -123,11 +144,7 @@ export default Vue.extend({
       )
     },
     save() {
-      this.setValue(
-        '#/text',
-        this.dialog.data.text.value,
-        this.dialog.data.text.default
-      )
+      this.setValue('#/text', this.dialog.data.text.value, this.dialog.data.text.default)
       this.setValue(
         '#/options/class',
         this.arrayToString(this.dialog.data.class.value, ' '),
@@ -156,9 +173,7 @@ export default Vue.extend({
     getValue(pointer: string, defaultValue: any): any {
       const elValue = JsonPointer.get(this.value, pointer)
       // Default values are not set mostly in FormSchema, therefore in this case return defaultValue, otherwise the real value
-      return typeof elValue === 'undefined' || elValue === defaultValue
-        ? defaultValue
-        : elValue
+      return typeof elValue === 'undefined' || elValue === defaultValue ? defaultValue : elValue
     },
     setValue(pointer: string, value: any, defaultValue: any): void {
       const vjpPointer = pointer.replace('#/', '/')
