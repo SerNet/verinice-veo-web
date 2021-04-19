ed8dd68e2a17d3a6030c98e1bc642aec356f7b78
Davit Svanidze <davit@cpmsys.de>
2021-04-19 21:42:44 +0200

    [#156] Wizard tests: all cases covered

--- cypress/integration/ObjectSchemaWizard.spec.ts
+++ cypress/integration/ObjectSchemaWizard.spec.ts
@@ -5,16 +5,14 @@ import { getCurrentOS } from '../support/utils'
 describe('Objectschema Editor', () => {
   before(() => {
     cy.auth()
-    cy.intercept(
-      {
-        method: 'GET',
-        url: 'https://veo.develop.cpmsys.io/schemas/process?domains=GDPR%2CISO_27001'
-      },
-      req => {
-        req.reply({ fixture: 'objectschema/process.json' })
-      }
-    )
 
+    /**
+     * Navigate through Wizard to ObjectSchemaEditor
+     */
+    cy.visit('http://localhost:3000/editor')
+  })
+
+  beforeEach(() => {
     cy.intercept(
       {
         method: 'GET',
@@ -26,16 +24,10 @@ describe('Objectschema Editor', () => {
         })
       }
     )
-
-    /**
-     * Navigate through Wizard to ObjectSchemaEditor
-     */
-    cy.visit('http://localhost:3000/editor')
-  })
-
-  beforeEach(() => {
-    cy.contains('Objektschema Editor')
-      .closest('.v-list-item.v-list-item--link')
+    cy.window().then(function(win: any) {
+      win.$nuxt?.$router?.push('/editor')
+    })
+    cy.contains('.v-list-item--link', 'Objektschema Editor')
       .should('have.attr', 'href', '/editor/objectschema')
       .click()
       .wait(1)
@@ -51,37 +43,30 @@ describe('Objectschema Editor', () => {
         .find('.v-list-item.v-list-item--link')
         .should('contain.text', 'Objektschema erstellen')
         .should('contain.text', 'Objektschema importieren')
-
       cy.get('.v-window-item--active')
         .contains('Objektschema erstellen')
         .closest('.v-list-item--link')
         .click()
         .wait(1)
-
       cy.get('.v-window-item--active')
         .find('h2')
         .should('contain.text', 'Objektschema erstellen')
-
       cy.get('.v-card__actions')
         .contains('Zurück')
         .click()
         .wait(1)
-
       cy.get('.v-window-item--active')
         .contains('Objektschema importieren')
         .closest('.v-list-item--link')
         .click()
         .wait(1)
-
       cy.get('.v-window-item--active')
         .find('h2')
         .should('contain.text', 'Objektschema importieren')
-
       cy.get('.v-window-item--active')
         .contains('Stattdessen ein neues Objektschema erstellen')
         .click()
         .wait(1)
-
       cy.get('.v-window-item--active')
         .find('h2')
         .should('contain.text', 'Objektschema erstellen')
@@ -94,27 +79,23 @@ describe('Objectschema Editor', () => {
         .contains('Stattdessen ein neues Objektschema erstellen')
         .click()
         .wait(1)
-
       cy.get('.v-window-item--active')
         .contains('.v-text-field', 'Schema-Typ')
         .type('Test')
       cy.get('.v-window-item--active')
         .contains('.v-text-field', 'Beschreibung')
         .type('Test Beschreibung')
-
       cy.get('.v-card__actions')
         .contains('.v-btn', 'Weiter')
         .click()
         .wait(1)
     })
-
     cy.contains('.v-text-field', 'Objektschema')
       .find('input')
       .should('have.value', 'Test')
     cy.contains('.v-text-field', 'Beschreibung')
       .find('input')
       .should('have.value', 'Test Beschreibung')
-
     cy.get('.editor .cm-content').then(function(editor) {
       cy.wrap(getCurrentOS(editor)).then(currentOS => {
         cy.fixture('objectschema/empty.json').then(emptyOS => {
@@ -123,4 +104,356 @@ describe('Objectschema Editor', () => {
       })
     })
   })
+
+  it('imports own objectschema by uploading', function() {
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Eigenes{enter}')
+      cy.get('.v-window-item--active')
+        .contains('.v-file-input', 'Objektschema hochladen (.json)')
+        .find('input[type="file"]')
+        .attachFile('objectschema/process.json')
+        .wait(2000)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Test')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Test Beschreibung')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/empty.json').then(emptyOS => {
+          cy.wrap(JSON.stringify(emptyOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports own objectschema by inserting code', function() {
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Eigenes{enter}')
+      cy.get('.v-window-item--active')
+        .contains('.v-tab', 'Code einfügen')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .find('.editor .cm-content')
+        .closest('.d-flex.flex-column')
+        .then((el: any) => {
+          cy.fixture('objectschema/empty.json').then(emptyOS => {
+            // TODO: this is a hack to load OS in Code Editor. It needs a better solution
+            el[0].__vue__.$emit('input', JSON.stringify(emptyOS, null, 2))
+          })
+        })
+      cy.contains('.v-btn', 'Codeänderungen übernehmen')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Test')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Test Beschreibung')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/empty.json').then(emptyOS => {
+          cy.wrap(JSON.stringify(emptyOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing control objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/control*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/control.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Control{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Control')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Control')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/control.json').then(controlOS => {
+          cy.wrap(JSON.stringify(controlOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing scope objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/scope*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/scope.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Scope{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Scope')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Scope')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/scope.json').then(scopeOS => {
+          cy.wrap(JSON.stringify(scopeOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing asset objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/asset*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/asset.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Asset{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Asset')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Asset')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/asset.json').then(assetOS => {
+          cy.wrap(JSON.stringify(assetOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing process objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/process*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/process.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Process{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Process')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Process')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/proces.json').then(procesOS => {
+          cy.wrap(JSON.stringify(procesOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing incident objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/incident*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/incident.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Incident{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Incident')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Incident')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/incident.json').then(incidentOS => {
+          cy.wrap(JSON.stringify(incidentOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing document objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/document*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/document.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Document{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Document')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Document')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/document.json').then(documentOS => {
+          cy.wrap(JSON.stringify(documentOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it('imports existing person objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/person*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/person.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Person{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Person')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Person')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/person.json').then(personOS => {
+          cy.wrap(JSON.stringify(personOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+
+  it.only('imports existing scenario objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://veo.develop.cpmsys.io/schemas/scenario*'
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/scenario.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('.v-text-field', 'Schema-Typ')
+        .type('Scenario{enter}')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Weiter')
+        .click()
+        .wait(1)
+    })
+    cy.contains('.v-text-field', 'Objektschema')
+      .find('input')
+      .should('have.value', 'Scenario')
+    cy.contains('.v-text-field', 'Beschreibung')
+      .find('input')
+      .should('have.value', 'Schema for Scenario')
+    cy.get('.editor .cm-content').then(function(editor) {
+      cy.wrap(getCurrentOS(editor)).then(currentOS => {
+        cy.fixture('objectschema/scenario.json').then(scenarioOS => {
+          cy.wrap(JSON.stringify(scenarioOS, null, 2)).should('eq', JSON.stringify(currentOS, null, 2))
+        })
+      })
+    })
+  })
+  //
 })
