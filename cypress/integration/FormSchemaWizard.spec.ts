110a5b19232ab15e44182cd1737127169e559c07
Davit Svanidze <davit@cpmsys.de>
2021-04-23 11:25:26 +0200

    [#164] covered FSE wizard with tests

--- cypress/integration/FormSchemaWizard.spec.ts
+++ cypress/integration/FormSchemaWizard.spec.ts
@@ -139,7 +139,7 @@ describe('Formschema Wizard', () => {
     })
   })
 
-  it('creates a new formschema based on own objectschema by inserting code', function() {
+  it('creates a new formschema based on own objectschema by code insterting', function() {
     cy.get('.v-dialog--active').within(dialogEl => {
       cy.get('.v-window-item--active')
         .contains('Formschema erstellen')
@@ -822,6 +822,302 @@ describe('Formschema Wizard', () => {
                 return JSON.stringify(modifiedEmptyOS)
               })
               .should('eq', JSON.stringify(currentOS))
+          })
+        })
+      })
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Schließen')
+        .click()
+        .wait(1)
+    })
+  })
+
+  it('imports a formschema by upload based on process objectschema', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /.*\/schemas\/process.*/
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/process.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('Formschema importieren')
+        .closest('.v-list-item--link')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-file-input', 'Formschema hochladen (.json)')
+        .find('input[type="file"]')
+        .attachFile('formschema/empty-process.json')
+        .wait(2000)
+    })
+    cy.get('h1').should('contain.text', 'Formschema Editor- Test Formschema')
+    cy.get('.mdi-wrench')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.contains('.v-text-field', 'Name des Formschemas')
+        .find('input')
+        .should('have.value', 'Test Formschema')
+      cy.contains('.v-text-field', 'Sub Typ')
+        .find('input')
+        .should('have.value', 'TF')
+      cy.contains('.v-text-field.v-input--is-disabled', 'Objektschematyp')
+        .find('input')
+        .should('have.value', 'process')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Abbrechen')
+        .click()
+        .wait(1)
+    })
+    cy.get('.mdi-code-tags')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.editor .cm-content').then(function(editor) {
+        cy.wrap(getCurrentOS(editor)).then(currentOS => {
+          cy.fixture('formschema/empty-process.json').then(emptyProcessOS => {
+            cy.wrap(JSON.stringify(emptyProcessOS)).should('eq', JSON.stringify(currentOS))
+          })
+        })
+      })
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Schließen')
+        .click()
+        .wait(1)
+    })
+  })
+
+  it('imports a formschema by code inserting based on process objectschema ', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /.*\/schemas\/process.*/
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/process.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('Formschema importieren')
+        .closest('.v-list-item--link')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-tab', 'Code einfügen')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .find('.editor .cm-content')
+        .closest('.d-flex.flex-column')
+        .then((el: any) => {
+          cy.fixture('formschema/empty-process.json').then(emptyProcessOS => {
+            // TODO: this is a hack to load OS in Code Editor. It needs a better solution
+            el[0].__vue__.$emit('input', JSON.stringify(emptyProcessOS))
+          })
+        })
+      cy.contains('.v-btn', 'Codeänderungen übernehmen')
+        .click()
+        .wait(1)
+    })
+    cy.get('h1').should('contain.text', 'Formschema Editor- Test Formschema')
+    cy.get('.mdi-wrench')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.contains('.v-text-field', 'Name des Formschemas')
+        .find('input')
+        .should('have.value', 'Test Formschema')
+      cy.contains('.v-text-field', 'Sub Typ')
+        .find('input')
+        .should('have.value', 'TF')
+      cy.contains('.v-text-field.v-input--is-disabled', 'Objektschematyp')
+        .find('input')
+        .should('have.value', 'process')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Abbrechen')
+        .click()
+        .wait(1)
+    })
+    cy.get('.mdi-code-tags')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.editor .cm-content').then(function(editor) {
+        cy.wrap(getCurrentOS(editor)).then(currentOS => {
+          cy.fixture('formschema/empty-process.json').then(emptyProcessOS => {
+            cy.wrap(JSON.stringify(emptyProcessOS)).should('eq', JSON.stringify(currentOS))
+          })
+        })
+      })
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Schließen')
+        .click()
+        .wait(1)
+    })
+  })
+
+  it('imports a formschema by uploading based on process objectschema also manually uploaded', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /.*\/schemas\/process.*/
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/process.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('Formschema importieren')
+        .closest('.v-list-item--link')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-input--checkbox', 'Existierendes Objektschema selbst hochladen.')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-file-input', 'Formschema hochladen (.json)')
+        .find('input[type="file"]')
+        .attachFile('formschema/empty-process.json')
+        .wait(2000)
+      cy.get('.v-window-item--active')
+        .contains('.v-file-input', 'Objektschema hochladen (.json)')
+        .find('input[type="file"]')
+        .attachFile('objectschema/process.json')
+        .wait(2000)
+    })
+    cy.get('h1').should('contain.text', 'Formschema Editor- Test Formschema')
+    cy.get('.mdi-wrench')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.contains('.v-text-field', 'Name des Formschemas')
+        .find('input')
+        .should('have.value', 'Test Formschema')
+      cy.contains('.v-text-field', 'Sub Typ')
+        .find('input')
+        .should('have.value', 'TF')
+      cy.contains('.v-text-field.v-input--is-disabled', 'Objektschematyp')
+        .find('input')
+        .should('have.value', 'process')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Abbrechen')
+        .click()
+        .wait(1)
+    })
+    cy.get('.mdi-code-tags')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.editor .cm-content').then(function(editor) {
+        cy.wrap(getCurrentOS(editor)).then(currentOS => {
+          cy.fixture('formschema/empty-process.json').then(emptyProcessOS => {
+            cy.wrap(JSON.stringify(emptyProcessOS)).should('eq', JSON.stringify(currentOS))
+          })
+        })
+      })
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Schließen')
+        .click()
+        .wait(1)
+    })
+  })
+
+  it('imports a formschema by uploading based on process objectschema by code insterting ', function() {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /.*\/schemas\/process.*/
+      },
+      req => {
+        req.reply({
+          fixture: 'objectschema/process.json'
+        })
+      }
+    )
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.v-window-item--active')
+        .contains('Formschema importieren')
+        .closest('.v-list-item--link')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-input--checkbox', 'Existierendes Objektschema selbst hochladen.')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .contains('.v-file-input', 'Formschema hochladen (.json)')
+        .find('input[type="file"]')
+        .attachFile('formschema/empty-process.json')
+        .wait(2000)
+      cy.get('.v-window-item--active')
+        .contains('.v-tab', 'Code einfügen')
+        .click()
+        .wait(1)
+      cy.get('.v-window-item--active')
+        .find('.editor .cm-content')
+        .closest('.d-flex.flex-column')
+        .then((el: any) => {
+          cy.fixture('objectschema/process.json').then(processOS => {
+            // TODO: this is a hack to load OS in Code Editor. It needs a better solution
+            el[0].__vue__.$emit('input', JSON.stringify(processOS))
+          })
+        })
+      cy.contains('.v-btn', 'Codeänderungen übernehmen')
+        .click()
+        .wait(1)
+    })
+    cy.get('h1').should('contain.text', 'Formschema Editor- Test Formschema')
+    cy.get('.mdi-wrench')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.contains('.v-text-field', 'Name des Formschemas')
+        .find('input')
+        .should('have.value', 'Test Formschema')
+      cy.contains('.v-text-field', 'Sub Typ')
+        .find('input')
+        .should('have.value', 'TF')
+      cy.contains('.v-text-field.v-input--is-disabled', 'Objektschematyp')
+        .find('input')
+        .should('have.value', 'process')
+      cy.get('.v-card__actions')
+        .contains('.v-btn', 'Abbrechen')
+        .click()
+        .wait(1)
+    })
+    cy.get('.mdi-code-tags')
+      .closest('.v-btn')
+      .click()
+      .wait(1)
+
+    cy.get('.v-dialog--active').within(dialogEl => {
+      cy.get('.editor .cm-content').then(function(editor) {
+        cy.wrap(getCurrentOS(editor)).then(currentOS => {
+          cy.fixture('formschema/empty-process.json').then(emptyProcessOS => {
+            cy.wrap(JSON.stringify(emptyProcessOS)).should('eq', JSON.stringify(currentOS))
           })
         })
       })
