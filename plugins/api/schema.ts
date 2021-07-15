a23ec77de0f97f615a64e5772a5b91f598b8d53e
Jonas Heitmann <jonas@cpmsys.de>
2021-07-15 10:41:10 +0200

    [#214] WIP

--- plugins/api/schema.ts
+++ plugins/api/schema.ts
@@ -1,45 +1,28 @@
 import { Client } from '~/plugins/api';
 import { IVeoObjectSchema } from '~/types/VeoTypes';
 
-export interface ISchemaEndpoint {
+export interface IVeoEntityMetaInfo {
+  collectionUri: string;
+  searchUri: string;
+  schemaUri: string;
+}
+
+export interface IVeoEntitiesMetaInfo {
+  [key: string]: IVeoEntityMetaInfo;
+}
+
+export interface IVeoSchemaEndpoint {
   schemaName: string;
   endpoint: string;
 }
 
-// The key might be different to the title of the object schema defined in its title property. However this key
-// should be seen as a technical id and is ALWAYS lowercase
-export const endpoints = {
-  Asset: 'assets',
-  Control: 'controls',
-  Document: 'documents',
-  Incident: 'incidents',
-  Person: 'persons',
-  Process: 'processes',
-  Scenario: 'scenarios',
-  Scope: 'scopes'
-};
+let endpoints: IVeoSchemaEndpoint[];
 
 /**
  * Schemas in this array usually get handled differently than "normal" schemas. While we don't treat them differently in
  * this file, other files in the project might refer to this array
  */
 export const nonLinkableSchemas = ['scope'];
-
-export function getSchemaName(endpoint: string): string | undefined {
-  for (const key of Object.keys(endpoints)) {
-    // @ts-ignore
-    if (endpoints[key] === endpoint) {
-      return key;
-    }
-  }
-
-  return undefined;
-}
-
-export function getSchemaEndpoint(schemaName: string): string | undefined {
-  // @ts-ignore
-  return endpoints[schemaName];
-}
 
 export default function (api: Client) {
   return {
@@ -49,18 +32,21 @@ export default function (api: Client) {
      * NOT PAGINATED
      *
      */
-    async fetchAll(ignoreMissingEndpoints: boolean = false, params?: Record<string, string>): Promise<ISchemaEndpoint[]> {
-      const schemas: { knownSchemas: string[] } = await api.req('/api/schemas', {
-        params
-      });
-      console.log(schemas);
-      return schemas.knownSchemas
-        .map((schema) => ({
-          schemaName: schema,
-          // @ts-ignore
-          endpoint: endpoints[schema]
-        }))
-        .filter((entry) => ignoreMissingEndpoints || !!entry.endpoint);
+    async fetchAll(ignoreMissingEndpoints: boolean = false, params?: Record<string, string>): Promise<IVeoSchemaEndpoint[]> {
+      if (!endpoints) {
+        const schemas: IVeoEntitiesMetaInfo = await api.req('/api/types', {
+          params
+        });
+
+        const types = Object.keys(schemas);
+
+        endpoints = types.map((type: string) => ({
+          endpoint: schemas[type].schemaUri,
+          schemaName: type
+        }));
+      }
+
+      return endpoints.filter((entry) => ignoreMissingEndpoints || !!entry.endpoint);
     },
 
     /**
