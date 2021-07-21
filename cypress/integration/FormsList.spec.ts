d713e0fa10630dc50613b5894871284f19d8d97f
Jonas Heitmann <jonas@cpmsys.de>
2021-07-21 12:05:37 +0200

    [#242] Made existing tests more reliable

--- cypress/integration/FormsList.spec.ts
+++ cypress/integration/FormsList.spec.ts
@@ -2,15 +2,12 @@
 import { IVeoEntity } from '../../types/VeoTypes';
 
 describe('Forms List', () => {
-  before(() => {
-    cy.interceptLayoutCalls();
-  });
-
   beforeEach(() => {
     cy.auth();
 
     cy.interceptLayoutCalls();
     cy.visit(`/unit-d496f98f-c051-443c-9b1f-65d65b64996d/domains/domain-ed67e4d7-c657-4479-ba8a-c53999d2930a/forms/form-abbd6bf3-891c-4120-a3fd-bff06748b3ab/`);
+    cy.wait('@G_fetchObjects');
   });
 
   // Only checking for correct request, we expect the vuetify component to work correctly
@@ -22,24 +19,29 @@ describe('Forms List', () => {
 
     // Go to next page (from page 0 to page 1)
     cy.get('.v-data-footer__icons-after .mdi-chevron-right').click();
-    cy.get('@fetchObjects').its('request.url').should('contain', 'page=1');
+    cy.wait('@fetchObjects').its('request.url').should('contain', 'page=1');
 
     // Go to previous page (from page 1 to page 0)
     cy.get('.v-data-footer__icons-before .mdi-chevron-left').click();
-    cy.get('@fetchObjects').its('request.url').should('contain', 'page=0');
+    cy.wait('@fetchObjects').its('request.url').should('contain', 'page=0');
   });
 
   // Only checking for correct request, we expect the vuetify component to work correctly
   it('Changes page size of the table', function () {
     const newTablePageSize = 25; // Valid option defined in itemsPerPageOptions in VeoFormList.vue
 
+    cy.intercept({
+      method: 'GET',
+      url: /.*\/api\/(assets|controls|documents|incidents|persons|processes|scenarios|scopes)\?(.+)$/
+    }).as('fetchObjects');
+
     // Skip to next page
     cy.get('.v-data-footer__icons-after .mdi-chevron-right').click();
-    cy.wait('@G_fetchObjects');
+    cy.wait('@fetchObjects');
 
     // Change page size
     cy.get('[data-cy=veo-form-list-forms-table]').get('.v-data-footer .v-input').closest('.v-select').type(`${newTablePageSize}{enter}`);
-    cy.get('@G_fetchObjects').should((req: any) => {
+    cy.wait('@fetchObjects').should((req: any) => {
       expect(req.request.url).to.contain(`size=${newTablePageSize}`); // Expect page size to increase to previously defined value
       expect(req.request.url).to.contain('page=0'); // Expect page to jump back to first page
     });
@@ -48,18 +50,27 @@ describe('Forms List', () => {
 
   // Only checking for correct request, we expect the vuetify component to work correctly
   it('Tests the sorting for every sortable column', function () {
+    let descendingSortings = 0;
+    let ascendingSortings = 0;
+    let totalQueries = 0;
     const sortableColumns = ['designator', 'name', 'updatedAt', 'updatedBy']; // Array containing the property that should be sorted for (NOTE: Column in the VeoFormList.vue file might be named differently), so we make sure only valid properties are sorted
+
+    cy.intercept({
+      method: 'GET',
+      url: /.*\/api\/(assets|controls|documents|incidents|persons|processes|scenarios|scopes)\?(.+)$/
+    }).as('fetchObjects');
 
     // Test whether the page jumps back to one if the user changes the sort direction
     cy.get('.v-data-footer__icons-after .mdi-chevron-right').click();
-    cy.wait('@G_fetchObjects');
+    cy.wait('@fetchObjects');
     cy.get('[data-cy=veo-form-list-forms-table]').contains('Objektname').click();
-    cy.get('@G_fetchObjects').its('request.url').should('contain', 'page=0');
+    cy.wait('@fetchObjects').its('request.url').should('contain', 'page=0');
 
     // Iterate over every sortable table header and check if the correct params are sent
     cy.get('[data-cy=veo-form-list-forms-table] th.sortable').each(($element, _index, _$list) => {
       return new Cypress.Promise((resolve) => {
         // Sort asc and desc
+        // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
         for (const sortOrder of ['asc', 'desc']) {
           cy.wrap($element).click();
           cy.get('@G_fetchObjects').should((req: any) => {
@@ -67,9 +78,19 @@ describe('Forms List', () => {
             expect(req.request.url).to.contain(`sortBy=`);
             // eslint-disable-next-line no-unused-expressions
             expect(sortByPropertyExists).to.be.true; // Expect parameter to sort by to be part of sortableColumns
-            expect(req.request.url).to.contain(`sortOrder=${sortOrder}`);
+            if (req.request.url.includes('sortOrder=asc')) {
+              ascendingSortings++;
+            } else if (req.request.url.includes('sortOrder=desc')) {
+              descendingSortings++;
+            }
+            totalQueries++;
           });
         }
+
+        // eslint-disable-next-line no-unused-expressions
+        expect(descendingSortings === ascendingSortings).to.be.true;
+        // eslint-disable-next-line no-unused-expressions
+        expect(totalQueries === descendingSortings * 2).to.be.true;
         resolve();
       });
     });
