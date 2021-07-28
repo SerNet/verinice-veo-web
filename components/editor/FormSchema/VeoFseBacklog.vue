96edb049d68365e36b9c461f78a37d0e8b6bb482
Jonas Heitmann <jonas.heitmann97@gmail.com>
2021-07-28 11:09:12 +0200

    [PATCH] Added standard property status to fse backlog

--- components/editor/FormSchema/VeoFseBacklog.vue
+++ components/editor/FormSchema/VeoFseBacklog.vue
@@ -281,7 +281,7 @@ export default defineComponent<IProps>({
      * React to formschema or objectschema changes
      */
     const objectSchemaPropertiesPatterns = {
-      standard: ['#/properties/name', '#/properties/abbreviation', '#/properties/description'],
+      standard: ['#/properties/name', '#/properties/abbreviation', '#/properties/description', '#/properties/status'],
       regexAspectsAttributes: /^#\/properties\/customAspects\/properties\/\w+\/properties\/attributes\/properties\/\w+$/,
       regexLinks: /^#\/properties\/links\/properties\/\w+$/,
       regexLinksAttributes: /^#\/properties\/links\/properties\/\w+\/items\/properties\/attributes\/properties\/\w+$/
