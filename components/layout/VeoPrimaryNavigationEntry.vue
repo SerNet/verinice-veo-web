8dfe12eb13fb7829066cf4b51069da89a6a0db30
Annemarie Bufe <abufe@nbrx.de>
2021-09-16 15:07:19 +0200

    [#301] added name to header

--- components/layout/VeoPrimaryNavigationEntry.vue
+++ components/layout/VeoPrimaryNavigationEntry.vue
@@ -1,6 +1,6 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2021  Jonas Heitmann, Davit Svandize
+   - Copyright (C) 2021  Jonas Heitmann, Davit Svandize, Annemarie Bufe
    - 
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
