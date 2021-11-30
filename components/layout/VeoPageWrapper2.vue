4af0cc364a0414806367420b948d0571789cdce7
Jonas Heitmann <jonas@cpmsys.de>
2021-11-30 14:47:43 +0100

    [#319] WIP:  Adding collapse buttons to page wrapper

---
+++ components/layout/VeoPageWrapper2.vue
@@ -0,0 +1,121 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2021  Jonas Heitmann, Davit Svandize
+   - 
+   - This program is free software: you can redistribute it and/or modify
+   - it under the terms of the GNU Affero General Public License as published by
+   - the Free Software Foundation, either version 3 of the License, or
+   - (at your option) any later version.
+   - 
+   - This program is distributed in the hope that it will be useful,
+   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   - GNU Affero General Public License for more details.
+   - 
+   - You should have received a copy of the GNU Affero General Public License
+   - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<script lang="ts">
+import { defineComponent, h, ref, useContext, watchEffect } from '@nuxtjs/composition-api';
+
+import VeoCollapseButton from '~/components/layout/VeoCollapseButton.vue';
+
+interface IProps {
+  title: String;
+  titleClass: String;
+  collapsableLeft: Boolean;
+  collapsableRight: Boolean;
+}
+
+export default defineComponent<IProps>({
+  components: {
+    VeoCollapseButton
+  },
+  props: {
+    title: {
+      type: String,
+      default: undefined
+    },
+    titleClass: {
+      type: String,
+      default: undefined
+    },
+    collapsableLeft: {
+      type: Boolean,
+      default: false
+    },
+    collapsableRight: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, context) {
+    let wrapper;
+    const observer = new MutationObserver((value) => {
+      console.log('Bla', value);
+    });
+    console.log(wrapper);
+    // observer.observe(wrapper as any);
+    watchEffect(
+      () => {
+        console.log(wrapper); // => <div>This is a root element</div>
+      },
+      {
+        flush: 'post'
+      }
+    );
+
+    return () =>
+      h(
+        'div',
+        {
+          class: 'fill-width fill-height d-flex flex-column overflow-hidden'
+        },
+        [
+          h(
+            'div',
+            {
+              class: props.titleClass
+            },
+            [
+              ...(props.title
+                ? [
+                    h('h1', {
+                      domProps: {
+                        innerText: props.title
+                      },
+                      class: 'd-inline px-4 py-1 flex-grow-0'
+                    })
+                  ]
+                : [context.slots.title ? context.slots.title() : []]),
+              context.slots.header ? context.slots.header() : []
+            ]
+          ),
+          h(
+            'div',
+            {
+              props: {
+                noGutters: true
+              },
+              class: 'row flex-nowrap overflow-hidden',
+              ref: wrapper,
+              refInFor: true
+            },
+            (context.slots.default ? context.slots.default() : []).map((slotItem, index) =>
+              h(
+                'div',
+                {
+                  style: {
+                    background: 'red'
+                  }
+                },
+                [slotItem]
+              )
+            )
+          ),
+          context.slots.helper ? context.slots.helper() : []
+        ]
+      );
+  }
+});
+</script>
---
+++ pages/test2.vue
@@ -0,0 +1,50 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2021  Jonas Heitmann
+   - 
+   - This program is free software: you can redistribute it and/or modify
+   - it under the terms of the GNU Affero General Public License as published by
+   - the Free Software Foundation, either version 3 of the License, or
+   - (at your option) any later version.
+   - 
+   - This program is distributed in the hope that it will be useful,
+   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   - GNU Affero General Public License for more details.
+   - 
+   - You should have received a copy of the GNU Affero General Public License
+   - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<template>
+  <VeoPageWrapper2>
+    <template #title>
+      <h1>Mein Titel</h1>
+    </template>
+    <template #header>
+      <v-btn @click="pages++">Add page</v-btn>
+    </template>
+    <template #default>
+      <VeoPage
+        v-for="page in pages"
+        :key="page"
+      >
+        <h2>Meine {{ page }} Seite</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </VeoPage>
+    </template>
+  </VeoPageWrapper2>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    const pages = ref(3);
+
+    return {
+      pages
+    };
+  }
+});
+</script>
