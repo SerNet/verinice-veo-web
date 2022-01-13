7a10f9646c89950990a8ef6962091a872be2cac8
Jonas Heitmann <jonas.heitmann97@gmail.com>
2022-01-13 10:33:36 +0100

    [#346] Added name to license header

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2021  Davit Svandize, Jonas Heitmann
+   - Copyright (C) 2021  Davit Svandize, Jonas Heitmann, Tino Groteloh
    - 
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
