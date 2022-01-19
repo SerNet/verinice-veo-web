09b330f9b360b8b1b2fa348189b2d40e3dc65f39
Tino Groteloh <tgroteloh@nbrx.de>
2022-01-19 17:57:38 +0100

    [#371] added my name to files

--- middleware/authentication.ts
+++ middleware/authentication.ts
@@ -1,6 +1,6 @@
 /*
  * verinice.veo web
- * Copyright (C) 2021  Jonas Heitmann, Davit Svandize, Markus Werner
+ * Copyright (C) 2021  Jonas Heitmann, Davit Svandize, Markus Werner, Tino Groteloh
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
