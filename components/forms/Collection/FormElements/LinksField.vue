a3f52fd704808da180b54c3a251b3cf5aa4aeebf
Samuel Vitzthum <svitzthum@nbrx.de>
2022-01-20 15:15:29 +0100

    [#372] rendering title of custom links as field label

--- components/forms/Collection/FormElements/LinksField.vue
+++ components/forms/Collection/FormElements/LinksField.vue
@@ -1,17 +1,17 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2021  Davit Svandize, Jonas Heitmann
-   - 
+   - Copyright (C) 2021  Davit Svandize, Jonas Heitmann, Samuel Vitzthum
+   -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
    - the Free Software Foundation, either version 3 of the License, or
    - (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful,
    - but WITHOUT ANY WARRANTY; without even the implied warranty of
    - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    - GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
@@ -22,15 +22,6 @@
     :class="options && options.class"
     :style="options && options.style"
   >
-    <div class="d-flex">
-      <span
-        v-if="options && options.label"
-        class="subtitle-1 mb-2"
-      >
-        {{ options && options.label }}
-      </span>
-      <v-spacer />
-    </div>
     <v-list
       dense
       class="py-0"
