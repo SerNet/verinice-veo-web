eba42f0d9bb1e1c20f3deaba8e8a86d752808288
Jonas Heitmann <jonas.heitmann97@gmail.com>
2022-02-14 15:46:00 +0100

    [#411] Fixed unit tests

--- components/forms/VeoForm.vue
+++ components/forms/VeoForm.vue
@@ -161,7 +161,7 @@ export default Vue.extend({
                 const scope = `#/properties/domains/properties/${this.domainId}/properties/status`;
                 let elementSchema: any = cloneDeep(JsonPointer.get(this.schema, scope) || {});
                 elementSchema = this.addConditionalSchemaPropertiesToControlSchema(elementSchema, scope);
-                return elementSchema.enum.map(
+                return elementSchema?.enum?.map(
                   (status: string) => this.generalTranslation[`${this.schema.title}_${this.value.domains?.[this.domainId]?.subType}_status_${status}`] || status
                 );
               })()
