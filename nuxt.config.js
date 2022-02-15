d34c56990224f56c560d54fd7ba81c0ab868dc90
Markus Werner <mwerner@nbrx.de>
2022-02-15 18:00:59 +0100

    [#392] Externalize scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+import hash from 'hash-sum';
+
 export default {
   /**
    *
@@ -220,6 +224,25 @@ export default {
           }
         ]
       ]
+    }
+  },
+
+  hooks: {
+    /**
+     * Externalize inline scripts
+     */
+    'generate:before'(me) {
+      const _minifyHtml = me.minifyHtml;
+      me.minifyHtml = function () {
+        const html = _minifyHtml.apply(this, arguments);
+        return html.replace(/<script>(.*?)<\/script>/g, (_, code) => {
+          const h = hash(code);
+          const inlineFile = `${h}.js`;
+          const inlineFilePath = resolve(me.distNuxtPath, inlineFile);
+          writeFileSync(inlineFilePath, code);
+          return `<script src="${me.options.build.publicPath}${inlineFile}"></script>`;
+        });
+      };
     }
   }
 };
