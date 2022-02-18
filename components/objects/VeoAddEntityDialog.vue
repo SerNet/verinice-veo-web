6a17f479b1e6f9ab4e4c6e4ec91cceb52c607fd4
Jessica Lühnen <jluehnen@nbrx.de>
2022-02-18 08:36:20 +0100

    [#381] composition api

--- components/objects/VeoAddEntityDialog.vue
+++ components/objects/VeoAddEntityDialog.vue
@@ -19,14 +19,14 @@
   <VeoDialog
     v-model="dialog"
     large
-    :headline="$t('headline')"
+    :headline="t('headline')"
     :persistent="saving"
     :close-disabled="saving"
     fixed-header
     fixed-footer
   >
     <template #default>
-      {{ $t('add_subentities', { displayName: entityDisplayName }) }}
+      {{ t('add_subentities', { displayName: entityDisplayName }) }}
       <v-row no-gutters>
         <v-col
           cols="auto"
@@ -42,7 +42,7 @@
             style="border: 1px solid black"
             @click="filterDialogVisible = true"
           >
-            <v-icon>{{ mdiFilter }}</v-icon> {{ upperFirst($t('filter').toString()) }}
+            <v-icon>{{ mdiFilter }}</v-icon> {{ upperFirst(t('filter').toString()) }}
           </v-btn>
         </v-col>
         <v-col
@@ -64,7 +64,7 @@
       <VeoEntitySelectionList
         v-model="selectedItems"
         :items="entities"
-        :loading="$fetchState.pending || loading"
+        :loading="fetchState.pending || loading"
         @page-change="onPageChange"
       />
     </template>
@@ -75,7 +75,7 @@
         :disabled="saving"
         @click="$emit('input', false)"
       >
-        {{ $t('global.button.cancel') }}
+        {{ t('global.button.cancel') }}
       </v-btn>
       <v-spacer />
       <v-btn
@@ -85,7 +85,7 @@
         :disabled="saving"
         @click="addEntities"
       >
-        {{ $t('add') }}
+        {{ t('add') }}
       </v-btn>
       <VeoFilterDialog
         v-model="filterDialogVisible"
@@ -100,143 +100,204 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { defineComponent, useRoute, ref, computed, useContext, useFetch, useRouter, watch, PropOptions } from '@nuxtjs/composition-api';
 import { upperFirst } from 'lodash';
-import { Prop } from 'vue/types/options';
+import { useI18n } from 'nuxt-i18n-composable';
 import { mdiFilter } from '@mdi/js';
 import { getEntityDetailsFromLink, IBaseObject, separateUUIDParam } from '~/lib/utils';
-
 import { getSchemaEndpoint, getSchemaName, IVeoSchemaEndpoint } from '~/plugins/api/schema';
 import { IVeoEntity, IVeoFormSchemaMeta, IVeoLink, IVeoPaginatedResponse, IVeoTranslations } from '~/types/VeoTypes';
 
-export default Vue.extend({
+export default defineComponent({
+  name: 'VeoAddEntityDialog',
   props: {
     value: {
       type: Boolean,
       required: true
     },
     addType: {
-      type: String as Prop<'entity' | 'scope' | undefined>,
+      type: [String, undefined],
       default: undefined
-    },
+    } as PropOptions<'entity' | 'scope' | undefined>,
     editedEntity: {
-      type: Object as Prop<IVeoEntity | undefined>,
+      type: [Object, undefined],
       default: undefined
-    }
+    } as PropOptions<IVeoEntity | undefined>
   },
-  data() {
-    return {
-      filter: {} as IBaseObject,
-      selectedItems: [] as { id: string; type: string }[],
-      saving: false as boolean,
-      entities: {
-        items: [],
-        page: 1,
-        pageCount: 0,
-        totalItemCount: 0
-      } as IVeoPaginatedResponse<IVeoEntity[]>,
-      loading: false as boolean,
-      schemas: [] as IVeoSchemaEndpoint[],
-      formschemas: [] as IVeoFormSchemaMeta[],
-      filterDialogVisible: false,
-      filterKeys: ['objectType', 'subType', 'designator', 'name', 'status', 'description', 'updatedBy', 'notPartOfGroup', 'hasChildObjects', 'hasLinks'],
-      translations: {} as IVeoTranslations['lang'],
-      mdiFilter,
-      upperFirst
-    };
-  },
-  async fetch() {
-    this.schemas = await this.$api.schema.fetchAll();
-    this.formschemas = await this.$api.form.fetchAll(this.domainId);
-    this.translations = (await this.$api.translation.fetch(['de', 'en'])).lang;
-  },
-  computed: {
-    entityDisplayName(): string {
-      return this.editedEntity?.displayName || '';
-    },
-    allowedObjectTypes(): IVeoSchemaEndpoint[] {
-      let schemas = [...this.schemas];
-      // Filter out scopes if the user wants to add objects and the parent is a scope
-      if (this.editedEntity?.type === 'scope' && this.addType === 'entity') {
-        schemas = schemas.filter((item) => item.endpoint !== 'scopes');
-      }
-      // Only scope if the user wants to add scopes and the parent is a scope
-      if (this.editedEntity?.type === 'scope' && this.addType === 'scope') {
-        schemas = schemas.filter((item) => item.endpoint === 'scopes');
-      }
-      // Only own object type if the user wants to add objects and the parent is a object
-      if (this.editedEntity?.type !== 'scope' && this.addType === 'entity') {
-        schemas = schemas.filter((item) => item.schemaName === this.editedEntity?.type);
-      }
+  setup(props, { emit }) {
+    const route = useRoute();
+    const { t, locale } = useI18n();
+    const { $api, $config, $user } = useContext();
 
-      return schemas;
-    },
-    dialog: {
+    const filter = ref<IBaseObject>({});
+    const selectedItems = ref<{ id: string; type: string }[]>([]);
+    const saving = ref(false); // saving status for adding entities
+    const loading = ref(false); // loading status for fetch entities
+    const entities = ref<IVeoPaginatedResponse<IVeoEntity[]>>({ items: [], page: 1, pageCount: 0, totalItemCount: 0 });
+    const schemas = ref<IVeoSchemaEndpoint[]>([]);
+    const formschemas = ref<IVeoFormSchemaMeta[]>([]);
+    const filterDialogVisible = ref(false);
+    const translations = ref<IVeoTranslations['lang']>({});
+
+    const domainId = computed(() => separateUUIDParam(route.value.params.domain).id);
+
+    const { fetchState } = useFetch(async () => {
+      const [_schemas, _formschemas, _translations] = await Promise.all([$api.schema.fetchAll(), $api.form.fetchAll(domainId.value), $api.translation.fetch(['de', 'en'])]);
+      schemas.value = _schemas;
+      formschemas.value = _formschemas;
+      translations.value = _translations.lang;
+    });
+
+    const entityDisplayName = computed(() => props.editedEntity?.displayName || '');
+
+    // control own dialog
+    const dialog = computed({
       get(): boolean {
-        return this.value;
+        return props.value;
       },
-      set(newValue: boolean) {
-        // reset dialog items + filter before close
-        this.entities = {
+      set() {
+        entities.value = {
           items: [],
           page: 1,
           pageCount: 0,
           totalItemCount: 0
         };
-        this.$emit('input', newValue);
+        emit('input', false);
       }
-    },
-    domainId(): string {
-      return separateUUIDParam(this.$route.params.domain).id;
-    },
-    activeFilterKeys(): string[] {
-      return this.filterKeys.filter((k) => this.filter[k] !== undefined);
-    }
-  },
-  watch: {
-    value(newValue: boolean) {
-      if (newValue) {
-        this.filter = { objectType: this.allowedObjectTypes[0].schemaName };
-        this.fetchEntities({ page: 1, sortBy: 'name', sortDesc: false });
+    });
 
-        let presetEntities: IVeoLink[];
-        if (!this.editedEntity) {
-          presetEntities = [];
-        } else if (this.editedEntity.type === 'scope') {
-          presetEntities = this.editedEntity.members;
-        } else {
-          presetEntities = this.editedEntity.parts;
+    // on open dialog do ...
+    watch(
+      () => props.value,
+      (newValue: boolean) => {
+        if (newValue) {
+          filter.value = { objectType: allowedObjectTypes.value[0].schemaName };
+          fetchEntities({ page: 1, sortBy: 'name', sortDesc: false });
+
+          let presetEntities: IVeoLink[];
+          if (!props.editedEntity) {
+            presetEntities = [];
+          } else if (props.editedEntity.type === 'scope') {
+            presetEntities = props.editedEntity.members;
+          } else {
+            presetEntities = props.editedEntity.parts;
+          }
+
+          selectedItems.value = presetEntities.map((member) => {
+            const details = getEntityDetailsFromLink(member);
+            const id = details.id;
+            let type = details.type;
+            type = getSchemaName(schemas.value, type) || type;
+
+            return { id, type };
+          });
         }
-
-        this.selectedItems = presetEntities.map((member) => {
-          const details = getEntityDetailsFromLink(member);
-          const id = details.id;
-          let type = details.type;
-          type = getSchemaName(this.schemas, type) || type;
-
-          return { id, type };
-        });
       }
-    },
-    filter() {
-      if (this.filter.objectType) this.fetchEntities({ page: 1, sortBy: 'name', sortDesc: false });
-    }
-  },
-  methods: {
-    async addEntities() {
-      if (!this.editedEntity) {
+    );
+
+    /**
+     * filter stuff
+     */
+
+    // available & active filter options
+    const filterKeys = ['objectType', 'subType', 'designator', 'name', 'status', 'description', 'updatedBy', 'notPartOfGroup', 'hasChildObjects', 'hasLinks'];
+    const activeFilterKeys = computed(() => {
+      return filterKeys.filter((k) => filter.value[k] !== undefined);
+    });
+
+    // fetch new entities on filters' objectType change
+    watch(
+      () => filter.value,
+      () => {
+        if (filter.value.objectType) fetchEntities({ page: 1, sortBy: 'name', sortDesc: false });
+      }
+    );
+
+    // formatting filter chips and their translations
+    const formatLabel = (label: string) => {
+      return upperFirst(t(`objectlist.${label}`).toString());
+    };
+    const formatValue = (label: string, value?: string) => {
+      switch (label) {
+        // Uppercase object types
+        case 'objectType':
+          return upperFirst(value);
+        // Translate sub types
+        case 'subType':
+          return formschemas.value.find((formschema) => formschema.subType === value)?.name?.[locale] || value;
+        case 'status':
+          return translations.value[locale.value]?.[`${filter.value.objectType}_${filter.value.subType}_status_${value}`] || value;
+        default:
+          return value;
+      }
+    };
+
+    // remove one filter
+    const clearFilter = (key: string) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [key]: remove, ...rest } = filter.value;
+      filter.value = { ...rest };
+    };
+
+    // update filter options
+    const updateFilter = (newFilter: IBaseObject) => {
+      filter.value = { ...newFilter };
+    };
+
+    // get allowed filter-objectTypes for current parent and child type
+    const allowedObjectTypes = computed(() => {
+      let _schemas = [...schemas.value];
+      // Filter out scopes if the user wants to add objects and the parent is a scope
+      if (props.editedEntity?.type === 'scope' && props.addType === 'entity') {
+        _schemas = _schemas.filter((item) => item.endpoint !== 'scopes');
+      }
+      // Only scope if the user wants to add scopes and the parent is a scope
+      if (props.editedEntity?.type === 'scope' && props.addType === 'scope') {
+        _schemas = _schemas.filter((item) => item.endpoint === 'scopes');
+      }
+      // Only own object type if the user wants to add objects and the parent is a object
+      if (props.editedEntity?.type !== 'scope' && props.addType === 'entity') {
+        _schemas = _schemas.filter((item) => item.schemaName === props.editedEntity?.type);
+      }
+      return _schemas;
+    });
+
+    /**
+     * entity control
+     */
+
+    // fetch entities for table
+    const fetchEntities = async (options: { page: number; sortBy: string; sortDesc: boolean }) => {
+      loading.value = true;
+      entities.value = await $api.entity.fetchAll(filter.value.objectType, options.page, {
+        size: $user.tablePageSize,
+        sortBy: options.sortBy,
+        sortOrder: options.sortDesc ? 'desc' : 'asc',
+        ...(filter.value || {})
+      });
+      loading.value = false;
+    };
+
+    // refetch entities on page or sort changes (in VeoObjectTable)
+    const onPageChange = async (opts: { newPage: number; sortBy: string; sortDesc?: boolean }) => {
+      await fetchEntities({ page: opts.newPage, sortBy: opts.sortBy, sortDesc: !!opts.sortDesc });
+    };
+
+    // add child entities to parent
+    const addEntities = async () => {
+      if (!props.editedEntity) {
         return;
       }
 
-      this.saving = true;
-      const _editedEntity = await this.$api.entity.fetch(this.editedEntity.type, this.editedEntity.id);
+      saving.value = true;
+      const _editedEntity = await $api.entity.fetch(props.editedEntity.type, props.editedEntity.id);
 
-      const children = this.selectedItems.map((item) => {
+      const children = selectedItems.value.map((item) => {
         return {
-          targetUri: `${this.$config.apiUrl}/${getSchemaEndpoint(this.schemas, item.type) || item.type}/${item.id}`
+          targetUri: `${$config.apiUrl}/${getSchemaEndpoint(schemas.value, item.type) || item.type}/${item.id}`
         };
       });
-      if (this.editedEntity.type === 'scope') {
+      if (props.editedEntity.type === 'scope') {
         // @ts-ignore
         _editedEntity.members = children;
       } else {
@@ -244,57 +305,44 @@ export default Vue.extend({
         _editedEntity.parts = children;
       }
 
-      this.$api.entity
-        .update(this.editedEntity.type, this.editedEntity.id, _editedEntity)
+      $api.entity
+        .update(props.editedEntity.type, props.editedEntity.id, _editedEntity)
         .then(() => {
-          this.$emit('success');
+          emit('success');
         })
         .catch((error: any) => {
-          this.$emit('error', error);
+          emit('error', error);
         })
         .finally(() => {
-          this.saving = false;
+          saving.value = false;
         });
-    },
-    async fetchEntities(options: { page: number; sortBy: string; sortDesc: boolean }) {
-      this.loading = true;
-      this.entities = await this.$api.entity.fetchAll(this.filter.objectType, options.page, {
-        size: this.$user.tablePageSize,
-        sortBy: options.sortBy,
-        sortOrder: options.sortDesc ? 'desc' : 'asc',
-        ...(this.filter || {})
-      });
-      this.loading = false;
-    },
-    formatLabel(label: string) {
-      return upperFirst(this.$t(`objectlist.${label}`).toString());
-    },
-    formatValue(label: string, value?: string) {
-      switch (label) {
-        // Uppercase object types
-        case 'objectType':
-          return upperFirst(value);
-        // Translate sub types
-        case 'subType':
-          return this.formschemas.find((formschema) => formschema.subType === value)?.name?.[this.$i18n.locale] || value;
-        case 'status':
-          return this.translations[this.$i18n.locale]?.[`${this.filter.objectType}_${this.filter.subType}_status_${value}`] || value;
-        default:
-          return value;
-      }
-    },
-    clearFilter(key: string) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { [key]: remove, ...rest } = this.filter;
-      this.filter = { ...rest };
-    },
-    updateFilter(filter: IBaseObject) {
-      this.filter = { ...filter };
-    },
-    // refetch on page or sort changes (in VeoObjectTable)
-    async onPageChange(opts: { newPage: number; sortBy: string; sortDesc?: boolean }) {
-      await this.fetchEntities({ page: opts.newPage, sortBy: opts.sortBy, sortDesc: !!opts.sortDesc });
-    }
+    };
+
+    return {
+      dialog,
+      filter,
+      saving,
+      loading,
+      entities,
+      domainId,
+      fetchState,
+      selectedItems,
+      activeFilterKeys,
+      entityDisplayName,
+      allowedObjectTypes,
+      filterDialogVisible,
+
+      addEntities,
+      formatLabel,
+      formatValue,
+      clearFilter,
+      updateFilter,
+      onPageChange,
+
+      t,
+      upperFirst,
+      mdiFilter
+    };
   }
 });
 </script>
