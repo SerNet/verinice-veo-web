6da2b4642ce36c0b2f7330c2e2aa3c3b9f0cf762
Jonas Heitmann <jonas.heitmann97@gmail.com>
2022-08-24 10:57:49 +0200

    Added error message to debug file

--- pages/_unit/domains/_domain/objects/_entity.vue
+++ pages/_unit/domains/_domain/objects/_entity.vue
@@ -353,7 +353,7 @@ export default defineComponent({
             }
           });
         } else {
-          displayErrorMessage(errorText, e.message, { details: cloneDeep({ object: modifiedObject.value, objectSchema: objectSchema.value }) });
+          displayErrorMessage(errorText, e.message, { details: cloneDeep({ object: modifiedObject.value, objectSchema: objectSchema.value, error: e.message }) });
         }
       }
     }
