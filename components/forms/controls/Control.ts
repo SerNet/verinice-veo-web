1718b69f258a4af81f43c4024ddb0f5b1947de8f
Jonas Heitmann <jonas.heitmann97@gmail.com>
2022-10-20 13:07:45 +0200

    Fixed reactivity issue with form controls

--- components/forms/controls/Control.ts
+++ components/forms/controls/Control.ts
@@ -61,37 +61,39 @@ export default defineComponent({
 
     const { locale } = useI18n();
 
-    const controls = AVAILABLE_CONTROLS.flatMap((control) => {
-      const definition = control.CONTROL_DEFINITION;
-      if (definition) {
-        if (process.dev && props.debug) {
-          // eslint-disable-next-line no-console
-          console.log(`VeoForm::Control: Checking whether ${definition.name[locale.value] || definition.name[0]} meets all conditions...`);
-        }
-        const evaluatedConditions: boolean[] = definition.conditions?.(props) || [];
-        const truthyConditions = evaluatedConditions.filter((condition) => condition).length;
+    const controls = computed(() =>
+      AVAILABLE_CONTROLS.flatMap((control) => {
+        const definition = control.CONTROL_DEFINITION;
+        if (definition) {
+          if (process.dev && props.debug) {
+            // eslint-disable-next-line no-console
+            console.log(`VeoForm::Control: Checking whether ${definition.name[locale.value] || definition.name[0]} meets all conditions...`);
+          }
+          const evaluatedConditions: boolean[] = definition.conditions?.(props) || [];
+          const truthyConditions = evaluatedConditions.filter((condition) => condition).length;
 
-        if (process.dev && props.debug) {
-          for (let j = 0; j < evaluatedConditions.length; j++) {
-            if (evaluatedConditions[j]) {
-              // eslint-disable-next-line no-console
-              console.log(`VeoForm::Control: Condition ${j} is met`);
-            } else {
-              // eslint-disable-next-line no-console
-              console.log(`VeoForm::Control: Condition ${j} is NOT met`);
+          if (process.dev && props.debug) {
+            for (let j = 0; j < evaluatedConditions.length; j++) {
+              if (evaluatedConditions[j]) {
+                // eslint-disable-next-line no-console
+                console.log(`VeoForm::Control: Condition ${j} is met`);
+              } else {
+                // eslint-disable-next-line no-console
+                console.log(`VeoForm::Control: Condition ${j} is NOT met`);
+              }
             }
           }
-        }
 
-        if (evaluatedConditions.length === truthyConditions) {
-          return [{ control, truthyConditions }];
+          if (evaluatedConditions.length === truthyConditions) {
+            return [{ control, truthyConditions }];
+          }
         }
-      }
-      return [];
-    });
+        return [];
+      })
+    );
 
     if (process.dev && props.debug) {
-      for (const control of controls) {
+      for (const control of controls.value) {
         // eslint-disable-next-line no-console
         console.log(
           `Control ${control.control.CONTROL_DEFINITION.name[locale.value] || control.control.CONTROL_DEFINITION.name[0]} has ${control.truthyConditions} truthy conditions`
@@ -117,7 +119,7 @@ export default defineComponent({
     }));
 
     return () =>
-      h(maxBy(controls, 'truthyConditions')?.control.default, {
+      h(maxBy(controls.value, 'truthyConditions')?.control.default, {
         props: _props.value,
         key: elementKey.value,
         scopedSlots: {
