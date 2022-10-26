74e0d3125ca3c6af99f2f3be1d090f67e54d381b
Jonas Heitmann <jonas.heitmann97@gmail.com>
2022-10-26 18:07:30 +0200

    Fixed build

--- plugins/api/index.ts
+++ plugins/api/index.ts
@@ -140,7 +140,7 @@ export class Client {
       headers: {
         Accept: 'application/json',
         Authorization: 'Bearer ' + this._user.token.value,
-        'Accept-Language': this.context.app.i18n.locale
+        'Accept-Language': this._context.i18n.locale
       } as Record<string, string>,
       method: 'GET',
       mode: 'cors'
