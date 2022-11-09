e833d90cada854fef1e518b881188f2a1dd3c4f3
Jonas Heitmann <jheitmann@nbrx.de>
2022-11-09 09:55:46 +0100

    Fixed extracted schema specific translations

--- pages/_unit/domains/_domain/editor/objectschema.vue
+++ pages/_unit/domains/_domain/editor/objectschema.vue
@@ -362,7 +362,7 @@ export default defineComponent({
 
     const schemaSpecificTranslations = computed<IVeoTranslations['lang']>(() => {
       const translationsToReturn: IVeoTranslations['lang'] = {};
-      const schemaTitle = objectSchemaHelper.value?.getTitle() || '';
+      const schemaTitle = `${objectSchemaHelper.value?.getTitle()}_` || '';
 
       for (const language of availableLanguages.value) {
         translationsToReturn[language] = pickBy(translations[language], (_value, key) => key.startsWith(schemaTitle));
