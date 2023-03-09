8c582d324c2be06c8ba14bf71d795e56e7971074
Jonas Heitmann <jheitmann@nbrx.de>
2023-03-09 08:09:13 +0100

    Updated dev requests

--- plugins/6-dev.client.ts
+++ plugins/6-dev.client.ts
@@ -15,44 +15,18 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { useQuerySync } from "~~/composables/api/utils/query";
-import accounts from '~~/composables/api/queryDefinitions/accounts';
-import catalogs from '~~/composables/api/queryDefinitions/catalogs';
-import domains from '~~/composables/api/queryDefinitions/domains';
-import forms from '~~/composables/api/queryDefinitions/forms';
-import history from '~~/composables/api/queryDefinitions/history';
-import monitoring from '~~/composables/api/queryDefinitions/monitoring';
-import objects from '~~/composables/api/queryDefinitions/objects';
-import reports from '~~/composables/api/queryDefinitions/reports';
-import schemas from '~~/composables/api/queryDefinitions/schemas';
-import translations from '~~/composables/api/queryDefinitions/translations';
-import units from '~~/composables/api/queryDefinitions/units';
+import { useRequest } from "~~/composables/api/utils/request";
 
 /**
- * This plugin allows developers to use the request functions of the frontend in the console, enabling them to develop e2e tests easier
+ * This plugin allows developers to use the request function of the frontend in the console, enabling them to develop e2e tests easier
+ * Example: await request('/api/units/1234-abcde-...', { method: DELETE })
  */
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
+  const { request } = useRequest();
 
   if(config.public.debug) {
-    const api = {
-      request: useQuerySync,
-      queryDefinitions: {
-        accounts,
-        catalogs,
-        domains,
-        forms,
-        history,
-        monitoring,
-        objects,
-        reports,
-        schemas,
-        translations,
-        units
-      }
-    };
-        
-    // @ts-ignore We expose the api to the window for developement purposes, so the integration-test-team can script without having to care about auth tokens and so on.
-    window.api = api;
+    // @ts-ignore We expose the request function to the window for developement purposes, so the integration-test-team can script without having to care about auth tokens and so on.
+    window.request = request;
   }
 });
