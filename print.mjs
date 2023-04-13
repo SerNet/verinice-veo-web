cb16f3206f31a2d258b8b3a86ec2686da846bdd5
Jonas Heitmann <jheitmann@nbrx.de>
2023-04-13 12:48:30 +0200

    Trying setting language via cookie

--- print.mjs
+++ print.mjs
@@ -18,6 +18,7 @@
 
 import path from 'path';
 import puppeteer from 'puppeteer';
+import Cookies from 'js-cookie';
 
 const LANGS = ['de', 'en'];
 
@@ -38,6 +39,7 @@ async function main() {
     .on('requestfailed', (request) => console.error(` ❌  ${request.failure().errorText} ${request.url()}`));
   console.log('Starting printing...');
   for await (const lang of LANGS) {
+    Cookies.set('i18n_redirected', lang);
     const outputFile = `${outputFolder}/${fileName}_${lang}.pdf`;
     console.log(`Printing: ${url} (${lang})...`);
     await page.goto(url + `&lang=${lang}`);
