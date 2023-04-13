d0d1d3c94fc0c888fc50bc5de917d3a9888a3720
Jonas Heitmann <jheitmann@nbrx.de>
2023-04-13 12:28:19 +0200

    More debugging

--- print.mjs
+++ print.mjs
@@ -38,6 +38,7 @@ async function main() {
     .on('requestfailed', (request) => console.error(` ❌  ${request.failure().errorText} ${request.url()}`));
   console.log('Starting printing...');
   for await (const lang of LANGS) {
+    console.log('docLang', lang, 'start');
     const outputFile = `${outputFolder}/${fileName}_${lang}.pdf`;
     console.log(`Printing: ${url} (${lang})...`);
     await page.goto(url + `&lang=${lang}`);
