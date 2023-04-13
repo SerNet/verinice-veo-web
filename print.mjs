ce755ab6e79bc7d5d98ff8ad03aa9eb062cc7f43
Jonas Heitmann <jheitmann@nbrx.de>
2023-04-13 13:18:11 +0200

    Fixed puppeteer setCookie call

--- print.mjs
+++ print.mjs
@@ -38,12 +38,10 @@ async function main() {
     .on('requestfailed', (request) => console.error(` ❌  ${request.failure().errorText} ${request.url()}`));
   console.log('Starting printing...');
   for await (const lang of LANGS) {
-    await page.setCookie([
-      {
-        name: 'i18n_redirected',
-        value: lang
-      }
-    ]);
+    await page.setCookie({
+      name: 'i18n_redirected',
+      value: lang
+    });
     const outputFile = `${outputFolder}/${fileName}_${lang}.pdf`;
     console.log(`Printing: ${url} (${lang})...`);
     await page.goto(url + `&lang=${lang}`);
