42bf5642d1f06b6ddefeddcfb770351630a1b1d1
jae <jae@sernet.de>
2023-05-09 10:20:51 +0200

    Write tests for chunkHistory()

---
+++ components/userData/__test__/data/historyItems.ts
@@ -0,0 +1,304 @@
+/*
+ * verinice.veo web
+ * Copyright (C) 2023  jae
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+export const historyItems = 
+[
+  {
+    "id": "a426677b-bf38-4965-9f0b-5a2b0060ac16",
+    "uri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/cd512ea7-01c8-4b22-b0cd-d3bc8b0f9d64",
+    "changeNumber": 1,
+    "type": "MODIFICATION",
+    "time": "2022-01-21T15:06:32.011530Z",
+    "author": "stce",
+    "content": {
+      "id": "cd512ea7-01c8-4b22-b0cd-d3bc8b0f9d64",
+      "_self": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/cd512ea7-01c8-4b22-b0cd-d3bc8b0f9d64",
+      "catalog": {
+        "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6",
+        "displayName": "DS-GVO-Controls"
+      },
+      "element": {
+        "targetUri": "/controls/39422d24-8456-4693-a109-1cfdb08a9220",
+        "searchesUri": "/controls/searches",
+        "resourcesUri": "/controls{?unit,displayName,subType,status,description,designator,name,updatedBy,size,page,sortBy,sortOrder}"
+      },
+      "createdAt": "2022-01-21T15:06:31.763513Z",
+      "createdBy": "stce",
+      "namespace": "TOM.TOM-P",
+      "updatedAt": "2022-01-21T15:06:32.010685Z",
+      "updatedBy": "stce",
+      "tailoringReferences": [
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.764202Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.764202Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/cd512ea7-01c8-4b22-b0cd-d3bc8b0f9d64"
+          },
+          "referenceType": "LINK_EXTERNAL"
+        }
+      ]
+    }
+  },
+  {
+    "id": "5925dd2a-6345-4416-8dd4-e7b990e9d887",
+    "uri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/fcc9f19d-4148-4f04-bdc7-b3df92386d35",
+    "changeNumber": 1,
+    "type": "MODIFICATION",
+    "time": "2022-01-21T15:06:31.943648Z",
+    "author": "stce",
+    "content": {
+      "id": "fcc9f19d-4148-4f04-bdc7-b3df92386d35",
+      "_self": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/fcc9f19d-4148-4f04-bdc7-b3df92386d35",
+      "catalog": {
+        "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6",
+        "displayName": "DS-GVO-Controls"
+      },
+      "element": {
+        "targetUri": "/controls/640f83b6-b60c-458b-a2e0-fd5b09f0eca5",
+        "searchesUri": "/controls/searches",
+        "resourcesUri": "/controls{?unit,displayName,subType,status,description,designator,name,updatedBy,size,page,sortBy,sortOrder}"
+      },
+      "createdAt": "2022-01-21T15:06:31.696299Z",
+      "createdBy": "stce",
+      "namespace": "TOM.TOM-REC",
+      "updatedAt": "2022-01-21T15:06:31.893550Z",
+      "updatedBy": "stce",
+      "tailoringReferences": [
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.747633Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.747633Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/fcc9f19d-4148-4f04-bdc7-b3df92386d35"
+          },
+          "referenceType": "LINK_EXTERNAL"
+        }
+      ]
+    }
+  },
+  {
+    "id": "6a06dd9f-efa5-46f0-a201-063e9f1bf998",
+    "uri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/dde38594-a14a-4791-94f1-71ff1455fb69",
+    "changeNumber": 1,
+    "type": "MODIFICATION",
+    "time": "2022-01-21T15:06:32.017522Z",
+    "author": "stce",
+    "content": {
+      "id": "dde38594-a14a-4791-94f1-71ff1455fb69",
+      "_self": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/dde38594-a14a-4791-94f1-71ff1455fb69",
+      "catalog": {
+        "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6",
+        "displayName": "DS-GVO-Controls"
+      },
+      "element": {
+        "targetUri": "/controls/c7dabb1f-7e14-4386-b127-a51ecb369bef",
+        "searchesUri": "/controls/searches",
+        "resourcesUri": "/controls{?unit,displayName,subType,status,description,designator,name,updatedBy,size,page,sortBy,sortOrder}"
+      },
+      "createdAt": "2022-01-21T15:06:31.767124Z",
+      "createdBy": "stce",
+      "namespace": "TOM.TOM-A",
+      "updatedAt": "2022-01-21T15:06:32.015044Z",
+      "updatedBy": "stce",
+      "tailoringReferences": [
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.767709Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.767709Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/dde38594-a14a-4791-94f1-71ff1455fb69"
+          },
+          "referenceType": "LINK_EXTERNAL"
+        }
+      ]
+    }
+  },
+  {
+    "id": "bbec0344-2793-4364-a21f-1f79cfff2167",
+    "uri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/78023d75-ab40-40aa-88d9-2c4db5c10ef1",
+    "changeNumber": 1,
+    "type": "MODIFICATION",
+    "time": "2022-01-21T15:06:32.021318Z",
+    "author": "stce",
+    "content": {
+      "id": "78023d75-ab40-40aa-88d9-2c4db5c10ef1",
+      "_self": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/78023d75-ab40-40aa-88d9-2c4db5c10ef1",
+      "catalog": {
+        "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6",
+        "displayName": "DS-GVO-Controls"
+      },
+      "element": {
+        "targetUri": "/controls/4086fbb0-8d0a-40fd-9937-0e2440359599",
+        "searchesUri": "/controls/searches",
+        "resourcesUri": "/controls{?unit,displayName,subType,status,description,designator,name,updatedBy,size,page,sortBy,sortOrder}"
+      },
+      "createdAt": "2022-01-21T15:06:31.770702Z",
+      "createdBy": "stce",
+      "namespace": "TOM.TOM-EFF",
+      "updatedAt": "2022-01-21T15:06:32.020727Z",
+      "updatedBy": "stce",
+      "tailoringReferences": [
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.771609Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.771609Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/78023d75-ab40-40aa-88d9-2c4db5c10ef1"
+          },
+          "referenceType": "LINK_EXTERNAL"
+        }
+      ]
+    }
+  },
+  {
+    "id": "bce1f24b-2fbd-4484-b68c-313dbf77542a",
+    "uri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/192972c2-d41e-4196-b32d-e6681ba06b74",
+    "changeNumber": 1,
+    "type": "MODIFICATION",
+    "time": "2022-01-21T15:06:32.028320Z",
+    "author": "stce",
+    "content": {
+      "id": "192972c2-d41e-4196-b32d-e6681ba06b74",
+      "_self": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/192972c2-d41e-4196-b32d-e6681ba06b74",
+      "catalog": {
+        "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6",
+        "displayName": "DS-GVO-Controls"
+      },
+      "element": {
+        "targetUri": "/processes/69905992-5ded-41ae-9f25-c901be26f72e",
+        "searchesUri": "/processes/searches",
+        "resourcesUri": "/processes{?unit,displayName,subType,status,description,designator,name,updatedBy,size,page,sortBy,sortOrder}"
+      },
+      "createdAt": "2022-01-21T15:06:31.775182Z",
+      "createdBy": "stce",
+      "namespace": "TOM.VVT",
+      "updatedAt": "2022-01-21T15:06:32.027700Z",
+      "updatedBy": "stce",
+      "tailoringReferences": [
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.776977Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.776977Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/fcc9f19d-4148-4f04-bdc7-b3df92386d35"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.778712Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.778712Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/7548806e-9d11-4306-a2b7-ca2e71c25a1d"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.777692Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.777692Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/78023d75-ab40-40aa-88d9-2c4db5c10ef1"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.780267Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.780267Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/dde38594-a14a-4791-94f1-71ff1455fb69"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.781605Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.781605Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/a8d2fd2b-c218-432d-95d4-6c33a0f04d7a"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.779467Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.779467Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/5ec3bc62-fea4-4618-bed6-6cd974d7ecef"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.776196Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.776196Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/cd512ea7-01c8-4b22-b0cd-d3bc8b0f9d64"
+          },
+          "referenceType": "LINK"
+        },
+        {
+          "linkType": "process_tom",
+          "createdAt": "2022-01-21T15:06:31.780918Z",
+          "createdBy": "stce",
+          "updatedAt": "2022-01-21T15:06:31.780918Z",
+          "updatedBy": "stce",
+          "attributes": {},
+          "catalogItem": {
+            "targetUri": "/catalogs/b346fd1f-2d62-4d4b-b1d4-ba98a782f2b6/items/5c77eeab-0415-498e-b23b-4ea454308839"
+          },
+          "referenceType": "LINK"
+        }
+      ]
+    }
+  }
+];
