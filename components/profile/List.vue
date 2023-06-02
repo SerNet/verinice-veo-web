9d2f33866180ff6785050461c5bb0308b2b6dd51
jae <jae@sernet.de>
2023-06-02 10:31:43 +0200

    Clean up + add a workaround to only select one item in `v-data-table`

---
+++ components/profile/List.vue
@@ -0,0 +1,97 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2023 jae
+   -
+   - This program is free software: you can redistribute it and/or modify
+   - it under the terms of the GNU Affero General Public License as published by
+   - the Free Software Foundation, either version 3 of the License, or
+   - (at your option) any later version.
+   -
+   - This program is distributed in the hope that it will be useful,
+   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   - GNU Affero General Public License for more details.
+   -
+   - You should have received a copy of the GNU Affero General Public License
+   - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+
+<template>
+  <BasePage :title="$t('breadcrumbs.profiles')">
+    <template #header>
+      <p class="mt-4 text-body-1">
+        {{ t('hint') }}
+      </p>
+    </template>
+    <template #default>
+      <BaseCard id="profileStyleScope">
+        <v-data-table
+          :headers="headers"
+          :items="testprofiles"
+          item-value="name"
+          items-per-page="5"
+          show-select
+          select-strategy="single"
+          :model-value="profileTable.selectedProfiles"
+          @update:model-value="newVal => selectNewItem(newVal)"
+        />
+      </BaseCard>
+
+      <!-- Table actions -->
+      <v-row
+        dense
+        class="mt-4"
+      >
+        <v-spacer />
+        <v-col
+          cols="auto"
+          class="justify-end"
+        >
+          <v-btn
+            flat
+            color="primary"
+            :disabled="!profileTable.selectedProfiles[0]"
+            @click="toggleDialog"
+          >
+            {{ t('applyBtn') }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </template>
+  </BasePage>
+</template>
+<script setup lang="ts">
+import { useProfiles, useUnits }  from './profiles.ts';
+const { profileTable, profiles, testprofiles } = useProfiles();
+const { dialog, toggleDialog } = useUnits();
+const { t } = useI18n();
+
+// Table setup
+const headers = [
+  { title: t('thName'), align: 'start', key: 'key'},
+  { title: t('thDescription'), align: 'start', key: 'desc', sortable: false }
+];
+
+/*******************
+* THIS IS A WORK ARROUND (JS + CSS):
+* In this table we only want to be able to select one item (profile) at a time.
+* At the time of writing, vuetify 3 v-data-table's `select-strategy='single'` did not work as expected (still part of vuetify LAB).
+* Once it does, TODO:
+* => on the component: use `v-model` instead of `model-value` + `@update`
+* => remove CSS (#profileStyleScope ...)
+* => remove this code:
+*******************/
+function selectNewItem(val) {
+  if (val.length >= 2) val.shift();
+  profileTable.selectedProfiles = val;
+}
+</script>
+
+<style>
+/* Do not show a checkbox in table header */
+#profileStyleScope th .v-selection-control__wrapper {
+  display: none;
+}
+</style>
+<i18n src="./messages.json"></i18n>
