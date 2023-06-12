17380aa1f2428278fe7a9193703148bf27118a5f
jae <jae@sernet.de>
2023-06-12 09:15:07 +0200

    Remove demo unit button

--- components/layout/PrimaryNavigation.vue
+++ components/layout/PrimaryNavigation.vue
@@ -54,15 +54,6 @@
             @expand-menu="miniVariant = false"
           />
         </template>
-        <template v-if="authenticated">
-          <v-divider class="mb-2" />
-          <div class="mx-2">
-            <LayoutDemoUnitButton
-              :icon-only="miniVariant"
-              :open-in-new-tab="route.path.startsWith('/docs')"
-            />
-          </div>
-        </template>
       </v-list>
     </template>
     <template #append>
