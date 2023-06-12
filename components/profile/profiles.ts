78cd50f295b33f36864aab97d32e568c01b169a0
jae <jae@sernet.de>
2023-06-12 08:37:33 +0200

    Remove test data

--- components/profile/profiles.ts
+++ components/profile/profiles.ts
@@ -1,3 +1,20 @@
+/*
+ * verinice.veo web
+ * Copyright (C) 2023 jae
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import { useQuery } from '~~/composables/api/utils/query';
 import domainQueryDefinitions from '~~/composables/api/queryDefinitions/domains';
 import unitQueryDefinitions from '~/composables/api/queryDefinitions/units';
@@ -5,29 +22,6 @@ import { separateUUIDParam } from '~/lib/utils';
 
 const route = useRoute();
 const { displayErrorMessage, displaySuccessMessage } = useVeoAlerts();
-
-const testprofiles = [
-  { abbr: '', key: 'Profile a', desc: ''},
-  { abbr: '', key: 'Profile b', desc: ''},
-  { abbr: '', key: 'Profile c', desc: ''},
-  { abbr: '', key: 'Profile d', desc: ''},
-  { abbr: '', key: 'Profile e', desc: ''},
-  { abbr: '', key: 'Profile f', desc: ''},
-  { abbr: '', key: 'Profile g', desc: ''},
-  { abbr: '', key: 'Profile h', desc: ''},
-  { abbr: '', key: 'Profile i', desc: ''},
-  { abbr: '', key: 'Profile j', desc: ''},
-  { abbr: '', key: 'Profile k', desc: ''},
-  { abbr: '', key: 'Profile l', desc: ''},
-  { abbr: '', key: 'Profile m', desc: ''},
-  { abbr: '', key: 'Profile n', desc: ''},
-  { abbr: '', key: 'Profile o', desc: ''},
-  { abbr: '', key: 'Profile p', desc: ''},
-  { abbr: '', key: 'Profile q', desc: ''},
-  { abbr: '', key: 'Profile r', desc: ''},
-  { abbr: '', key: 'Profile s', desc: ''},
-  { abbr: '', key: 'Profile t', desc: ''}
-];
 
 type Profile = {
   key: string;
