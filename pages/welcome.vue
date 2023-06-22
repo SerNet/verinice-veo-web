baa436ac1240cf743dad48dfb3fe2ba7c1999dcf
Frank Schneider <fs@sernet.de>
2023-06-22 12:18:28 +0200

    style: improve UI / UX stuff

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -94,9 +94,12 @@
           </i18n-t>
         </v-card-text>
 
-        <v-layout class="mb-4 justify-center">
+        <v-layout
+          class="mb-4 justify-center"
+        >
           <v-btn
             color="primary"
+            :disabled="maxUnitsExceeded"
             elevation="2"
             :prepend-icon="mdiCableData"
             size="large"
@@ -106,6 +109,28 @@
             {{ t('sampleDataButtonLabel') }}
           </v-btn>
         </v-layout>
+        <v-progress-linear
+          v-if="isLoading"
+          class="my-4"
+          color="primary"
+          height="32"
+          indeterminate
+          striped
+        >
+          <span style="color: white;">{{ t('applyProfile').toUpperCase() }}</span>
+        </v-progress-linear>
+
+        <BaseAlert
+          v-if="maxUnitsExceeded && !isLoading"
+          :model-value="true"
+          :type="VeoAlertType.INFO"
+          :buttons="[{text: t('goto'), onClick: () => navigateTo('/')}]"
+          class="ma-4"
+          flat
+          no-close-button
+        >
+          {{ t('requestForDeletion') }}
+        </BaseAlert>
 
         <v-divider />
 
@@ -198,10 +223,15 @@ import { LOCAL_STORAGE_KEYS } from '~/types/localStorage';
 
 import domainQueryDefinitions from '~/composables/api/queryDefinitions/domains';
 import unitQueryDefinitions from '~/composables/api/queryDefinitions/units';
+
 import { getFirstDomainDomaindId } from '~/lib/utils';
 import { useQueryClient } from '@tanstack/vue-query';
 import { useQuery, useQuerySync } from '~~/composables/api/utils/query';
 import { useMutation } from '~~/composables/api/utils/mutation';
+
+import { VeoAlertType } from '~/types/VeoTypes';
+import { useVeoUser } from '~/composables/VeoUser';
+
 
 const { t } = useI18n();
 
@@ -210,6 +240,8 @@ const queryClient = useQueryClient();
 
 const { createLink } = useCreateLink();
 const { displayErrorMessage} = useVeoAlerts();
+
+const { userSettings } = useVeoUser();
 
 const links = ref({
   forum: 'https://forum.verinice.com',
@@ -225,17 +257,20 @@ const showAtStartup = computed({
 });
 
 // *********************************************************************************
-const { mutateAsync: create, data: unitPropsPayload } = useMutation(unitQueryDefinitions.mutations.create);
-const { mutateAsync: apply } = useMutation(domainQueryDefinitions.mutations.applyProfile);
+const { data: units } = useQuery(unitQueryDefinitions.queries.fetchAll);
+const maxUnitsExceeded = computed(() => (units.value?.length || 0) >= userSettings.value.maxUnits);
 
-const { data: _domains } = useQuery(domainQueryDefinitions.queries.fetchDomains);
+const { mutateAsync: create, data: unitPropsPayload } = useMutation(unitQueryDefinitions.mutations.create);
+const { mutateAsync: apply, isLoading } = useMutation(domainQueryDefinitions.mutations.applyProfile);
+
+const { data: domains } = useQuery(domainQueryDefinitions.queries.fetchDomains);
 
 const createUnit = async () => {
   try {
     await create({
-      name: _domains.value?.[0].profiles.demoUnit.name,
-      description: _domains.value?.[0].profiles.demoUnit.description,
-      domains: (_domains.value || []).map((domain) => createLink('domains', domain.id))
+      name: domains.value?.[0].profiles.demoUnit.name,
+      description: domains.value?.[0].profiles.demoUnit.description,
+      domains: (domains.value || []).map((domain) => createLink('domains', domain.id))
     });
 
     const unit = await useQuerySync(unitQueryDefinitions.queries.fetch, { id: unitPropsPayload.value?.resourceId as string }, queryClient);
@@ -261,28 +296,36 @@ const createUnit = async () => {
 <i18n>
   {
     "en": {
+      "applyProfile": "Applying sample data. Please be patient ...",
       "channel": "YouTube channel",
       "checkboxLabel": "Show at startup",
       "documentation": "online documentation",
       "forum": "verinice.forum",
+      "goto": "Goto unit selection",
       "greeting": "Welcome to",
       "headline": "First steps",
       "hint": "You can access this page again at any time via the account button!",
       "intro": "In this section you will find suggestions from the verinice.team to get you started quickly:",
+      "requestForDeletion": "You have reached the maximum amount of units.
+        To enable the sample profile again, you have to delete an existing unit first.",
       "sampledata": "sample data",
       "sampleDataButtonLabel": "Load sample data now",
       "tutorial": "tutorials",
       "webinar": "webinars"
     },
     "de": {
+      "applyProfile": "Beispieldaten werden geladen ...",
       "channel": "YouTube Kanal",
       "checkboxLabel": "Beim Start anzeigen",
       "documentation": "Online-Dokumentation",
       "forum": "verinice.forum",
       "greeting": "Willkommen bei",
+      "goto": "Zur Unit-Auswahl",
       "headline": "Erste Schritte",
       "hint": "Sie können diese Seite jederzeit über den Account Button erneut aufrufen!",
       "intro": "Im diesem Abschnitt finden Sie Anregungen des verinice.Teams, die Ihnen einen schnellen Einstieg ermöglichen:",
+      "requestForDeletion": "Sie haben die maximale Anzahl an Units erreicht.
+        Um das Anwenden des Demoprofils wieder zu aktivieren, müssen Sie zunächst eine bestehende Unit löschen.",
       "sampledata": "Beispieldaten",
       "sampleDataButtonLabel": "Beispieldaten jetzt laden",
       "tutorial": "Tutorials",
