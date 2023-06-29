e072d7b05cc18410948e5687ac4f50d83bfc25dd
jae <jae@sernet.de>
2023-06-29 11:51:42 +0200

    Allow applying profiles on new units

--- components/profile/UnitDialog.vue
+++ components/profile/UnitDialog.vue
@@ -1,6 +1,6 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2023  snxy
+   - Copyright (C) 2023 jae
    -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
@@ -22,20 +22,70 @@
     :close-function="toggleDialog"
   >
     <template #default>
-      <v-card-text>
-        <p>{{ t('unitSelectionHint') }}</p>
-      </v-card-text>
-      <v-list-item>
-        <v-autocomplete
-          v-model="state.selectedUnit"
-          :label="t('unitSelectionDropdownLabel')"
-          :items="units"
-          item-title="name"
-          item-value="id"
-          flat
-          variant="underlined"
-        />
-      </v-list-item>
+      <BaseCard>
+        <v-card-text>
+          <v-icon
+            :icon="mdiFilePlusOutline"
+            size="x-large"
+            start
+          />
+          {{ t('unitSelectionCreateNewHint') }}
+        </v-card-text>
+        <v-card-text>
+          <v-text-field
+            v-model="state.newUnit.name"
+            :label="t('unitSelectionCreateName')"
+            :disabled="!!state.selectedUnit"
+            :rules="[requiredRule]"
+            required
+            clearable
+            variant="underlined"
+          />
+          <v-text-field
+            v-model="state.newUnit.description"
+            :label="t('unitSelectionCreateDesc')"
+            :disabled="!!state.selectedUnit"
+            variant="underlined"
+            clearable
+          />
+          <v-autocomplete
+            v-model="state.newUnit.selectedDomains"
+            :items="toRaw(domains)"
+            item-title="name"
+            item-key="id"
+            :label="t('unitSelectionCreateDomains')"
+            chips
+            multiple
+            flat
+            clearable
+            return-object
+            variant="underlined"
+          />
+        </v-card-text>
+      </BaseCard>
+      <BaseCard class="mt-4">
+        <v-card-text>
+          <v-icon
+            :icon="mdiFolderOpenOutline"
+            size="x-large"
+            start
+          />
+          {{ t('unitSelectionHint') }}
+        </v-card-text>
+        <v-list-item>
+          <v-autocomplete
+            v-model="state.selectedUnit"
+            :label="t('unitSelectionDropdownLabel')"
+            :items="units"
+            :disabled="!!(state.newUnit.name || state.newUnit.description) || state.newUnit.selectedDomains.length !== 0"
+            item-title="name"
+            item-value="id"
+            flat
+            clearable
+            variant="underlined"
+          />
+        </v-list-item>
+      </BaseCard>
     </template>
     <template #dialog-options>
       <v-btn
@@ -50,14 +100,9 @@
       <v-btn
         flat
         color="primary"
-        :disabled="false"
-        :loading="state.isApplyingProfile"
-        @click="() => applyProfile({
-          profileKey: state.selectedProfiles[0],
-          unitId: state.selectedUnit,
-          domainId: state.domainId,
-          messages: {success: t('messageSuccess'), error: t('messageError')}
-        })"
+        :loading="state.isApplyingProfile || state.isCreatingUnit"
+        :disabled="applyIsDisabled"
+        @click="apply"
       >
         {{ t('unitSelectionApplyBtn') }}
       </v-btn>
@@ -66,8 +111,42 @@
 </template>
 
 <script setup lang="ts">
+import {
+  mdiFolderOpenOutline,
+  mdiFilePlusOutline
+} from '@mdi/js';
 import { useUnits } from './profiles';
-const { state, units, applyProfile, toggleDialog } = useUnits();
+const { requiredRule } = useRules();
+const { state, units, domains, applyProfile, createUnitAndApplyProfile, toggleDialog } = useUnits();
 const { t } = useI18n();
+
+const applyIsDisabled = computed(() =>
+  (state.newUnit.name  === null || toRaw(state.newUnit.selectedDomains).length === 0) && state.selectedUnit === null
+);
+
+function apply() {
+  if (state.newUnit.name) {
+    createUnitAndApplyProfile({
+      name: state.newUnit.name,
+      domains:
+        state.newUnit.selectedDomains.map(domain =>
+          toRaw(({targetUri: domain._self}))
+        ),
+      description: state.newUnit.description || undefined,
+      messages: {
+        success: t('messageUnitWithProfileSuccess'),
+        error: t('messageUnitWithProfileError')
+      }
+    });
+  }
+  else {
+    applyProfile({
+      profileKey: state.selectedProfiles[0],
+      unitId: state.selectedUnit,
+      domainId: state.domainId,
+      messages: {success: t('messageSuccess'), error: t('messageError')}
+    });
+  }
+}
 </script>
 <i18n src="./messages.json"></i18n>
