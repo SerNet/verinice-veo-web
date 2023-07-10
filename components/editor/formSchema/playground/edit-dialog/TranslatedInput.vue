f20c76a2a20a4e16a1d61793020d86144322530d
Jonas Heitmann <jheitmann@nbrx.de>
2023-07-10 20:58:51 +0200

    Fixed a bug that caused groups and labels to not get internationalized

--- components/editor/formSchema/playground/edit-dialog/TranslatedInput.vue
+++ components/editor/formSchema/playground/edit-dialog/TranslatedInput.vue
@@ -39,6 +39,7 @@
 <script setup lang="ts">
 import { mdiAlphabeticalVariant, mdiLabelOutline, mdiTranslate } from '@mdi/js';
 import { cloneDeep } from 'lodash';
+import { v4 as UUIDv4 } from 'uuid';
 
 import { PROVIDE_KEYS as FORMSCHEMA_PROVIDE_KEYS } from '~~/pages/[unit]/domains/[domain]/editor/formschema.vue';
 import { IVeoFormSchemaItem } from '~~/composables/api/queryDefinitions/forms';
@@ -64,7 +65,7 @@ const noValueText = computed(() => props.formSchemaElement.type === 'Label' ? t(
 const icon = computed(() => props.formSchemaElement.type === 'Label' ? mdiAlphabeticalVariant : mdiLabelOutline);
 
 const labelExists = computed(() => !!props.formSchemaElement.text || !!props.formSchemaElement.options?.label);
-const isTranslatedLabel = computed(() => (props.formSchemaElement.text || props.formSchemaElement.options?.label)?.startsWith('#lang/') || false);
+const isTranslatedLabel = computed(() => (props.formSchemaElement.text || props.formSchemaElement.options?.label || '')?.startsWith('#lang/'));
 const translatedLabelKey = computed(() => (props.formSchemaElement.text || props.formSchemaElement.options?.label || '').replace('#lang/', ''));
 
 const formSchemaTranslation = computed(() => language?.value ? translations?.value?.[translatedLabelKey.value]?.[TRANSLATION_SOURCE.FORMSCHEMA]?.[language.value] : undefined);
@@ -116,21 +117,35 @@ watch(() => props.formSchemaElement, () => {
   modelValue.value = getElementName();
 }, { deep: true, immediate: true });
 
+// We always want to translate the label (untranslated labels are a thing of the past)
 const onInput = (newValue: string) => {
-  if(isTranslatedLabel.value) {
-    emit('set-translation', translatedLabelKey.value, newValue);
-  } else {
-    const currentData = cloneDeep(props.formSchemaElement);
-    if(props.formSchemaElement.type === 'Label') {
-      currentData.text = newValue;
-    } else {
-      if(!currentData.options) {
-        currentData.options = {};
+  const localFormSchemaElement = cloneDeep(props.formSchemaElement);
+  switch (localFormSchemaElement.type) {
+    case 'Label':
+      if(localFormSchemaElement.text) {
+        break;
       }
-      currentData.options.label = newValue;
-    }
-    emit('update:form-schema-element', currentData);
+      localFormSchemaElement.text = `#lang/text_${UUIDv4()}`;
+      emit('update:form-schema-element', localFormSchemaElement);
+      break;
+    case 'Layout':
+      if(localFormSchemaElement.options?.label) {
+        break;
+      }
+      if(!localFormSchemaElement.options) {
+        localFormSchemaElement.options = {};
+      }
+      localFormSchemaElement.options.label = `#lang/group_${UUIDv4()}`;
+      emit('update:form-schema-element', localFormSchemaElement);
+      break;
+    default:
+      break;
   }
+  const key = (localFormSchemaElement.text || localFormSchemaElement.options?.label)?.replace('#lang/', '');
+  if(!key) {
+    throw new Error('TranslatedInput: No key found to use as translation key');
+  }
+  emit('set-translation', key, newValue);
 };
 </script>
 
