d184aa5255d31a905baeffe3474e4babe3e942f9
jae <jae@sernet.de>
2023-09-14 08:38:14 +0200

    Compliance: create base page

---
+++ pages/[unit]/domains/[domain]/compliance.vue
@@ -0,0 +1,27 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2023 jae
+   -
+   - This program is free software: you can redistribute it and/or modify
+   - it under the terms of the GNU Affero General Public License as published by
+   - the Free Software Foundation, either version 3 of the License, or
+   - (at your option) any later version.
+   -
+   - This program is distributed in the hope that it will be useful,
+   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   - GNU Affero General Public License for more details.
+   -
+   - You should have received a copy of the GNU Affero General Public License
+   - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <div>
+  </div>
+</template>
+<script lang="ts">
+export const ROUTE_NAME = 'unit-domains-domain-compliance';
+</script>
+
+<script setup lang="ts"></script>
