eac1895b68e8397487e8fe7d09a27cb4fa23d6cb
jae <jae@sernet.de>
2023-11-09 13:51:30 +0100

    Ignore ResizeObserver error in headless firefox

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -11,9 +11,19 @@ Cypress.Commands.addAll({
   deleteUnit
 });
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+// Uncaught exeptions make cypress test runs fail
+// However, this is not always correct
+Cypress.on('uncaught:exception', (err, _runnable) => {
+
   // Prevent tests from failing on localhost
   if (err.message.includes("Cannot read properties of null")) {
-    return false
+    return false;
   }
+
+  // Prevent ResizeObserver errors when testing in headless firefox
+  if (err.message.includes("Resize0bserver loop completed with undelivered notifications.")) {
+    return false;
+  }
+
 });
+
