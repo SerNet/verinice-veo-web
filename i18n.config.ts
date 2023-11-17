64fec7a386e85fa923c8109b4556c9368ce5842f
Frank Schneider <fs@sernet.de>
2023-11-17 10:28:50 +0100

    fix: suppress i18n console warnings

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,7 @@
 export default {
   legacy: false,
-  silentFallbackWarn: true,
+  fallbackWarn: false,
+  missingWarn: false,
   datetimeFormats: Object.fromEntries(
     ["de", "en"].map((lang) => [
       lang,
@@ -10,14 +11,14 @@ export default {
           month: "2-digit",
           year: "numeric",
           hour: "2-digit",
-          minute: "2-digit",
+          minute: "2-digit"
         },
         short: {
           day: "2-digit",
           month: "2-digit",
-          year: "numeric",
-        },
-      },
+          year: "numeric"
+        }
+      }
     ])
-  ),
+  )
 };
