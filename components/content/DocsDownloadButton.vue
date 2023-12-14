178e106827a07a8ecf76f07469b7ccd6cde52ec7
jae <jae@sernet.de>
2023-12-14 10:56:42 +0100

    Documentation: Link to print view instead of pdf download

--- components/content/DocsDownloadButton.vue
+++ components/content/DocsDownloadButton.vue
@@ -1,78 +1,56 @@
 <!--
    - verinice.veo web
    - Copyright (C) 2022  Jonas Heitmann
-   - 
+   -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
    - the Free Software Foundation, either version 3 of the License, or
    - (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful,
    - but WITHOUT ANY WARRANTY; without even the implied warranty of
    - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    - GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
   <v-tooltip
     location="bottom"
-    :disabled="pdfExists"
   >
     <template #activator="{ props }">
       <div v-bind="props">
         <v-btn
-          :disabled="!pdfExists"
-          :href="pdfExists ? pdfPath : '#'"
+          href="/docs?print"
           variant="flat"
           color="primary"
           class="mr-2"
           target="_blank"
         >
-          {{ t('exportAsPDF') }}
-        </v-btn>            
+          {{ t('buttonCopy') }}
+        </v-btn>
       </div>
     </template>
     <template #default>
-      {{ t('noPdfExists') }}
+      {{ t('tooltipCopy') }}
     </template>
   </v-tooltip>
 </template>
 
-<script lang="ts">
-export default defineComponent({
-  setup() {
-    const { locale, t } = useI18n();
-
-    const pdfExists = ref(false);
-    const pdfPath = computed(() => `/Documentation_${locale.value}.pdf`);
-    onMounted(async () => {
-      try {
-        const response = await fetch(pdfPath.value);
-        pdfExists.value = !!response.headers.get('content-type')?.startsWith('application/pdf');
-      } catch (_) { /* empty */ }
-    });
-
-    return {
-      pdfExists,
-      pdfPath,
-
-      t
-    };
-  }
-});
+<script setup lang="ts">
+const { t } = useI18n();
 </script>
 
 <i18n>
 {
   "en": {
-    "exportAsPDF": "download as pdf",
-    "noPdfExists": "There is no downloadable pdf for this language"
+    "buttonCopy": "Print preview / Download",
+    "tooltipCopy": "Print this documentation or create a PDF"
   },
   "de": {
-    "exportAsPDF": "Als PDF herunterladen",
-    "noPdfExists": "Für diese Sprache existiert keine PDF"
+    "buttonCopy": "Druckvorschau / Download",
+    "tooltipCopy": "Drucken Sie diese Dokumentation oder erstellen Sie ein PDF"
   }
-}  
+}
 </i18n>
