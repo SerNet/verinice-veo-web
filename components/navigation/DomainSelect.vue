33934541a5be977cf44838a02ebde7090927ed2a
Frank Schneider <fs@sernet.de>
2024-01-23 16:37:05 +0100

    unit select: add description, use v-select

--- components/navigation/DomainSelect.vue
+++ components/navigation/DomainSelect.vue
@@ -1,117 +1,84 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2021 Jonas Heitmann, Annemarie Bufe, Frank Schneider
-   - 
+   - Copyright (C) 2024 Frank Schneider
+   -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
    - the Free Software Foundation, either version 3 of the License, or
    - (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful,
    - but WITHOUT ANY WARRANTY; without even the implied warranty of
    - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    - GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <v-menu
+  <v-tooltip
+    v-if="miniVariant"
     location="bottom"
-    width="275px"
-    :transition="false"
-    offset="200"
   >
-    <template #activator="{ props: menuProps, isActive }">
-      <v-list
-        class="mt-0"
-        nav
-        density="compact"
-        :rounded="miniVariant"
-      >
-        <v-list-item
-          v-show="!miniVariant"
-          class="veo-domain-select bg-basepage"
-          data-component-name="domain-select"
-          v-bind="menuProps"
-          :disabled="disabled"
-        >
-          <v-skeleton-loader
-            v-if="domainIsFetching"
-            height="24px"
-            style="border-radius: 999px"
-            type="image"
-            width="100px"
-          />
-          <span
-            v-else
-            class="veo-domain-select__selection text-no-wrap"
-          >
-            {{ domainName }}
-          </span>
-          <template #append>
-            <v-icon
-              color="primary"
-              :icon="isActive ? mdiChevronUp : mdiChevronDown"
-            />
-          </template>
-        </v-list-item>
-        <v-list-item
-          v-show="miniVariant"
-          style="height:44px;"
-          v-bind="menuProps"
-          @click="$emit('expand-menu')"
-        >
-          <v-tooltip
-            location="end"
-            :disabled="!miniVariant"
-          >
-            <template #activator="{ props: tooltip }">
-              <div v-bind="tooltip">
-                <v-icon
-                  color="black"
-                  :icon="mdiDomain"
-                />
-              </div>
-            </template>
-            <span>{{ t('noDomainSelected') }}</span>
-          </v-tooltip>
-        </v-list-item>
-      </v-list>
+    <template #activator="{ props: toolTips }">
+      <v-icon
+        class="ma-4"
+        :icon="mdiDomain"
+        v-bind="toolTips"
+        @click="$emit('expand-menu')"
+      />
     </template>
 
     <template #default>
-      <v-card>
-        <v-list density="compact">
-          <v-list-item
-            v-for="domain of itemSelection"
-            :key="domain.value"
-            :active="domainId === domain.value"
-            color="primary"
-            :value="domain.value"
-            :title="domain.title"
-            @click="domainId = domain.value"
-          />
-          <v-divider v-show="!miniVariant" />
-          <v-list-item
-            :active="domainId === 'more'"
-            color="primary"
-            value="more"
-            :title="globalT('breadcrumbs.more')"
-            @click="domainId = 'more'"
-          />
-        </v-list>
-      </v-card>
+      {{ t('select') }}
     </template>
-  </v-menu>
+  </v-tooltip>
+
+  <v-select
+    v-else
+    ref="closeMenu"
+    :model-value="items"
+    class="mx-2"
+    data-component-name="domain-select"
+    :disabled="disabled"
+    :items="itemSelection"
+    :label="items.length ? t('domain') : t('select')"
+  >
+    <template #item="{ props, item }">
+      <v-list-item
+        v-show="!miniVariant"
+        v-bind="props"
+        :active="domainId === item.value"
+        color="primary"
+        data-veo-test="domain-selection-nav-item"
+        max-width="550"
+        :title="item.title"
+        :value="item.value"
+        @click="domainId = item.value"
+      />
+    </template>
+
+    <template #append-item>
+      <v-divider />
+
+      <v-list-item
+        :active="domainId === 'more'"
+        color="primary"
+        value="more"
+        :title="globalT('breadcrumbs.more')"
+        @click="domainId = 'more'; closeMenu.menu = false"
+      />
+    </template>
+  </v-select>
 </template>
 
 <script setup lang="ts">
 import { ROUTE_NAME as ROUTE_MORE_DOMAINS } from '~/pages/[unit]/domains/more.vue';
 import { ROUTE_NAME as ROUTE_DOMAIN_DASHBOARD } from '~/pages/[unit]/domains/[domain]/index.vue';
+
 import { useFetchUnitDomains } from '~/composables/api/domains';
 
-import { mdiChevronDown, mdiChevronUp, mdiDomain } from '@mdi/js';
+import { mdiDomain } from '@mdi/js';
 
 withDefaults(defineProps<{
   disabled?: boolean,
@@ -128,6 +95,16 @@ defineEmits<{
 const route = useRoute();
 const { t } = useI18n();
 const { t: globalT } = useI18n({ useScope: 'global' });
+
+const fetchUnitDomainsQueryParameters = computed(() => ({ unitId: route.params.unit as string }));
+const fetchUnitDomainsQueryEnabled = computed(() => !!route.params.unit);
+const { data: domains } = useFetchUnitDomains(fetchUnitDomainsQueryParameters, { enabled: fetchUnitDomainsQueryEnabled });
+
+// v-select's append-item slot has no events (!), hence we have to reference it
+const closeMenu = ref();
+
+const items = computed(() => ((domains.value || []).find((domain: any) => domain.id === route.params.domain))?.name || []);
+const itemSelection = computed(() => (domains.value || []).map((domain: any) => ({ value: domain.id, title: domain.name })));
 
 const domainId = computed({
   get() {
@@ -146,40 +123,17 @@ const domainId = computed({
     });
   }
 });
-
-const fetchUnitDomainsQueryParameters = computed(() => ({ unitId: route.params.unit as string }));
-const fetchUnitDomainsQueryEnabled = computed(() => !!route.params.unit);
-const { data: domains, isFetching: domainIsFetching } = useFetchUnitDomains(fetchUnitDomainsQueryParameters, { enabled: fetchUnitDomainsQueryEnabled });
-
-const itemSelection = computed(() => (domains.value || []).map((domain: any) => ({ value: domain.id, title: domain.name })));
-const domainName = computed(() => domainId.value === undefined ? t('noDomainSelected').toString() : (itemSelection.value.find((domain: any) => domain.value === domainId.value)?.title));
 </script>
 
 <i18n>
 {
   "en": {
-    "noDomainSelected": "Select domain"
+    "domain": "Domain",
+    "select": "Select domain"
   },
   "de": {
-    "noDomainSelected": "Domäne auswählen"
+    "domain": "Domäne",
+    "select": "Domäne auswählen"
   }
 }
 </i18n>
-
-<style lang="scss" scoped>
-.veo-domain-select {
-  border-radius: 6px;
-}
-
-.veo-domain-select.v-list-item--disabled {
-  background: rgba(0, 0, 0, 0.16);
-}
-
-.veo-domain-select__selection {
-  color: #666666;
-  font-size: 1.2rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-}
-</style>
--- components/navigation/UnitSelect.vue
+++ components/navigation/UnitSelect.vue
@@ -16,95 +16,61 @@
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <v-menu
+  <v-tooltip
+    v-if="miniVariant"
     location="bottom"
-    width="275px"
-    :transition="false"
-    offset="130"
   >
-    <template #activator="{ props: menuProps, isActive }">
-      <v-list
-        class="mt-4"
-        nav
-        density="compact"
-        :rounded="miniVariant"
-      >
-        <v-list-item
-          v-show="!miniVariant"
-          class="veo-unit-select bg-basepage"
-          data-component-name="unit-select"
-          v-bind="menuProps"
-          :disabled="disabled"
-        >
-          <v-skeleton-loader
-            v-if="isFetching"
-            height="24px"
-            style="border-radius: 999px"
-            type="image"
-            width="100px"
-          />
-          <span
-            v-else
-            class="veo-unit-select__selection text-no-wrap"
-          >
-            {{ unitName }}
-          </span>
-          <template #append>
-            <v-icon
-              color="primary"
-              :icon="isActive ? mdiChevronUp : mdiChevronDown"
-            />
-          </template>
-        </v-list-item>
-        <v-list-item
-          v-show="miniVariant"
-          style="height:44px;"
-          v-bind="menuProps"
-          @click="$emit('expand-menu')"
-        >
-          <v-tooltip
-            location="end"
-            :disabled="!miniVariant"
-          >
-            <template #activator="{ props: tooltip }">
-              <div v-bind="tooltip">
-                <v-icon
-                  color="black"
-                  :icon="mdiUnity"
-                />
-              </div>
-            </template>
-            <span>{{ t('management') }}</span>
-          </v-tooltip>
-        </v-list-item>
-      </v-list>
+    <template #activator="{ props: toolTips }">
+      <v-icon
+        class="ma-4"
+        :icon="mdiUnity"
+        v-bind="toolTips"
+        @click="$emit('expand-menu')"
+      />
     </template>
 
     <template #default>
-      <v-card>
-        <v-list density="compact">
-          <v-list-item
-            v-for="unit of itemSelection"
-            :key="unit.value"
-            :active="unitId === unit.value"
-            color="primary"
-            :value="unit.value"
-            :title="unit.title"
-            @click="unitId = unit.value"
-          />
-          <v-divider v-show="!miniVariant" />
-          <v-list-item
-            :active="unitId === 'management'"
-            color="primary"
-            data-veo-test="unit-selection-nav-item"
-            value="management"
-            :title="t('management')"
-            @click="unitId = 'management'"
-          />
-        </v-list>
-      </v-card>
+      {{ t('select') }}
     </template>
-  </v-menu>
+  </v-tooltip>
+
+  <v-select
+    v-else
+    ref="closeMenu"
+    :model-value="items"
+    class="ma-2 mt-4"
+    data-component-name="unit-select"
+    :disabled="disabled"
+    :items="itemSelection"
+    :label="items.length ? 'Unit' : t('select')"
+  >
+    <template #item="{ props, item }">
+      <v-list-item
+        v-bind="props"
+        :active="unitId === item.value"
+        color="primary"
+        data-veo-test="unit-selection-nav-item"
+        max-width="550"
+        :subtitle="item.raw.description"
+        :title="item.title"
+        :value="item.value"
+        @click="unitId = item.value"
+      />
+    </template>
+
+    <template #append-item>
+      <v-divider />
+
+      <v-list-item
+        :active="unitId === 'management'"
+        color="primary"
+        value="management"
+        @click="unitId = 'management'; closeMenu.menu = false"
+      >
+        {{ t('management') }}
+      </v-list-item>
+    </template>
+  </v-select>
 </template>
 
 <script setup lang="ts">
@@ -114,11 +80,11 @@ import { ROUTE_NAME as ROUTE_INDEX } from '~/pages/index.vue';
 import { useQuery } from '~/composables/api/utils/query';
 import unitQueryDefinitions from '~/composables/api/queryDefinitions/units';
 
-import { mdiChevronDown, mdiChevronUp, mdiUnity } from '@mdi/js';
+import { mdiUnity } from '@mdi/js';
 
 withDefaults(defineProps<{
   disabled?: boolean,
-  miniVariant: boolean
+  miniVariant?: boolean
 }>(), {
   disabled: false,
   miniVariant: false
@@ -130,17 +96,22 @@ defineEmits<{
 
 const route = useRoute();
 const { t } = useI18n();
+// v-select's append-item slot has no events (!), hence we have to reference it
+const closeMenu = ref();
 
 // fetch all client units
-const { data: units, isFetching } = useQuery(unitQueryDefinitions.queries.fetchAll);
+const { data: units } = useQuery(unitQueryDefinitions.queries.fetchAll);
+
+const items = computed(() => ((units.value || []).find((unit: any) => unit.id === route.params.unit))?.name || []);
+const itemSelection = computed(() => (units.value || []).map((unit: any) => ({ value: unit.id, title: unit.name, description: unit.description })));
 
 const unitId = computed({
   get() {
     return route.params.unit as string || 'management';
   },
   set(newValue: string) {
-    
     let params;
+
     if (newValue === 'management') {
       params = { ...route.params };
     } else {
@@ -156,38 +127,17 @@ const unitId = computed({
     });
   }
 });
-
-const itemSelection = computed(() => (units.value || []).map((unit: any) => ({ value: unit.id, title: unit.name })));
-const unitName = computed(() => units.value?.find((unit: any) => (unitId.value === unit.id))?.name || t('noUnitSelected').toString());
 </script>
 
 <i18n>
 {
   "en": {
     "management": "Manage units",
-    "noUnitSelected": "Select unit"
+    "select": "Select unit"
   },
   "de": {
     "management": "Units verwalten",
-    "noUnitSelected": "Unit auswählen"
+    "select": "Unit auswählen"
   }
 }
 </i18n>
-
-<style lang="scss" scoped>
-.veo-unit-select {
-  border-radius: 6px;
-}
-
-.veo-unit-select.v-list-item--disabled {
-  background: rgba(0, 0, 0, 0.16);
-}
-
-.veo-unit-select__selection {
-  color: #666666;
-  font-size: 1.2rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-}
-</style>
