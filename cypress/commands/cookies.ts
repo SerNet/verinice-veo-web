519a203c5292e2bff95fb5313c547af937423195
jae <jae@sernet.de>
2024-01-30 10:49:31 +0100

    Cypress: simplify acceptCookies command

--- cypress/commands/cookies.ts
+++ cypress/commands/cookies.ts
@@ -10,11 +10,5 @@ declare global {
 }
 
 export function acceptAllCookies() {
-  cy.get('body').then(($body) => {
-    if ($body.find('[data-veo-test="cookies-btn-accept-all"]').length) {
-      cy.get('[data-veo-test="cookies-btn-accept-all"]').click();
-    } else {
-      cy.log('no cookie banner present');
-    }
-  });
+  cy.get('[data-veo-test="cookies-btn-accept-all"]').click();
 }
