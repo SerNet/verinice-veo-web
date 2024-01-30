b35002e6a83583b4a537682b08b61ac85fe4e9e2
jae <jae@sernet.de>
2024-01-30 16:18:55 +0100

    Test with huge waiting time when first visiting login page

--- cypress/commands/login.ts
+++ cypress/commands/login.ts
@@ -24,7 +24,7 @@ export function login({
     [username, password],
     () => {
       cy.visit('/login');
-      cy.url().should('contain', '/login');
+      cy.wait(30000)
       cy.get('[data-veo-test="login-btn-login"]', { timeout: 30000 }).click();
 
       if (!isLocalhost) {
