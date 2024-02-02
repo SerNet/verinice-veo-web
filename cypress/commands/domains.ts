8a37e6d5531378e936419265b2c8e6ab9c3a3ee8
jae <jae@sernet.de>
2024-02-02 14:26:54 +0100

    Cypress: add command to get and store domain info from api

---
+++ cypress/commands/domains.ts
@@ -0,0 +1,42 @@
+import { IVeoDomain } from '../../composables/api/queryDefinitions/domains';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      getVeoDomains: typeof getVeoDomains;
+    }
+  }
+}
+
+export type TCYVeoUnitNames = 'DS-GVO' | 'ITGS' | 'NIS2';
+export interface ICYVeoDomain {
+  name: string;
+  id: string;
+  targetUri: string;
+}
+
+/**
+ * Get all available domains,
+ * reduce them to a concise format,
+ * write them into an env var.
+ */
+export function getVeoDomains(): Promise<ICYVeoDomain[]> {
+  const savedDomains = Cypress.env('veoDomains');
+  if (savedDomains) return savedDomains;
+
+  return cy
+    .veoRequest({
+      url: '/api/domains',
+      method: 'GET'
+    })
+    .then((domains: IVeoDomain[]) => {
+      const reducedDomains = domains.reduce(
+        (acc: ICYVeoDomain[], cur: IVeoDomain) => [...acc, { name: cur.name, id: cur.id, targetUri: cur._self }],
+        []
+      );
+
+      Cypress.env('veoDomains', reducedDomains);
+      return reducedDomains;
+    });
+}
