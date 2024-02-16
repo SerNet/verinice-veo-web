44eee04f7055e4006795c46678ae6a6ecf810fea
gk <gk@sernet.de>
2024-02-16 13:08:47 +0100

    Format check

--- cypress/e2e/elements/createElements.cy.ts
+++ cypress/e2e/elements/createElements.cy.ts
@@ -3,61 +3,61 @@ describe('Create elements', () => {
     cy.login();
     cy.createUnit();
   });
-  
+
   beforeEach(() => {
     cy.viewport(1000, 1000);
     cy.login();
     cy.goToUnitSelection();
     cy.selectUnit();
   });
-  
+
   after(() => cy.deleteUnit());
-  
+
   const elementTypeList: string[] = ['Scope', 'Process', 'Asset', 'Person', 'Incident', 'Document', 'Scenario'];
-  
+
   const childNummber = 1;
-  
+
   for (const elementType of elementTypeList) {
     it('creates elements in ' + elementType, () => {
       cy.navigation(['Objects', elementType]);
-  
+
       cy.contains('div[sub-group="true"] > div', new RegExp(`^${elementType}$`))
         .should('be.visible')
         .parent()
         .find('a')
         .each(($subType) => {
           if ($subType.text() == 'All') return;
-  
+
           cy.wrap($subType).click();
-  
+
           cy.wait(100);
-  
+
           for (let i = 0; i < childNummber; i++) {
             cy.get('button[data-component-name="create-object-button"]').click();
-  
+
             const elements = {
               name: elementType + '_' + $subType.text() + '_' + i,
               abb: 'Abb_' + i,
               des: 'Cypress testElement',
               status: selectRandom(['Archived', 'New', 'In progress', 'Released', 'For review'])
             };
-  
+
             let extra = 's';
             if (elementType === 'Process') extra = 'es';
-  
+
             cy.intercept(
               'POST',
               `${Cypress.env('veoApiUrl')}/domains/**/${elementType.toLocaleLowerCase() + extra}?scopes=**`
             ).as('createElement');
-  
+
             createElement(elements.abb, elements.name, elements.des, elements.status);
-  
+
             cy.wait(['@createElement']).its('response.statusCode').should('eq', 201);
-  
+
             veoAlertDismiss();
-  
+
             cy.contains(elements.name).closest('tr').as('tmp');
-  
+
             cy.get('@tmp').then(($row) => {
               cy.wrap($row).contains(elements.abb).should('be.visible');
               cy.wrap($row).contains(elements.status).should('be.visible');
@@ -66,29 +66,28 @@ describe('Create elements', () => {
         });
     });
   }
-  
+
   const veoAlertDismiss = () => {
     cy.get('div[role="alert"][params="[object Object]"]').click();
   };
-  
+
   const createElement = (abbreviation: string, name: string, description: string, status: string) => {
     cy.get('input[id="#/properties/abbreviation"]').type(abbreviation);
     cy.get('input[id="#/properties/name"]').type(name);
     cy.get('textarea[id="#/properties/description"]').type(description);
     selectMenuItem(status);
-  
+
     cy.wait(200);
     cy.contains('Save').click();
   };
-  
+
   const selectMenuItem = (status: string) => {
     cy.get('div[data-attribute-name="status"]').click();
     cy.get('div[role="listbox"]').contains(status).click();
   };
-  
+
   const selectRandom = (list) => {
     const randomIndex = Math.floor(Math.random() * list.length);
     return list[randomIndex];
   };
 });
-  
