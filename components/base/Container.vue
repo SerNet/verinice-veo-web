65c0de88399f30eb1c8a7c281ed2253bf7371b67
jae <jae@sernet.de>
2024-03-08 08:47:04 +0100

    Add templates for card lists
    veo-2759

---
+++ components/base/Container.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container style="max-width: 1020px">
+    <v-row>
+      <slot name="default"></slot>
+    </v-row>
+  </v-container>
+</template>
---
+++ components/base/List/Item.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-col cols="12">
+    <v-card :loading="false">
+      <div class="veo-base-card__slot-prepend" style="padding: 0">
+        <slot :item="item" name="prepend" />
+      </div>
+
+      <div class="veo-base-card__slot-center">
+        <div>
+          <NuxtLink :to="item.link" class="veo-base-card__slot-center__main">
+            <v-card-title v-text="item.name"></v-card-title>
+            <v-card-subtitle v-text="item.metaData ?? 'No meta data available'"></v-card-subtitle>
+            <v-card-text
+              data-veo-test="item-card-text"
+              v-text="item.description ?? 'No description available.'"
+            ></v-card-text>
+          </NuxtLink>
+
+          <div class="veo-base-card__slot-center__aside">
+            <slot :item="item" name="center-aside"></slot>
+          </div>
+        </div>
+
+        <v-card-actions class="veo-card-actions">
+          <div data-veo-test="item-card-slot-left" class="veo-card-actions__slot-left">
+            <slot :item="item" name="bottom-left"></slot>
+          </div>
+
+          <div class="veo-base-card-actions__slot-append">
+            <slot :item="item" name="bottom-right"></slot>
+          </div>
+        </v-card-actions>
+      </div>
+    </v-card>
+  </v-col>
+</template>
+<script setup lang="ts">
+interface Props {
+  item: {
+    name: string;
+    link: string;
+    description: string | undefined;
+    metaData: string | undefined;
+  };
+}
+const props = defineProps<Props>();
+</script>
+<style scoped lang="scss">
+.v-card {
+  display: flex;
+}
+
+.veo-base-card__slot-center {
+  flex: 1;
+
+  > div:first-child {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+
+  /* reset padding */
+  a > * {
+    padding-left: 8px;
+    white-space: break-spaces;
+  }
+  a:hover div:first-child {
+    text-decoration: underline;
+  }
+}
+
+.veo-base-card__slot-center__main {
+  text-decoration: none;
+  color: var(--veo-text, rgba(0, 0, 0, 0.87)) !important;
+  display: flex;
+  flex-direction: column;
+}
+
+.veo-base-card__slot-center__aside {
+  flex-direction: column;
+}
+
+.veo-card-actions__slot-left {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  > * {
+    margin: 0 !important;
+  }
+}
+.veo-base-card-actions__slot-append {
+  margin-left: auto;
+}
+
+.v-theme--dark {
+  /* Dark mode text color */
+  --veo-text: white;
+}
+</style>
