c8c574821176e8458bff556c7e0fe00054f851d6
jae <jae@sernet.de>
2024-03-11 15:01:40 +0100

    Refactor and move unit data mapping

--- components/unit/Units.vue
+++ components/unit/Units.vue
@@ -41,24 +41,11 @@ import {
   mdiShapeOutline
 } from '@mdi/js';
 import { format } from 'date-fns';
+import { mapUnitValues } from './unit-module';
 
 // Locally used types
 import type { IVeoUnit } from '~/composables/api/queryDefinitions/units';
-import type { IVeoDomain } from '~/composables/api/queryDefinitions/domains';
-interface IUnit {
-  id: string;
-  name: string;
-  description: string;
-  metaData: string;
-  domains: {
-    names: string[];
-    abbrevs: string[];
-    raw: IVeoDomain;
-  };
-  link: string;
-  profilesUrl: string;
-  isFavorite: boolean;
-}
+import type { TUnit } from './unit-module.ts';
 
 type TInlineComponent = {
   props?: string[];
@@ -69,7 +56,7 @@ type TInlineComponent = {
 
 const { data: _units, isFetching: isFetchingUnits } = useQuery(unitQueryDefinitions.queries.fetchAll);
 const activeUnits = computed(() => _units.value?.length || null);
-const favoriteUnitId = localStorage.getItem(LOCAL_STORAGE_KEYS.FAVORITE_UNIT);
+const favoriteUnitId: string | null = localStorage.getItem(LOCAL_STORAGE_KEYS.FAVORITE_UNIT);
 
 const newUnits = ref<any>(null);
 const units = computed({
@@ -77,24 +64,8 @@ const units = computed({
     if (newUnits.value) return newUnits.value;
     if (!_units.value) return [];
 
-    return _units.value.map((unit: IVeoUnit) => ({
-      id: unit.id,
-      name: unit.name,
-      description: unit?.description,
-      link: `/${unit.id}/domains/${unit.domains?.[0].id}`,
-      profilesUrl: `${unit.id}/domains/${unit.domains?.[0].id}/profiles`,
-      isFavorite: unit.id === favoriteUnitId ? true : false,
-      metaData: `by: ${unit.createdBy} | at: ${format(unit.createdAt, 'dd.MM.yyyy')}`,
-      domains: {
-        names: unit.domains.map((d) => d.name),
-        abbrevs: unit.domains.map((d) => d.abbreviation),
-        colors: { ITGS: 'green', 'DS-GVO': 'primary', NIS2: 'purple', DEFAULT: 'black' }
-      },
-      raw: toRaw(unit),
-      isFavorite: false
-    }));
+    return _units.value.map((unit: IVeoUnit) => mapUnitValues({ unit, favoriteUnitId }));
   },
-
   set(newValue) {
     newUnits.value = newValue;
   }
@@ -111,7 +82,7 @@ function createUnit() {
   isManageDialogOpen.value = true;
 }
 
-function editUnit(unit: IUnit) {
+function editUnit(unit: TUnit) {
   unitToEditId.value = unit.id;
   isManageDialogOpen.value = true;
 }
@@ -121,9 +92,10 @@ function deleteUnit(unit: TUnit) {
   deleteDialogIsOpen.value = true;
 }
 
-function bookmarkFavoriteUnit(unit: IUnit) {
+function bookmarkFavoriteUnit(unit: TUnit) {
   if (!units.value) return;
   const favoriteUnitDomainId = unit.raw.domains[0]?.id;
+
   localStorage.setItem(LOCAL_STORAGE_KEYS.FAVORITE_UNIT, unit.id);
 
   if (typeof favoriteUnitDomainId === 'string') {
@@ -137,7 +109,7 @@ function bookmarkFavoriteUnit(unit: IUnit) {
 }
 
 // Domain Actions
-function editDomains(unit: IUnit) {
+function editDomains(unit: TUnit) {
   // For now the same action as in `editUnit` is triggered
   // In a later iteration this is going to change
   editUnit(unit);
@@ -177,14 +149,14 @@ const DomainActions: TInlineComponent = {
     }
   },
   template: `
-    <v-btn v-for="(name, index) in this.domains.names"
+    <v-btn v-for="(domain, index) in this.domains"
       :key="index"
       :prepend-icon="mdiPuzzle"
       variant="outlined"
-      :color="this.domains.colors[this.domains.abbrevs[index]] || this.domains.colors['DEFAULT']"
+      :color="domain.color"
       size="x-small"
     >
-      {{ name }}
+      {{ domain.name }}
     </v-btn>
 
     <v-btn
