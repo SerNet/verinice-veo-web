d803c2a7b8e94d7fb3fde2baf8ecfff4611178bd
jae <jae@sernet.de>
2024-03-12 09:43:31 +0100

    Cypress: generate shorter test unit names + show creation date in description

--- cypress/support/setupHelpers.ts
+++ cypress/support/setupHelpers.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 type UnitDetails = {
   name: string | null;
   desc: string | null;
@@ -8,8 +10,10 @@ export function generateUnitDetails() {
   const id = Cypress.env('pipelineId') ? Cypress.env('pipelineId') : self.crypto.randomUUID();
 
   const unitDetails: UnitDetails = {
-    name: `${Cypress.env('testUnitPrefix')}${id}-spec-${Cypress.spec.name}`,
-    desc: `Spec: ${Cypress.spec.name} | Test: ${Cypress.currentTest.title}`,
+    name: `${Cypress.env('testUnitPrefix')}${id}`,
+    desc: `StartAt: ${format(new Date(), 'yy-MM-dd::hh:mm:ss')} | Spec: ${Cypress.spec.name} | Test: ${
+      Cypress.currentTest.title
+    }`,
     domains: ['DS-GVO']
   };
 
