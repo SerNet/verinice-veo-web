15053ce606479069ea9559aa64366ae16f66493f
jae <jae@sernet.de>
2024-04-18 10:26:34 +0200

    LoadingWrapper: improve styling

--- components/layout/LoadingWrapper.vue
+++ components/layout/LoadingWrapper.vue
@@ -1,24 +1,24 @@
 <!--
    - verinice.veo web
    - Copyright (C) 2021  Jonas Heitmann, Davit Svandize
-   - 
+   -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
    - the Free Software Foundation, either version 3 of the License, or
    - (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful,
    - but WITHOUT ANY WARRANTY; without even the implied warranty of
    - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    - GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <div class="veo-loading-wrapper d-flex align-center justify-center">
+  <div class="veo-loading-wrapper d-flex align-center justify-center flex-column">
     <v-progress-circular size="64" color="primary" indeterminate />
-    <span v-if="text" class="font-size: 1.5rem;">{{ text }}</span>
+    <div v-if="text" style="font-size: 1.8rem">{{ text }}</div>
   </div>
 </template>
 
