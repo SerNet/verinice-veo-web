c1ee232689720de55c4839dcfa29a4fa5401d855
jae <jae@sernet.de>
2024-04-22 08:51:45 +0200

    Test new apply profiles feature

--- cypress/e2e/profiles/applyProfiles.cy.ts
+++ cypress/e2e/profiles/applyProfiles.cy.ts
@@ -8,31 +8,54 @@ describe('Apply Profiles', () => {
 
   it('applies the DS-GVO demo profile to the test unit', () => {
     const profileName = 'Beispieldaten';
-    const unitName = Cypress.env('unitDetails').name;
+    const unitId = Cypress.env('unitDetails').unitId;
+    const domainId = Cypress.env('unitDetails').domains[0].id;
+
+    cy.log(JSON.stringify(Cypress.env('unitDetails')));
 
     // Per default test units are associated with the DS-GVO Domain
+    cy.acceptAllCookies();
     cy.goToUnitSelection();
-    cy.selectUnit();
+    cy.getVeoTestUnitCard().as('testUnitCard');
 
-    // Go to /profiles
-    cy.get('[data-veo-test="profiles-nav-item"]').click();
-    // Select Profile
-    // get table row containing given profileName
-    cy.get('.v-data-table__tr').contains(profileName).parent().as('checkboxParent');
-    // select checkbox in that row
-    cy.get('@checkboxParent').within(() => cy.get('[type="checkbox"]').click());
+    // Go to profiles
+    cy.get('@testUnitCard').within((_card) => {
+      cy.get('[data-veo-test="apply-profiles-link"]').click();
+    });
 
-    // Select a unit to which the profile will be applied
-    cy.get('[data-veo-test="profiles-btn-apply"]').click();
+    // Get profile card
+    cy.get(`[data-veo-test="profile-${profileName}"]`).as('profileCard');
 
-    cy.get('[role="dialog"] .v-card input').eq(2).type(unitName).type('{upArrow}');
-    cy.get('.v-autocomplete__content .v-list-item').click();
+    // Choose profile
+    cy.get('@profileCard').find('[data-veo-test="profile-radio-btn"] input').click();
 
-    // Set up a listener for API responses
+    // Apply profile
     cy.intercept('POST', `${Cypress.env('veoApiUrl')}/domains/**/profiles/**/incarnation?unit=**`).as('applyProfile');
-    cy.get('.v-card-actions button').last().click();
-
-    // Wait for API response
+    cy.get('[data-veo-test="apply-profile"]').click();
     cy.wait(['@applyProfile'], { responseTimeout: 15000 }).its('response.statusCode').should('eq', 204);
+
+    // Check redirect to unit dashboard
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${unitId}/domains/${domainId}`);
+
+    // Test if widgets contain the assumed number of canvas elements
+    // (Canvas is used to draw charts if elements/objects exist,
+    // otherwise a regular div is used)
+    const widgets = [
+      { name: 'scope', numOfElements: 5 },
+      { name: 'process', numOfElements: 3 },
+      { name: 'asset', numOfElements: 3 },
+      { name: 'person', numOfElements: 2 },
+      { name: 'control', numOfElements: 1 },
+      { name: 'incident', numOfElements: 1 },
+      { name: 'document', numOfElements: 3 },
+      { name: 'scenario', numOfElements: 1 }
+    ];
+
+    widgets.forEach((widget) => {
+      cy.get(`[data-veo-test="domain-dashboard-${widget.name}-widget"]`).as('widget');
+      cy.get('@widget').within((_w) => {
+        cy.get('canvas').should('have.length', widget.numOfElements);
+      });
+    });
   });
 });
