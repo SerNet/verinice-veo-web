c5512059dfe697dad954d501f75490435a330d5a
jae <jae@sernet.de>
2024-05-30 07:51:57 +0200

    Cypress: Make debug logging configurable
    verinice-veo#2832

--- cypress/commands/requests.ts
+++ cypress/commands/requests.ts
@@ -28,6 +28,8 @@ export function veoRequest({
 }): any {
   const options = requestBody ? { method, json: requestBody } : { method };
 
+  if (Cypress.env('debug')) cy.log('veoRequest arguments:', JSON.stringify(arguments));
+
   // Make sure window.request is available
   if (waitForRequestMethod) {
     cy.visit('/security');
@@ -37,7 +39,7 @@ export function veoRequest({
 
   return cy.window().then((win) => {
     return win.request(url, options).then((response: any) => {
-      cy.log(JSON.stringify(response));
+      if (Cypress.env('debug')) cy.log(JSON.stringify(response));
       return response;
     });
   });
