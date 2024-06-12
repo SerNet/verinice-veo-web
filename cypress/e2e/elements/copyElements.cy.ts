66b0d40565dc658b5a88665dad95a5561b332cf9
azk <azk@sernet.de>
2024-06-13 00:41:59 +0200

    Add Copy Elements E2E Test
    verinice-veo#2724

---
+++ cypress/e2e/elements/copyElements.cy.ts
@@ -0,0 +1,105 @@
+describe('Copy elements', () => {
+  before(() => {
+    cy.login();
+    cy.importUnit({ fixturePath: 'units/test-unit-dsgvo.json' });
+  });
+
+  beforeEach(() => {
+    cy.viewport(1000, 1000);
+    cy.login();
+    cy.goToUnitSelection();
+    cy.acceptAllCookies();
+    cy.selectUnit();
+    cy.selectDomain('DS-GVO');
+  });
+
+  after(() => cy.deleteUnit());
+
+  const elementTypeList: string[] = ['Scope', 'Process', 'Asset', 'Person', 'Incident', 'Document', 'Scenario'];
+
+  elementTypeList.forEach((elementType) => {
+    const pluralizedElementType = elementType.toLowerCase() + (elementType === 'Process' ? 'es' : 's');
+
+    it('copies element in ' + elementType, () => {
+      handleLanguageBug();
+      navigateToElementType(elementType);
+
+      iterateSubTypes(elementType, ($subType) => {
+        cy.wrap($subType).click();
+        cy.wait(100);
+
+        selectOriginalRow();
+
+        cy.get('@originalRow').then(($row) => {
+          interceptCloneRequest(pluralizedElementType);
+          cy.intercept('GET', `${Cypress.env('veoApiUrl')}/domains/**/${pluralizedElementType}**`).as(
+            'getClonedElement'
+          );
+
+          cloneElement($row);
+          const cells = $row.children();
+          let texts = [];
+          cells.each((index, cell) => {
+            if (Cypress.$(cell).text()) texts.push(Cypress.$(cell).text());
+          });
+
+          texts.forEach((text) => {
+            cy.log(text); // This is just to log the texts, you can use them as needed
+          });
+          let name = texts[0];
+          let status = texts[1];
+          cy.wait('@cloneElement').its('response.statusCode').should('eq', 201);
+          cy.wait('@getClonedElement').its('response.statusCode').should('eq', 200);
+
+          verifyElementCopy(name, status);
+        });
+      });
+    });
+
+    function iterateSubTypes(elementType, callback) {
+      cy.contains('div[sub-group="true"] > div', new RegExp(`^${elementType}$`))
+        .should('be.visible')
+        .parent()
+        .find('a')
+        .each(($subType) => {
+          if ($subType.text() === 'All') return;
+          callback($subType);
+        });
+    }
+
+    const selectOriginalRow = () => {
+      cy.get('.v-data-table__tr') // Adjust this selector if needed to be more specific
+        .first()
+        .as('originalRow');
+    };
+
+    const handleLanguageBug = () => {
+      cy.get('nav[data-component-name="primary-navigation"]').then((body) => {
+        if (body.find('div[data-component-name="objects-nav-item"]:contains("Objects")').length === 0) {
+          cy.languageTo('English');
+        }
+      });
+    };
+
+    const navigateToElementType = (elementType) => {
+      cy.navigateTo(['Objects', elementType]);
+    };
+
+    const interceptCloneRequest = (pluralizedElementType) => {
+      cy.intercept('POST', `${Cypress.env('veoApiUrl')}/domains/**/${pluralizedElementType}`).as('cloneElement');
+    };
+
+    const cloneElement = ($row) => {
+      cy.wrap($row).find('[data-component-name="object-overview-clone-button"]').should('be.visible').click();
+    };
+
+    const verifyElementCopy = (name, status) => {
+      cy.get('tr')
+        .filter((index, row) => {
+          const rowText = Cypress.$(row).text();
+          return rowText.includes(`${name} (duplicated)`) && rowText.includes(status);
+        })
+        .should('be.visible');
+    };
+  });
+});
