78f71140ec5245258838f553a4a5d604e3381c3d
jae <jae@sernet.de>
2024-10-04 15:31:40 +0200

    e2e: send API calls without using `window.request`
    verinice-veo#3091

--- cypress/commands/requests.ts
+++ cypress/commands/requests.ts
@@ -12,35 +12,50 @@ declare global {
   }
 }
 
-/**
- * Wrap veo's `window.request` method into a cypress command.
- */
-export function veoRequest({
-  url,
-  method,
-  requestBody,
-  waitForRequestMethod = true
-}: {
-  url: string;
-  method: string;
-  requestBody?: any;
-  waitForRequestMethod?: boolean;
-}): any {
-  const options = requestBody ? { method, json: requestBody } : { method };
+type RequestHeaders = { Authorization: string; 'Content-type': string; Accept: string; 'If-Match': string };
+type RequestOptions = { endpoint: string; method?: string; headers?: RequestHeaders; body?: any };
 
-  if (Cypress.env('debug')) cy.log('veoRequest arguments:', (...args: any[]) => JSON.stringify(args));
-
-  // Make sure window.request is available
-  if (waitForRequestMethod) {
-    cy.visit('/security');
-    cy.get('[data-component-name="breadcrumbs"]').as('prepareVeoRequest');
-    cy.wait(1000);
-  }
-
-  return cy.window().then((win) => {
-    return win.request(url, options).then((response: any) => {
-      if (Cypress.env('debug')) cy.log(JSON.stringify(response));
+export function veoRequest(options: RequestOptions): Cypress.Chainable<any> {
+  const url = `${Cypress.env('veoApiUrl')}/${options.endpoint}`;
+  cy.log(url);
+  return getToken().then((token) => {
+    const opts = { method: 'GET', headers: generateHeaders(token), ...options, url };
+    return cy.request(opts).then((response) => {
       return response;
     });
   });
+}
+
+// veoRequest helpers
+function getToken(): Cypress.Chainable<any> {
+  const user = Cypress.env('testUser');
+  const keycloak = Cypress.env('keycloak');
+  const url = `${keycloak.baseUrl}/auth/realms/${keycloak.realm}/protocol/openid-connect/token`;
+
+  const body = {
+    username: user.name,
+    password: user.pw,
+    grant_type: 'password',
+    client_id: keycloak.clientId
+  };
+
+  const requestOptions = {
+    url,
+    body,
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' }
+  };
+
+  return cy.request(requestOptions).then((res) => {
+    return `Bearer ${res.body.access_token}`;
+  });
+}
+
+function generateHeaders(token: string, etag = '') {
+  return {
+    Authorization: token,
+    'If-Match': etag,
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  };
 }
