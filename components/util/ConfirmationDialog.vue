3f995218e2f192f053290e819cfb960d753711fa
Aziz Khalledi <azk@sernet.de>
2024-10-07 20:59:06 +0200

    Confirmation Dialog should not emit success
    verinice-veo#3029

--- components/util/ConfirmationDialog.vue
+++ components/util/ConfirmationDialog.vue
@@ -1,19 +1,18 @@
 <!--
-- verinice.veo web
-- Copyright (C) 2023 Jonas Heitmann, Frank Schneider
--
-- This program is free software: you can redistribute it and/or modify
-- it under the terms of the GNU Affero General Public License as published by
-- the Free Software Foundation, either version 3 of the License, or
-- (at your option) any later version.
--
-- This program is distributed in the hope that it will be useful,
-- but WITHOUT ANY WARRANTY; without even the implied warranty of
-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-- GNU Affero General Public License for more details.
--
-- You should have received a copy of the GNU Affero General Public License
-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   - verinice.veo web
+   - Copyright (C) 2024 Aziz Khalledi
+   - 
+   - This program is free software: you can redistribute it and/or modify it
+   - under the terms of the GNU Affero General Public License
+   - as published by the Free Software Foundation, either version 3 of the License,
+   - or (at your option) any later version.
+   - 
+   - This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+   - without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   - See the GNU Affero General Public License for more details.
+   - 
+   - You should have received a copy of the GNU Affero General Public License along with this program.
+   - If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
   <BaseDialog
@@ -27,20 +26,21 @@
     </template>
 
     <template #dialog-options>
-      <v-btn variant="text" @click="$emit('update:model-value', false)">
+      <v-btn variant="text" @click="cancel">
         {{ globalT('global.button.no') }}
       </v-btn>
       <v-spacer />
       <v-btn variant="text" color="primary" @click="handler">
         {{ confirmationText }}
       </v-btn>
+      <LayoutLoadingWrapper v-if="callbackExecuting" />
     </template>
   </BaseDialog>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-  callback: () => any;
+  callback: () => Promise<any>;
   confirmationText: string;
   modelValue: boolean;
   text: string;
@@ -48,19 +48,20 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits<{
-  (event: 'error', error: any): void;
-  (event: 'success'): void;
   (event: 'update:model-value', newValue: boolean): void;
 }>();
 
-const handler = () => {
-  try {
-    props.callback();
-    emit('success');
-    emit('update:model-value', false);
-  } catch (e: any) {
-    emit('error', e);
-  }
+const callbackExecuting = ref(false);
+
+// Handler for executing the callback
+const handler = async () => {
+  callbackExecuting.value = true;
+  await props.callback();
+  callbackExecuting.value = false;
+};
+
+const cancel = () => {
+  emit('update:model-value', false);
 };
 
 const { t: globalT } = useI18n({ useScope: 'global' });
