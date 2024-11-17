e78260350d2491c8395f29809b6d35166251bd5d
Aziz Khalledi <azk@sernet.de>
2024-11-17 19:37:06 +0100

    Cypress Risk Dialog: Add Scenario to Element
    verinice-veo#3318

--- cypress/e2e/elements/riskDialog.cy.ts
+++ cypress/e2e/elements/riskDialog.cy.ts
@@ -38,4 +38,40 @@ describe('Risk Dialog', { testIsolation: false }, () => {
     cy.wait(1000);
     cy.getCustom('[data-component-name="object-details-risks-tab"]').first().should('not.have.attr', 'disabled');
   });
+
+  it('Should add a Scenario to an Element', () => {
+    cy.getCustom('[data-component-name="object-details-risks-tab"]').first().click();
+    cy.getCustom('[data-component-name="object-details-actions-button"]').click();
+    cy.getCustom('.v-overlay__content').contains('Create risk').click();
+    cy.getCustom('[data-veo-test="dialog-card"]').as('container');
+
+    // Get the text of the parent row when clicking the checkbox
+    cy.getCustom('@container').within(() => {
+      cy.getCustom('[id^="checkbox-"]')
+        .filter('[aria-disabled="false"]')
+        .first()
+        .then(($checkbox) => {
+          // Traverse up to the parent <tr> and get the text
+          const $parentRow = $checkbox.closest('tr');
+          const selectedRiskText = $parentRow.find('td').eq(4).text().trim();
+          // Click the checkbox and wrap the text value
+          cy.wrap(selectedRiskText).as('selectedRiskText');
+          cy.wrap($checkbox).click();
+        });
+    });
+
+    // Intercept the POST request and wait for the response
+    cy.intercept('POST', `${Cypress.env('veoApiUrl')}/scopes/**/risks`).as('addRisk');
+    cy.containsCustom('create risk').should('exist').click();
+    cy.wait('@addRisk').its('response.statusCode').should('eq', 201);
+
+    // Verify that the selected risk is added to the data table
+    cy.getCustom('[data-veo-test="loadedDataTable"]').should('be.visible');
+    cy.getCustom('.v-data-table__tr.v-data-table__tr--clickable').should('have.length.greaterThan', 0);
+
+    // Check if the selected risk text is present in the added items
+    cy.get('@selectedRiskText').then((selectedRiskText) => {
+      cy.getCustom('.v-data-table__tr.v-data-table__tr--clickable').contains(selectedRiskText).should('exist');
+    });
+  });
 });
