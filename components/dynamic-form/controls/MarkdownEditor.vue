d81c9c2faa93e4e5199b8ad433ed86d8aec23712
Frank Schneider <fs@sernet.de>
2025-01-09 13:49:15 +0100

    update nuxt to latest version; update @nuxtjs/i18n and eslint to latest prior major;

--- components/dynamic-form/controls/MarkdownEditor.vue
+++ components/dynamic-form/controls/MarkdownEditor.vue
@@ -181,8 +181,7 @@ export default defineComponent({
 .vf-markdown-editor {
   position: relative;
   z-index: 0;
-
-  @import 'node_modules/@toast-ui/editor/dist/toastui-editor';
+  @import 'node_modules/@toast-ui/editor/dist/toastui-editor.css';
   @import 'prismjs/themes/prism';
   @import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight';
   .toastui-editor-contents h1,
