e3eebc50071747cb5a8c3b098047c2056c4f278e
Frank Schneider <fs@sernet.de>
2025-01-29 16:57:47 +0100

    compose the two shell-scripts created to extract translations into a single script

--- migrate_translations.sh
+++ migrate_translations.sh
@@ -2,7 +2,7 @@
 
 # This script extracts all local translations (<i18n>-Tags) from the SFCs into
 # separate json files located at <~locales/base/[components|pages]/[file].json>
-# and replaces these with a single <src-line>; e.g. *<i18n src="..."></i18n>*
+# It replaces these with a single <src-line>; e.g. *<i18n src="..."></i18n>*
 
 HEADLINE="T-A-G  E-X-T-R-A-C-T-O-R"
 SUB_DIRS=( 'components' 'pages' )
@@ -36,27 +36,29 @@ for FILE in "${SFC[@]}"; do
   STRIP_CWD=${FILE#*/}
 # delete everything but the first dir. Results in [<components> | <pages>]
   SUB_FOLDER=${STRIP_CWD%%/*}
-# strip off the <SUB_FOLDER> and replace all occurences of slashes and brackets
-  OUTPUT_FILE_NAME=$( echo ${FILE/vue/json} | /usr/bin/sed $SED_PARAM 's:.*(components|pages)/::; s:/:-:g; s:(\[|\])::g' )
+# strip off the path; replace all occurences of slashes by dashes; delete all brackets
+  FILE_NAME_SANITIZED=$( echo ${FILE/vue/json} | /usr/bin/sed $SED_PARAM 's:.*(components|pages)/::; s:/:-:g; s:(\[|\])::g' )
+# *kebab-case* the filename
+  LC_FILE_NAME=$(perl -ne 'print lc s|[[:lower:]]\K[[:upper:]]|-$&|rg' <<< $FILE_NAME_SANITIZED)
 
-  I18N_SRC="~/${TARGET_PATH#*/}$SUB_FOLDER/$OUTPUT_FILE_NAME"
-  JSON_FILE_PATH="${TARGET_PATH}$SUB_FOLDER/$OUTPUT_FILE_NAME"
+  I18N_SRC="~/${TARGET_PATH#*/}$SUB_FOLDER/$LC_FILE_NAME"
+  JSON_FILE_PATH="${TARGET_PATH}$SUB_FOLDER/$LC_FILE_NAME"
 
-  if [[ ! $FILE =~ 'error.vue' && ! $FILE =~ 'layouts' ]]; then
-    # extract the translations and save the result into a new json-file
-    /usr/bin/sed '/<i18n>/, /<\/i18n>/!d;//d' $FILE > $JSON_FILE_PATH
-    # delete the translations from the original SFC and replace it with a single line sourcing the JSON-file created above
-    if [[ $REPLY -eq 1 ]]; then
-      # GNU_SED on *nix
-      /usr/bin/sed -i -e '/<\/i18n>/a \\n<i18n src="'${I18N_SRC}'"></i18n>' -e '/<i18n>/, /<\/i18n>/d' $FILE
-    else
-      # BSD_SED on OS X
-      /usr/bin/sed -i -e '' '/<\/i18n/a\'$'\n''<i18n src="'${I18N_SRC}'"></i18n>' -e '/<i18n>/, /<\/i18n>/d' "$FILE"
-    fi
+  # extract the translations and save the result into a new json-file
+  /usr/bin/sed '/<i18n>/, /<\/i18n>/!d;//d' $FILE > $JSON_FILE_PATH
+
+  # delete the translations from the original SFC and replace it with a single line sourcing the JSON-file created above
+  if [[ $REPLY -eq 1 ]]; then
+    # GNU_SED on *nix
+    /usr/bin/sed -i -e '/<\/i18n>/a \\n<i18n src="'${I18N_SRC}'"></i18n>' -e '/<i18n>/, /<\/i18n>/d' $FILE
+  else
+    # BSD_SED on OS X
+    /usr/bin/sed -i '' -e '/<\/i18n>/a\'$'\n''<i18n src="'"${I18N_SRC}"'"></i18n>' \
+      -e '/<i18n>/,/<\/i18n>/d' "$FILE"
   fi
 done
 
-# output the amount of matches; ignore <error.vue> and <layouts/default.vue>
-echo "$(( ${#SFC[@]} - 2 )) files containing translations have been patched."
+# output the amount of matches
+echo "$(( ${#SFC[@]} )) files containing translations have been patched."
 
 exit 0
