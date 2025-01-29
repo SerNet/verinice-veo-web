fc217c09336318580b0ac98311a262281c121905
Aziz Khalledi <azk@sernet.de>
2025-01-29 11:18:02 +0100

    Cypress Test Object Editor Create+Import
    verinice-veo#3545

---
+++ cypress/e2e/editors/objectEditor.cy.ts
@@ -0,0 +1,51 @@
+import { visitEditor } from '../../commands/navigation';
+import { setupVeo } from '../../commands/setup';
+
+function assertEditorPage() {
+  cy.get('[data-component-name="breadcrumbs"]').contains('Objektschema editor', { matchCase: false });
+  cy.get('div').contains('Basic properties', { matchCase: false });
+}
+
+describe('Object Editor', () => {
+  before(() => {
+    setupVeo('ObjectEditor');
+  });
+  beforeEach(() => {
+    cy.login();
+    cy.acceptAllCookies();
+  });
+  after(() => cy.deleteUnit());
+
+  it('should set up the editor to create a new object schema', () => {
+    visitEditor();
+    cy.get('[data-veo-test="objectschema-item"]').click();
+    cy.get('[data-veo-test="dialog-card"]').within(() => {
+      cy.get('[data-veo-test="create-object-schema"]').click();
+      cy.get('[data-veo-test="object-schema-type"]')
+        .click()
+        .within(() => {
+          cy.get('input').type('test');
+        });
+
+      cy.get('[data-veo-test="object-schema-description"]')
+        .click()
+        .within(() => {
+          cy.get('input').type('test');
+        });
+      cy.get('[data-veo-test="create-object-schema-button"]').click();
+    });
+    assertEditorPage();
+  });
+
+  it('should import an object editor', () => {
+    visitEditor();
+    cy.get('[data-veo-test="objectschema-item"]').click();
+    cy.get('[data-veo-test="dialog-card"]').within(() => {
+      cy.get('[data-veo-test="import-object-schema"]').click();
+      cy.get('[data-veo-test="object-schema-select"]').click();
+    });
+    cy.getCustom('.v-overlay__content div[role="listbox"]').contains('scope').click();
+    cy.get('[data-veo-test="import-object-schema-button"]').click();
+    assertEditorPage();
+  });
+});
