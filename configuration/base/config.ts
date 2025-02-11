236e9d8ef4031d920864450a0eb825dd79e082db
jae <jae@sernet.de>
2025-02-11 15:49:01 +0100

    Update base config
    verinice-veo#3598

--- configuration/base/config.ts
+++ configuration/base/config.ts
@@ -1,22 +1,15 @@
 export const config = {
   riskAffectedObjectTypes: ['asset', 'process', 'scope'],
-  objectDetails: {
-    hasRisks: ['asset', 'process', 'scope']
-  },
   riEditor: {
     renderedProperties: {
       targetObject: [
         {
-          label: 'riskAffected',
+          label: 'targetObject',
           key: 'displayName'
         },
         {
           label: 'name',
           key: 'name'
-        },
-        {
-          label: 'id',
-          key: 'id'
         }
       ],
       control: [
@@ -25,20 +18,8 @@ export const config = {
           key: 'abbreviation'
         },
         {
-          label: 'designator',
-          key: 'designator'
-        },
-        {
-          label: 'protectionApproach',
-          key: 'protectionApproachTranslation'
-        },
-        {
           label: 'name',
           key: 'name'
-        },
-        {
-          label: 'origination',
-          key: 'origination'
         }
       ]
     }
