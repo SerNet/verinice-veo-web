1ef509d5e4bdea0574e9711061da96eb972f2723
Haneen <hh@sernet.de>
2025-02-28 19:27:30 +0100

    Inhancment Code
    verinice-veo#2329

--- components/layout/CookieBanner.vue
+++ components/layout/CookieBanner.vue
@@ -181,7 +181,6 @@ export default defineComponent({
   z-index: 6;
 }
 body:has(.v-navigation-drawer--active) .veo-cookie-banner {
-  flex: 1 0 auto;
   max-width: 100%;
   transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
   padding-left: var(--v-layout-left);
