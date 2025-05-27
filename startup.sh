df4ac516ab83182d1a7cde0026d0dbdc81319192
Haneen <hh@sernet.de>
2025-05-27 11:32:57 +0200

    configuration for the deployment
    verinice-veo#3873

--- startup.sh
+++ startup.sh
@@ -13,4 +13,5 @@ sed -i "s,veo-oidcclient-example,$VEO_OIDC_CLIENT,g" $file
 sed -i "s,veo-documentation-url-example,$VEO_DOCUMENTATION_URL,g" $file
 sed -i "s,veo-beta-mode-example,$VEO_BETA_MODE,g" $file
 sed -i "s,veo-feature-flag-card-view-example,$VEO_FEATURE_FLAG_CARD_VIEW,g" $file
+sed -i "s,veo-feature-flag-user-settings-example,$VEO_FEATURE_FLAG_USER_SETTINGS,g" $file
 nginx -c /etc/nginx/conf.d/custom.conf -g 'pid /tmp/nginx.pid; daemon off;'
