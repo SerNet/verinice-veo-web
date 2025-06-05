481eec274b8e8443b93f7d10822c9c1e772a5824
jae <jae@sernet.de>
2025-06-05 12:25:34 +0200

    Improve, rename, move `riskLegends` component
    verinice-veo#3769

---
+++ components/risk/Property.vue
@@ -0,0 +1,75 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2024 Aziz Khalledi
+   -
+   - This program is free software: you can redistribute it and/or modify it
+   - under the terms of the GNU Affero General Public License
+   - as published by the Free Software Foundation, either version 3 of the License,
+   - or (at your option) any later version.
+   -
+   - This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+   - without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   - See the GNU Affero General Public License for more details.
+   -
+   - You should have received a copy of the GNU Affero General Public License along with this program.
+   - If not, see <http://www.gnu.org/licenses/>.
+-->
+<template>
+  <div class="my-2">
+    <div class="my-2 d-flex align-center">
+      <v-card-subtitle class="pr-0">{{ title }} </v-card-subtitle>
+    </div>
+
+    <div class="px-3">
+      <v-tooltip
+        v-for="item in items"
+        :key="item.ordinalValue"
+        max-width="400px"
+        top
+        :text="
+          (item.translations[locale] && item.translations[locale].description) ||
+          Object.values(item.translations)[0].description
+        "
+      >
+        <template #activator="{ props }">
+          <v-chip
+            v-bind="props"
+            label
+            outlined
+            size="small"
+            :style="{
+              marginLeft: '1px',
+              marginRight: '1px',
+              marginTop: '1px',
+              marginBottom: '1px',
+              backgroundColor: item.htmlColor,
+              color: getMostContrastyColor(item.htmlColor)
+            }"
+          >
+            {{
+              (item.translations[locale] && item.translations[locale].name) || Object.values(item.translations)[0].name
+            }}&nbsp;</v-chip
+          >
+        </template>
+      </v-tooltip>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getMostContrastyColor } from '~/lib/utils';
+import type { IVeoRiskPotentialImpact, IVeoRiskProbabilityLevel } from '~/types/VeoTypes';
+
+defineProps({
+  items: {
+    type: (Array as PropType<IVeoRiskPotentialImpact[]>) || (Array as PropType<IVeoRiskProbabilityLevel[]>),
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+});
+
+const { locale } = useI18n();
+</script>
--- lib/utils.ts
+++ lib/utils.ts
@@ -154,3 +154,17 @@ export const OBJECT_TYPE_SORT_ORDER = [
   'control',
   'scenario'
 ];
+
+export function getMostContrastyColor(backgroundColor: string) {
+  const CONTRAST_THRESHOLD = 90;
+  if (backgroundColor) {
+    const hex = backgroundColor.substring(1);
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+
+    return r * 0.299 + g * 0.587 + b * 0.114 > CONTRAST_THRESHOLD ? '#000000' : '#ffffff';
+  } else {
+    return '#000000';
+  }
+}
