fe7de600f3a647d9f0d0fffbacb7ce55a8624d69
jae <jae@sernet.de>
2025-06-06 14:14:19 +0200

    Move risk.vue, add proper redirect and error message
    verinice-veo#3769

--- pages/[unit]/domains/[domain]/risks/index.vue
+++ pages/[unit]/domains/[domain]/risks/index.vue
@@ -1,21 +1,55 @@
 <!--
    - verinice.veo web
-   - Copyright (C) 2022  Jonas Heitmann
-   - 
+   - Copyright (C) 2025 jae
+   -
    - This program is free software: you can redistribute it and/or modify
    - it under the terms of the GNU Affero General Public License as published by
    - the Free Software Foundation, either version 3 of the License, or
    - (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful,
    - but WITHOUT ANY WARRANTY; without even the implied warranty of
    - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    - GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License
    - along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <span>Hello world</span
-  ><!-- This page is never visible, however the breadcrumbs and navigation need it for some reason -->
+  <BasePage data-component-name="unit-domains-domain-risks-page">
+    <v-skeleton-loader v-if="isLoadingDomain" type="table" />
+    <UtilNotFoundError v-else has-dashboard-button />
+  </BasePage>
 </template>
+<script setup lang="ts">
+const route = useRoute();
+const router = useRouter();
+const { data: currentDomain, isLoading: isLoadingDomain } = useCurrentDomain();
+
+const riskDefintionId = computed(() => {
+  if (isLoadingDomain.value || !currentDomain.value?.riskDefinitions) {
+    return;
+  }
+  const riskDefinitions = Object.keys(currentDomain.value?.riskDefinitions);
+  return currentDomain.value?.riskDefinitions?.[riskDefinitions?.[0]]?.id;
+});
+
+watch(
+  riskDefintionId,
+  () => {
+    if (!riskDefintionId.value || isLoadingDomain.value) {
+      return;
+    }
+
+    return router.replace({
+      name: 'unit-domains-domain-risks-definition',
+      params: {
+        ...route.params,
+        definition: riskDefintionId.value
+      }
+    });
+  },
+  { immediate: true }
+);
+</script>
+<i18n src="~/locales/base/pages/unit-domains-domain-risks.json"></i18n>
