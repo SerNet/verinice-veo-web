fc20d67db7065ad3177458c533cc3be0cfd76ca8
Haneen <hh@sernet.de>
2025-06-06 10:04:01 +0200

    add Compliance accessibility test file
    verinice-veo#3867

---
+++ cypress/e2e/accessibility/checkCompliancepage.cy.ts
@@ -0,0 +1,22 @@
+import { visitRIList } from '../../commands/navigation';
+import { setupVeo } from '../../commands/setup';
+import { applyCatalogItem } from '../../requests/catalogs';
+import { addModule } from '../../requests/control-implementations';
+
+describe('Compliance', () => {
+  beforeEach(() => {
+    cy.injectAxe();
+    // Setup API
+    setupVeo('workWithRis').then(() => {
+      applyCatalogItem();
+      addModule();
+    });
+  });
+
+  it('check Accessibility in Compliance', () => {
+    visitRIList();
+    cy.checkAxeViolations();
+    cy.getCustom('.v-data-table__tr').first().click();
+    cy.checkAxeViolations();
+  });
+});
