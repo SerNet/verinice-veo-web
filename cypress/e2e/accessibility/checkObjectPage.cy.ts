906dac1802909abebb64e75391f4774152a83ff4
Haneen <hh@sernet.de>
2025-06-13 12:24:24 +0200

    Add an object accessibility test file to check accessibility on the object page
    verinice-veo#3867

---
+++ cypress/e2e/accessibility/checkObjectPage.cy.ts
@@ -0,0 +1,75 @@
+import { createObject } from '../../requests/objects';
+import { generateUnitDetails, UnitDetails } from '../../support/setupHelpers';
+import { Actions, ElementActions, Tabs } from '../elements/elementHelpers';
+
+let unitDetails: UnitDetails;
+type TabConfig = {
+  tab: Tabs;
+  actions?: Actions[];
+};
+const tabConfigs: TabConfig[] = [
+  {
+    tab: 'parentScopes',
+    actions: ['Create scope', 'Select scope']
+  },
+  {
+    tab: 'links'
+  },
+  {
+    tab: 'risks',
+    actions: ['Create risk']
+  },
+  {
+    tab: 'childScopes',
+    actions: ['Create scope', 'Select scope']
+  },
+  {
+    tab: 'childObjects',
+    actions: ['Create object', 'Select object']
+  }
+];
+const SideBarActions = ['view', 'toc', 'history', 'messages'];
+describe('check Accessibility', () => {
+  before(() => {
+    unitDetails = generateUnitDetails('ElementsDetailsTab');
+    cy.importUnit(unitDetails.name, { fixturePath: 'units/test-unit-dsgvo.json' });
+    cy.login();
+    cy.goToUnitSelection();
+    cy.acceptAllCookies();
+    cy.selectUnit(unitDetails.name);
+    cy.injectAxe();
+  });
+  it('check Accessibility in object', () => {
+    createObject({
+      objectData: {
+        owner: {},
+        riskDefinition: 'DSRA',
+        name: 'test-object-name',
+        objectType: 'scope',
+        objectTypePlural: 'scopes',
+        subType: 'SCP_Scope',
+        subTypePlural: 'Scopes',
+        status: 'NEW'
+      }
+    });
+    cy.visitObject();
+
+    cy.checkAxeViolations();
+
+    // Test each tab configuration
+
+    tabConfigs.forEach(({ tab, actions }) => {
+      cy.get(`[data-component-name="object-details-${tab}-tab"]`).click();
+
+      cy.checkAxeViolations();
+      if (actions?.length) {
+        ElementActions.verifyAndPerformTabActions(tab, actions);
+      }
+    });
+
+    SideBarActions.forEach((tab) => {
+      cy.get(`[data-component-name="object-form-${tab}-tab"]`).click();
+      cy.checkAxeViolations();
+    });
+  });
+});
--- cypress/e2e/elements/elementHelpers.ts
+++ cypress/e2e/elements/elementHelpers.ts
@@ -20,7 +20,8 @@ export const ElementActions = {
   selectElement: (isRisk: boolean = false) => {
     // Find the dialog container
     cy.getCustom('[data-veo-test="dialog-card"]').as('container');
-
+    cy.injectAxe();
+    cy.checkAxeViolations();
     // Find the first enabled row and check its checkbox
     cy.getCustom('@container')
       .find('tbody.v-data-table__tbody tr.v-data-table__tr')
@@ -48,6 +49,8 @@ export const ElementActions = {
   createElement: (status: Status) => {
     cy.getCustom('[data-veo-test="dialog-card"]').as('container');
     cy.log(testData.element.abbreviation);
+    cy.injectAxe();
+    cy.checkAxeViolations();
     cy.getCustom('@container').find('input[id="#/properties/abbreviation"]').type(testData.element.abbreviation);
     cy.getCustom('@container').find('input[id="#/properties/name"]').type(testData.element.name);
     cy.getCustom('div[data-attribute-name="subType"]').click();
