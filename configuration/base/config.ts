c786f389cb685569ed15573185fd240bde63e535
jae <jae@sernet.de>
2025-07-21 10:39:21 +0200

    Add color for new ISO domain name
    verinice-veo#4027

--- configuration/base/config.ts
+++ configuration/base/config.ts
@@ -3,7 +3,8 @@ export const config = {
   domains: {
     colors: {
       'ds-gvo': '#c90000',
-      'iso-iec-27000': '#003f63',
+      'iso-iec-27000': '#003f63', // This domain name is deprecated (ISO/IEC 27000)
+      'iso-27001-de': '#003f63',
       nis2: '#9c26af',
       'it-grundschutz': '#2E7D32',
       'it-grundschutz++': '#009192',
