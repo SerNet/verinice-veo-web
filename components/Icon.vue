ddfb22c8027aeb4715bd414f971d68cf75bccb08
Aziz Khalledi <azk@sernet.de>
2025-08-21 14:40:06 +0200

    Add simple Icon Component with tooltip
    verinice-veo#3523

---
+++ components/Icon.vue
@@ -0,0 +1,44 @@
+<!--
+  - verinice.veo web
+  - Generic Icon with Tooltip component
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<template>
+  <v-tooltip location="top" :aria-label="translatedTooltip">
+    <template #activator="{ props: tooltipProps }">
+      <div
+        v-bind="tooltipProps"
+        :style="{
+          color: 'inherit',
+          textDecoration: 'none'
+        }"
+      >
+        <v-icon v-bind="$attrs" :icon="icon" />
+      </div>
+    </template>
+    <template #default>
+      {{ translatedTooltip }}
+    </template>
+  </v-tooltip>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  icon: string | string[];
+  tooltipTranslation: string;
+}>();
+
+const translatedTooltip = computed(() => props.tooltipTranslation);
+</script>
