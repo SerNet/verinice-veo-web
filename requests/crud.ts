6a9f18ec003b290c4d8549a7e3c19cc73d4064d0
jae <jae@sernet.de>
2025-09-08 11:46:05 +0200

    Add basic fn for fetch calls
    verinice-veo#4175

---
+++ requests/crud.ts
@@ -0,0 +1,65 @@
+import { omit } from 'lodash';
+
+type RequestHeaders = {
+  Authorization?: string;
+  'If-Match'?: string;
+  'Content-Type'?: 'application/json';
+  Accept?: string;
+};
+
+type RequestOptions = {
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  headers?: RequestHeaders;
+  token?: string;
+  body?: any;
+};
+
+const etags = new Map<string, string>();
+
+export async function read({
+  path,
+  options = {},
+  callback
+}: {
+  path: string;
+  options?: RequestOptions;
+  callback?: () => Promise<void>;
+}) {
+  const config = useRuntimeConfig();
+  const url = config.public.apiUrl + path;
+
+  const { token } = useVeoUser();
+
+  const opts: RequestOptions = {
+    method: 'GET',
+    headers: generateHeaders(token.value, options.headers ? options.headers : {}),
+    ...omit(options, 'headers')
+  };
+
+  // Do we need more error handling?
+  const response = await fetch(url, opts);
+
+  await callback?.();
+
+  if (!response.ok) {
+    throw new Error('Response was not ok:', {
+      cause: await response.text()
+    });
+  }
+
+  // Store etag for later use (e.g. in update)
+  const etag = response.headers.get('ETag') || '';
+  etags.set(path, etag);
+
+  return await response.json();
+}
+
+// Utils
+function generateHeaders(token: string, headers: RequestHeaders = {}, etag = ''): RequestHeaders {
+  return {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    ...headers
+  };
+}
