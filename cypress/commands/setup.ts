71032b8cd6133ed6c6a15437016c12373c60d234
jae <jae@sernet.de>
2025-09-18 09:19:45 +0200

    Try deleting previous test unit before setting up next test
    verinice-veo#4214

--- cypress/commands/setup.ts
+++ cypress/commands/setup.ts
@@ -12,17 +12,19 @@ export const objectDataDefaults = {
 };
 
 export function setupVeo(unitName?: string, domains: string[] = ['IT-Grundschutz'], objectData = objectDataDefaults) {
-  const unitDetails = generateUnitDetails(unitName || 'Veo');
-  return cy.createUnit({ name: unitDetails.name, desc: unitDetails.desc, domains: domains }).then(() => {
-    domains.forEach((domain) => {
-      createObject({
-        objectData: {
-          riskDefinition: domain === 'DS-GVO' ? 'DSRA' : 'GSRA',
-          ...objectData
-        }
+  return cy.deleteTestUnits().then(() => {
+    const unitDetails = generateUnitDetails(unitName || 'Veo');
+    cy.createUnit({ name: unitDetails.name, desc: unitDetails.desc, domains: domains }).then(() => {
+      domains.forEach((domain) => {
+        createObject({
+          objectData: {
+            riskDefinition: domain === 'DS-GVO' ? 'DSRA' : 'GSRA',
+            ...objectData
+          }
+        });
       });
+      cy.login();
+      cy.acceptAllCookies();
     });
-    cy.login();
-    cy.acceptAllCookies();
   });
 }
