0a74bb0670e5f8e57fc0b58d0624af2c40c279a7
jae <jae@sernet.de>
2025-09-23 14:43:54 +0200

    Rewrite and reactivate e2e test

--- cypress/e2e/elements/addElementToOtherDomain.cy.ts
+++ cypress/e2e/elements/addElementToOtherDomain.cy.ts
@@ -1,148 +1,68 @@
-import { getRandomElementType } from '../../commands/utils';
-
-describe.skip('Add Elements to other Domain', () => {
-  before(() => {
-    cy.importUnit({ fixturePath: 'units/test-unit-dsgvo.json' }).then(() => {
-      cy.login();
-      cy.goToUnitSelection();
-      cy.addDomain(Cypress.env('dynamicTestData').testUnits[0].name, 'IT-Grundschutz');
-    });
-  });
-
+describe('Add Elements to other Domain', () => {
   beforeEach(() => {
-    cy.login();
-    cy.goToUnitSelection();
-    cy.acceptAllCookies();
-    cy.selectUnit(Cypress.env('dynamicTestData').testUnits[0].name);
+    cy.setupVeo('addElementToOtherDomain', ['DS-GVO', 'IT-Grundschutz']).then(() => {
+      const unitId = Cypress.env('dynamicTestData').testUnits[0].unitId;
+      const domainId = Cypress.env('dynamicTestData').testUnits[0].domains.find((d) => d.name === 'DS-GVO').id;
+      cy.visit(`${unitId}/domains/${domainId}/scopes/SCP_Scope`, { failOnStatusCode: false });
+    });
   });
 
-  const elementTypeList: string[] = ['Scopes', getRandomElementType()];
+  it('adds an element of type SCOPE to IT-GS', () => {
+    const targetDomain = 'IT-Grundschutz';
+    cy.getCustom('[data-veo-test="object-overview-assign-button"]').click();
 
-  elementTypeList.forEach((elementType) => {
-    it('Add Element of type ' + elementType + ' to other Domain', () => {
-      navigateToElementType(elementType);
-      const targetDomain = 'IT-Grundschutz';
-      let targetObjects = []; // Initialize array to store target objects and statuses
-      let originalElementName;
+    // Get dialog content
+    cy.getCustom('[data-veo-test="dialog-card"]').as('container');
 
-      cy.selectFirstSubType(elementType, ($subType) => {
-        cy.wrap($subType).click();
-
-        cy.checkSubTypePage($subType[0].innerText);
-
-        cy.getCustom('.v-data-table__tr').first().as('originalRow');
-
-        cy.getCustom('@originalRow').then(($row) => {
-          const cells = $row.children();
-          const texts = [];
-          cells.each((index, cell) => {
-            texts.push(Cypress.$(cell).text());
-          });
-          originalElementName = texts[2];
-
-          cy.wrap($row)
-            .find('[data-veo-test="object-overview-assign-button"]')
-            .should('exist')
-            .then(($button) => {
-              cy.wrap($button).scrollIntoView().should('be.visible').click();
-            });
-          cy.wait(200);
-        });
-
-        cy.getCustom('[data-veo-test="dialog-card"]').as('container');
-
-        cy.getCustom('@container')
-          .find('.v-list-item.v-list-item--link:not(.v-list-item--disabled)')
-          .each(($item) => {
-            const text = $item.find('.v-list-item-title').text().trim();
-            if (text === targetDomain) {
-              cy.wait(200);
-
-              cy.wrap($item).find('.v-icon--clickable').click({ force: true });
-
-              cy.wrap($item).find('[data-veo-test="subtype-select"] .v-field').first().click({ force: true });
-              cy.wait(200);
-
-              cy.getCustom('.v-menu')
-                .should('be.visible')
-                .first()
-                .within(() => {
-                  cy.getCustom('.v-list-item:not(.v-list-item--disabled)').then(($dropdownItems) => {
-                    const randomIndex = Math.floor(Math.random() * $dropdownItems.length);
-                    const $randomItem = $dropdownItems.eq(randomIndex);
-                    const targetObject = $randomItem.find('.v-list-item-title').text().trim();
-                    cy.wait(200);
-                    cy.wrap($randomItem).click();
-                    targetObjects.push({ object: targetObject });
-                  });
-                });
-
-              cy.wrap($item).find('[data-veo-test="status-select"] .v-field').first().click({ force: true });
-              cy.getCustom('.v-menu')
-                .should('be.visible')
-                .first()
-                .within(() => {
-                  cy.getCustom('.v-list-item:not(.v-list-item--disabled)').then(($dropdownItems) => {
-                    const randomIndex = Math.floor(Math.random() * $dropdownItems.length);
-                    const $randomItem = $dropdownItems.eq(randomIndex);
-                    const targetObjectStatus = $randomItem.find('.v-list-item-title').text().trim();
-                    cy.wrap($randomItem).click();
-
-                    targetObjects[targetObjects.length - 1].status = targetObjectStatus;
-                    targetObjects[targetObjects.length - 1].name = originalElementName;
-                  });
-                });
-              cy.getCustom('@container').contains('button', 'Save').should('exist').click();
-              veoAlertDismiss();
-            }
-          });
-      });
-
-      cy.then(() => {
-        cy.selectDomain(targetDomain);
-        navigateToElementType(elementType);
-        let _foundCount = 0;
-        targetObjects.forEach((target) => {
-          const targetObject = target.object;
-          const targetObjectStatus = target.status;
-          const targetObjectName = target.name;
-          let targetSubType;
-          const normalizeString = (str) => str.toLowerCase().replace(/[^a-z]/g, '');
-          cy.containsCustom('div[sub-group="true"] > div', new RegExp(`^${elementType}$`))
-            .should('be.visible')
-            .parent()
-            .find('a')
-            .then(($subTypes) => {
-              const $matchingSubType = $subTypes.filter((index, $subType) => {
-                return normalizeString(Cypress.$($subType).text().trim()) === normalizeString(targetObject);
-              });
-              if ($matchingSubType.length > 0) {
-                targetSubType = $matchingSubType.first();
-                cy.wrap(targetSubType).click();
-              }
-            });
-
-          cy.wait(200);
-
-          cy.getCustom('tr')
-            .filter(`:contains("${targetObjectName}")`)
-            .filter(`:contains("${targetObjectStatus}")`)
-            .then(($rows) => {
-              if ($rows.length === 1) {
-                _foundCount++;
-              }
-            });
-        });
-        targetObjects = [];
-      });
+    cy.get('@container').within(() => {
+      // Regex matches exact text
+      cy.get('.v-list-item')
+        .contains(new RegExp('^' + targetDomain + '$', 'g'))
+        .parent()
+        .parent()
+        .as('DomainItem');
     });
 
-    const navigateToElementType = (elementType: string) => {
-      cy.navigateTo({ group: 'objects', category: elementType });
-    };
+    // Select domain
+    cy.get('@DomainItem')
+      .within(() => {
+        cy.get('.v-icon--clickable').click();
+      })
+      .as('DomainItemSelected');
 
-    const veoAlertDismiss = () => {
-      cy.getCustom('div[role="alert"][params="[object Object]"]').click();
-    };
+    // Fill in subtype and status
+    cy.get('@DomainItemSelected')
+      .within(() => {
+        cy.get('[data-veo-test="subtype-select"]').click();
+      })
+      .then(() =>
+        cy
+          .get('.v-menu .v-list-item')
+          .contains('Scope')
+          .then(($item) => $item[0].click())
+      );
+
+    cy.get('@DomainItemSelected')
+      .within(() => {
+        cy.getCustom('[data-veo-test="status-select"]').click();
+      })
+      .then(() =>
+        cy
+          .getCustom('.v-menu .v-list-item')
+          .contains('New')
+          .then(($item) => $item[0].click())
+      );
+
+    // Save
+    cy.getCustom('button').contains('Save').click();
+
+    // Verify that the element now appears in the target domain
+    const unitId = Cypress.env('dynamicTestData').testUnits[0].unitId;
+    const domainId = Cypress.env('dynamicTestData').testUnits[0].domains.find((d) => d.name === targetDomain).id;
+    cy.visit(`${unitId}/domains/${domainId}/scopes/SCP_Scope`, { failOnStatusCode: false });
+
+    return cy
+      .getCustom('[data-veo-test="loadedDataTable"] tbody tr')
+      .then(($rows) => expect($rows.length).to.be.equal(2));
   });
 });
