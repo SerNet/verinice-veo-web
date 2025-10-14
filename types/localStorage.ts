b0ca4af6e177c4239f7716c9b09c032ea628535e
jae <jae@sernet.de>
2025-10-14 08:04:49 +0200

    Remove unused code

--- types/localStorage.ts
+++ types/localStorage.ts
@@ -24,6 +24,5 @@ export const LOCAL_STORAGE_KEYS = {
   LAST_UNIT: 'last-unit',
   PRIMARY_NAV_MINI_VARIANT: 'primary-navigation-drawer--mini-variant',
   FAVORITE_UNIT: 'favorite-unit',
-  FAVORITE_UNIT_DOMAIN: 'favorite-unit-domain',
-  FEATURE_FLAGS: 'feature-flags'
+  FAVORITE_UNIT_DOMAIN: 'favorite-unit-domain'
 };
