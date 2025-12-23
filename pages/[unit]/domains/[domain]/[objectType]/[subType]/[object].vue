9d90293793a3e2427450161a5be452428871743b
Haneen <hh@sernet.de>
2025-12-23 11:02:52 +0100

    prevent dirty state when loading object history
    verinice-veo#4322
    
    edit resetobject type
    
    verinice-veo#4322
    
    reset baseObject after loading data
    
    verinice-veo#4322

--- pages/[unit]/domains/[domain]/[objectType]/[subType]/[object].vue
+++ pages/[unit]/domains/[domain]/[objectType]/[subType]/[object].vue
@@ -228,6 +228,8 @@ const canManageUnitContent = computed(() =>
 );
 
 const modifiedObject = ref<IVeoEntity | undefined>(undefined);
+const baseObject = ref<IVeoEntity | undefined>(undefined);
+
 // Data that should get merged back into modifiedObject after the object has been reloaded, useful to persist children of objects while keeping form changes
 const wipObjectData = ref<Record<string, any> | undefined>(undefined);
 
@@ -264,6 +266,7 @@ const sourceIconColor = computed<string | undefined>(() => (appliedCatalogItem.v
 // Function to update modifiedObject with fetched data
 function finalizeModifiedObject(data?: IVeoEntity) {
   modifiedObject.value = cloneDeep(data);
+  baseObject.value = cloneDeep(data);
   // On the next tick, object is populated so disabling subtype will work
   nextTick(getAdditionalContext);
 
@@ -304,7 +307,11 @@ const onPageCollapsed = (collapsedPages: boolean[]) => {
 const objectType = computed(() => VeoElementTypesSingular[route.params.objectType as string]);
 
 const isFormDirty = computed(
-  () => !isEqual(object.value as IVeoEntity, modifiedObject.value as IVeoEntity) && !formDataIsRevision.value
+  () =>
+    baseObject.value &&
+    modifiedObject.value &&
+    !isEqual(baseObject.value as IVeoEntity, modifiedObject.value as IVeoEntity) &&
+    !formDataIsRevision.value
 );
 const isFormValid = ref(false);
 const objectForm = ref();
@@ -412,7 +419,6 @@ function onShowRevision(data: IVeoObjectHistoryEntry, isRevision: boolean) {
   const displayRevisionCallback = () => {
     formDataIsRevision.value = isRevision;
     entityModifiedDialogVisible.value = false;
-
     // We have to stringify the content and then manually add the host, as the history api currently doesn't support absolute urls 18-01-2022
     if (isRevision) {
       modifiedObject.value = JSON.parse(JSON.stringify(data.content).replaceAll(/"\//g, `"${config.public.apiUrl}/`));
@@ -423,6 +429,7 @@ function onShowRevision(data: IVeoObjectHistoryEntry, isRevision: boolean) {
     modifiedObject.value.displayName = `${data.content.designator} ${data.content.abbreviation || ''} ${
       data.content.name
     }`;
+    baseObject.value = cloneDeep(modifiedObject.value);
     version.value = data.changeNumber;
   };
   if (isFormDirty.value) {
