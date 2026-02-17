de97cb80576e68a375cd8a3a40913bc4308c076e
dmirosavljevic <djm@sernet.de>
2026-02-17 11:26:09 +0100

    fix a11y, change to v-btn
    verinice-veo#4551

--- components/navigation/NavigationDrawerItems.vue
+++ components/navigation/NavigationDrawerItems.vue
@@ -1,5 +1,23 @@
+<!--
+   - verinice.veo web
+   - Copyright (C) 2026  Djordje Mirosavljevic
+   -
+   - This program is free software: you can redistribute it and/or modify
+   - it under the terms of the GNU Affero General Public License as published by
+   - the Free Software Foundation, either version 3 of the License, or
+   - (at your option) any later version.
+   -
+   - This program is distributed in the hope that it will be useful,
+   - but WITHOUT ANY WARRANTY; without even the implied warranty of
+   - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   - GNU Affero General Public License for more details.
+   -
+   - You should have received a copy of the GNU Affero General Public License
+   - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <template>
-  <div v-if="miniVariant && !xs" key="mini" role="menu">
+  <div v-if="miniVariant && !xs" key="mini">
     <v-menu
       v-if="item.children && item.children.length > 0"
       v-model="isSubMenuOpen"
@@ -10,31 +28,26 @@
       <template #activator="{ props: menuProps }">
         <v-tooltip :text="item.name" location="right" offset="10">
           <template #activator="{ props: tooltipProps }">
-            <v-list-item
+            <v-btn
               v-bind="mergeProps(menuProps, tooltipProps)"
-              variant="text"
-              :class="[
-                'mb-2 d-flex justify-center rounded-lg mx-auto',
-                { 'v-list-item--active text-primary': isItemSelected }
-              ]"
-              :style="{ width: '44px', height: '44px' }"
               :aria-label="item.name"
-              aria-haspopup="menu"
-              role="menuitem"
+              aria-haspopup="true"
+              :aria-expanded="isSubMenuOpen"
+              :class="['mb-2 rounded-lg mx-auto d-flex', { 'text-primary': isItemSelected }]"
+              :style="{ width: '44px', height: '44px' }"
+              variant="text"
+              icon
             >
-              <div class="d-flex align-center justify-center w-100 h-100" aria-hidden="true">
-                <v-icon :icon="item.icon" size="22" />
-              </div>
-            </v-list-item>
+              <v-icon :icon="item.icon" size="22" aria-hidden="true" />
+            </v-btn>
           </template>
         </v-tooltip>
       </template>
 
       <v-card min-width="220" elevation="12" class="border pa-1 bg-surface shadow-xl">
-        <v-list density="compact" class="py-0" role="menu" :aria-label="item.name">
+        <v-list density="compact" class="py-0" :aria-label="item.name">
           <template v-for="child in item.children" :key="child.id">
             <NavigationPrimaryNavigationCategory v-if="child.children" v-bind="child" :mini-variant="false" />
-
             <NavigationPrimaryNavigationEntry
               v-else
               v-bind="child"
@@ -48,28 +61,23 @@
 
     <v-tooltip v-else :text="item.name" location="right" offset="10">
       <template #activator="{ props: tooltipProps }">
-        <v-list-item
+        <v-btn
           v-bind="tooltipProps"
           :to="item.to"
-          link
-          variant="text"
-          :class="[
-            'mb-2 d-flex justify-center rounded-lg mx-auto',
-            { 'v-list-item--active text-primary': isItemSelected }
-          ]"
-          :style="{ width: '44px', height: '44px' }"
           :aria-label="item.name"
-          role="menuitem"
+          :aria-current="isItemSelected ? 'page' : undefined"
+          :class="['mb-2 rounded-lg mx-auto d-flex', { 'text-primary': isItemSelected }]"
+          :style="{ width: '44px', height: '44px' }"
+          variant="text"
+          icon
         >
-          <div class="d-flex align-center justify-center w-100 h-100" aria-hidden="true">
-            <v-icon :icon="item.icon" size="22" />
-          </div>
-        </v-list-item>
+          <v-icon :icon="item.icon" size="22" aria-hidden="true" />
+        </v-btn>
       </template>
     </v-tooltip>
   </div>
 
-  <div v-else key="expanded" role="group">
+  <div v-else key="expanded">
     <NavigationPrimaryNavigationCategory v-if="item.children" v-bind="item" :mini-variant="false" />
     <NavigationPrimaryNavigationEntry v-else v-bind="item" :mini-variant="false" />
   </div>
@@ -87,7 +95,6 @@ const props = defineProps<{
 
 const { xs } = useDisplay();
 const route = useRoute();
-
 const isSubMenuOpen = ref(false);
 
 watch(
@@ -105,9 +112,7 @@ const isItemSelected = computed(() => {
   };
   const target = parentMaps[props.item.id];
   const routeName = route.name?.toString() || '';
-
   if (props.item.to?.name === route.name) return true;
-
   return !!(target && routeName.includes(target));
 });
 </script>
