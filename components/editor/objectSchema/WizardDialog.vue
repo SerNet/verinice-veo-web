2680b4b24b5361238fec4b4643e60f465b29485a
Frank Schneider <fs@sernet.de>
2026-02-24 11:51:57 +0100

    delete everything related to object schema creation

--- components/editor/objectSchema/WizardDialog.vue
+++ components/editor/objectSchema/WizardDialog.vue
@@ -27,71 +27,21 @@
     <template #default>
       <v-window v-model="state">
         <v-window-item value="start" class="py-4">
-          <h3 class="text-h3">
-            {{ t('start') }}
-          </h3>
           <v-list class="px-0 overflow-hidden" color="transparent">
-            <v-list-item lines="two" data-veo-test="create-object-schema" @click="state = 'create'">
+            <v-list-item lines="two" data-veo-test="open-object-schema" @click="state = 'import'">
               <v-list-item-title class="font-weight-bold">
-                {{ t('createObjectSchema') }}
+                {{ t('openObjectSchema') }}
               </v-list-item-title>
-              <v-list-item-subtitle>{{ t('createObjectSchemaDescription') }}</v-list-item-subtitle>
-              <template #append>
-                <v-icon size="x-large" :icon="mdiChevronRight" />
-              </template>
-            </v-list-item>
-            <v-list-item lines="two" data-veo-test="import-object-schema" @click="state = 'import'">
-              <v-list-item-title class="font-weight-bold">
-                {{ t('importObjectSchema') }}
-              </v-list-item-title>
-              <v-list-item-subtitle>{{ t('importObjectSchemaDescription') }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ t('openObjectSchemaDescription') }}</v-list-item-subtitle>
               <template #append>
                 <v-icon size="x-large" :icon="mdiChevronRight" />
               </template>
             </v-list-item>
           </v-list>
         </v-window-item>
-        <v-window-item value="create" class="px-4">
-          <v-form v-model="createFormIsValid" @submit.prevent="createSchema()">
-            <h2 class="text-h2">
-              {{ t('createObjectSchema') }}
-            </h2>
-            <v-row no-gutters class="align-center mt-4">
-              <v-col cols="12" :md="5">
-                <span style="font-size: 1.2rem"> {{ t('type') }}*: </span>
-              </v-col>
-              <v-col cols="12" :md="5">
-                <v-text-field
-                  v-model="createForm.type"
-                  :label="t('type')"
-                  :rules="[requiredRule]"
-                  required
-                  variant="underlined"
-                  data-veo-test="object-schema-type"
-                />
-              </v-col>
-            </v-row>
-            <v-row no-gutters class="align-center mt-4">
-              <v-col cols="12" :md="5">
-                <span style="font-size: 1.2rem"> {{ t('description') }}*: </span>
-              </v-col>
-              <v-col cols="12" :md="5">
-                <v-text-field
-                  v-model="createForm.description"
-                  :label="t('description')"
-                  :rules="[requiredRule]"
-                  required
-                  variant="underlined"
-                  data-veo-test="object-schema-description"
-                />
-              </v-col>
-            </v-row>
-          </v-form>
-          <small>{{ $t('global.input.requiredfields') }}</small>
-        </v-window-item>
         <v-window-item value="import" class="px-4">
           <h2 class="text-h2">
-            {{ t('importObjectSchema') }}
+            {{ t('openObjectSchema') }}
           </h2>
           <v-row no-gutters class="align-center mt-4">
             <v-col cols="12" :md="5">
@@ -110,44 +60,23 @@
           </v-row>
           <v-row v-if="modelType === 'custom'">
             <v-col cols="12">
-              <EditorFileUpload :code="code" :input-label="t('uploadLabel')" @schema-uploaded="importSchema" />
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <span style="text-decoration: underline; font-weight: bold; cursor: pointer" @click="state = 'create'">
-                {{ t('importObjectSchemaSwitch') }}
-              </span>
+              <EditorFileUpload :code="code" :input-label="t('uploadLabel')" @schema-uploaded="openSchema" />
             </v-col>
           </v-row>
         </v-window-item>
       </v-window>
     </template>
     <template #dialog-options>
-      <v-btn v-if="state !== 'start'" @click="state = 'start'">
-        {{ $t('global.button.previous') }}
-      </v-btn>
       <v-spacer />
-      <v-btn
-        v-if="state === 'create'"
-        color="primary"
-        role="submit"
-        type="submit"
-        :disabled="createFormIsValid === false"
-        data-veo-test="create-object-schema-button"
-        @click="createSchema()"
-      >
-        {{ $t('global.button.next') }}
-      </v-btn>
       <v-btn
         v-if="state === 'import' && modelType !== 'custom'"
         color="primary"
         variant="text"
         role="submit"
         type="submit"
-        data-veo-test="import-object-schema-button"
+        data-veo-test="open-object-schema-button"
         :disabled="importNextDisabled"
-        @click="importSchema()"
+        @click="openSchema()"
       >
         {{ $t('global.button.next') }}
       </v-btn>
@@ -183,11 +112,10 @@ const route = useRoute();
 const router = useRouter();
 const { locale, t } = useI18n();
 const { t: $t } = useI18n({ useScope: 'global' });
-const { requiredRule } = useRules();
 const queryClient = useQueryClient();
 
 // Layout stuff
-const state = ref<'start' | 'import' | 'create'>('start');
+const state = ref<'start' | 'import'>('start');
 
 watch(
   () => props.modelValue,
@@ -200,14 +128,6 @@ watch(
 
 const isNavigatedByDialog = computed(() => isEmpty(route.query));
 const isDialogCustom = computed(() => route.query.os === 'custom');
-
-// Create stuff
-const createForm = ref({
-  type: undefined,
-  description: undefined
-});
-
-const createFormIsValid = ref(false);
 
 // Import stuff
 const code = ref();
@@ -230,21 +150,7 @@ const availableObjectSchemas = computed(() =>
 
 const importNextDisabled = computed(() => (modelType.value === 'custom' && !code.value) || !modelType.value);
 
-const createSchema = async () => {
-  emit('completed', {
-    schema: undefined,
-    meta: {
-      type: createForm.value.type,
-      description: createForm.value.description
-    }
-  });
-  await navigateTo({
-    type: createForm.value.type,
-    description: createForm.value.description
-  });
-};
-
-const importSchema = async (schema?: any) => {
+const openSchema = async (schema?: any) => {
   if (schema) {
     emit('completed', { schema, meta: undefined });
     await navigateTo({ os: 'custom' });
@@ -259,12 +165,6 @@ const importSchema = async (schema?: any) => {
   }
 };
 
-const clearCreateForm = () => {
-  createForm.value = {
-    type: undefined,
-    description: undefined
-  };
-};
 const onClose = () => {
   router.push({
     name: 'unit-domains-domain-editor'
@@ -286,12 +186,7 @@ watch(
   (newValue) => {
     // If the user navigates by URL, depending on the parameters, schemas should be generated
     if (!isNavigatedByDialog.value || isDialogCustom.value) {
-      if (isString(newValue.query.type) && isString(newValue.query.description)) {
-        // If a user navigates through a URL which has parameters type and description, new OS should be created
-        createForm.value.type = newValue.query.type;
-        createForm.value.description = newValue.query.description;
-        createSchema();
-      } else if (newValue.query.os === 'custom') {
+      if (newValue.query.os === 'custom') {
         // If a user navigates through a URL which has custom os parameter,
         // the dialog with selected custom OS should be opened
         state.value = 'import';
@@ -301,13 +196,12 @@ watch(
         // (e.g. 'process', 'asset', etc.), the OS should be automatically loaded from the server
         state.value = 'import';
         modelType.value = newValue.query.os;
-        importSchema();
+        openSchema();
       }
     } else if (isEmpty(newValue.query)) {
       state.value = 'start';
       code.value = '';
       modelType.value = '';
-      clearCreateForm();
       emit('completed', {});
     }
   },
