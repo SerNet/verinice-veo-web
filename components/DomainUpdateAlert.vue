053eb8131ae1c370736037d2e03e7bb7f3c3ac0c
jae <jae@sernet.de>
2026-02-27 15:01:55 +0100

    Change domain migration alert type

--- components/DomainUpdateAlert.vue
+++ components/DomainUpdateAlert.vue
@@ -1,16 +1,16 @@
 <!--
    - verinice.veo web
    - Copyright (C) 2026 Haneen Husin
-   - 
+   -
    - This program is free software: you can redistribute it and/or modify it
    - under the terms of the GNU Affero General Public License
    - as published by the Free Software Foundation, either version 3 of the License,
    - or (at your option) any later version.
-   - 
+   -
    - This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
    - without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
    - See the GNU Affero General Public License for more details.
-   - 
+   -
    - You should have received a copy of the GNU Affero General Public License along with this program.
    - If not, see <http://www.gnu.org/licenses/>.
 -->
@@ -23,9 +23,10 @@
         :model-value="message.isVisible"
         :title="t('title', [message.domainName])"
         :text="t('hin', [message.domainName, message.currentVersion, message.newVersion])"
-        :type="VeoAlertType.WARNING"
+        :type="VeoAlertType.INFO"
         class="mt-2 text-pre-wrap"
         :buttons="getMigrationButtons(message)"
+        :defaultButtonText="t('dismiss')"
         @update:model-value="(val) => handleDismiss(val, message.id)"
       >
       </BaseAlert>
