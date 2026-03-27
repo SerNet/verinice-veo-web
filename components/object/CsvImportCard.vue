df98407a5ecd833a67d0e3f13917f41c92a22a9d
dmirosavljevic <djm@sernet.de>
2026-03-27 12:42:38 +0100

    not used class
    verinice-veo#4662

--- components/object/CsvImportCard.vue
+++ components/object/CsvImportCard.vue
@@ -307,18 +307,6 @@ const handleNavigate = (objectType: string, subType: string) => {
   background-color: rgba(25, 118, 210, 0.05);
 }
 
-.hidden-input {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border: 0;
-}
-
 ::v-deep(.v-divider__content) {
   color: #555555 !important;
 }
