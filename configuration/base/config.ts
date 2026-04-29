cf2db6ca40bef99b44350dca9b97861bf6f0ac9b
dmirosavljevic <djm@sernet.de>
2026-04-29 11:19:19 +0200

    add color for GDPR domain
    verinice-veo#4765

--- configuration/base/config.ts
+++ configuration/base/config.ts
@@ -3,6 +3,7 @@ export const config = {
   domains: {
     colors: {
       'ds-gvo': '#c90000',
+      'gdpr-int': '#c90000',
       'iso-iec-27000': '#003f63', // This domain name is deprecated (ISO/IEC 27000)
       'iso-27001-de': '#003f63',
       nis2: '#9c26af',
