74f8ed9eb044e1aa5df2ce243b15768e745c2733
jae <jae@sernet.de>
2026-05-12 13:27:07 +0200

    Workaround nuxt dev server regression
    verinice-veo#4803

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
   //==============================================================
   telemetry: false,
   experimental: {
+    viteEnvironmentApi: true,
     entryImportMap: false
   },
 
